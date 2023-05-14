12e9058259be79c34812c93d11ba4fd6773eb808
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2023-05-14 09:04:09 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i, a, b;
+	cin >> i;
+
+	for (int j = 0; j < i; j++)
+	{
+		cin >> a >> b;
+		cout << a + b << endl;;
+	}
+
+	return 0;
+}
