1aa6b9f2dda370abac4f1dac04df6601338b3aa2
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-01-30 22:36:58 +0900

    [Bronze II] Title: 숫자, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+
+	if (a == b)
+		cout << 0;
+	else if (a > b)
+	{
+		cout << a - b - 1 << "\n";
+		for (int i = b + 1; i < a; i++)
+			cout << i << " ";
+	}
+	else
+	{
+		cout << b - a - 1 << "\n";
+		for (int i = a + 1; i < b; i++)
+			cout << i << " ";
+	}
+
+	return 0;
+}
