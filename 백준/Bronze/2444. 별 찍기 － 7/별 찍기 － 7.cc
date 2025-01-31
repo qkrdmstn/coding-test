2d9db0adf28fe02d73476c3f44fc2bdbe278d1da
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-01-31 20:58:23 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -1,37 +1,29 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	int n;
-	cin >> n;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	for (int i = 1; i <= (2 * n - 1); i++)
-	{
-		if (i <= n)
-		{
-			for (int k = 0; k < n - i; k++)
-				cout << " ";
+    int n;
+    cin >> n;
 
-			for (int j = 0; j < (2 * i - 1); j++)
-				cout << "*";
-
-			cout << endl;
-		}
-		else
-		{
-			for (int k = 0; k < i - n; k++)
-				cout << " ";
-
-			for (int j = 0; j < 2*(2 * n - i)-1; j++)
-				cout << "*";
-
-			cout << endl;
-		}
-		
-	}
-
-
-	return 0;
+    for(int i=1; i<n; i++)
+    {
+        for(int j=0; j<n-i; j++)            
+            cout << " ";
+        for(int j=0; j<(2*i-1); j++)            
+            cout << "*";
+        cout << "\n";
+    }
+    
+    for(int i=n; i>0; i--)
+    {
+        for(int j=0; j<n-i; j++)            
+            cout << " ";
+        for(int j=0; j<(2*i-1); j++)            
+            cout << "*";
+        cout << "\n";
+    }
+    return 0;
 }
