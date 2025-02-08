fc0f8e3a66b270b92d5b87051256325df7a80001
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-08 15:11:41 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 8 ms, Memory: 2416 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -1,28 +1,26 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
+
+int n;
+long long sum = 0;
+stack<int> s;
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	int n;
-	long long ans = 0;
-	stack<int> s;
-
 	cin >> n;
-	
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		long long h;
 		cin >> h;
-
-		while (!s.empty() && s.top() <= h)
+		while (!s.empty() && s.top() <= h) {
 			s.pop();
-		ans += s.size();
+		}
+		sum += s.size();
 		s.push(h);
 	}
-
-	cout << ans;
+	cout << sum;
 	return 0;
 }
