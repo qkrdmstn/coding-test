ff77f40543c4cc4ace43ca47f46f4b88c804e2b8
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-09 09:22:00 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.cc
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -1,54 +1,52 @@
-#include<iostream>
+#include <iostream>
 #include <string>
 #include <stack>
 using namespace std;
+
+string str;
+stack<char> s;
+int sum = 0;
+int num = 1;
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	string str;
-	stack<char> s;
-	int num = 1;
-	int ans = 0;
-
 	cin >> str;
-	for (int i = 0; i < str.length(); i++) {
+	for (int i = 0; i < str.size(); i++) {
 		if (str[i] == '(') {
-			s.push(str[i]);
 			num *= 2;
+			s.push(str[i]);
 		}
 		else if (str[i] == '[') {
-			s.push(str[i]);
 			num *= 3;
+			s.push(str[i]);
 		}
 		else if (str[i] == ')') {
 			if (s.empty() || s.top() != '(') {
-				cout << "0";
+				cout << 0;
 				return 0;
 			}
 			s.pop();
-			if(str[i-1] == '(')
-				ans += num;
+			if(str[i - 1] == '(')
+				sum += num;
 			num /= 2;
-		}
+		}		
 		else if (str[i] == ']') {
 			if (s.empty() || s.top() != '[') {
-				cout << "0";
+				cout << 0;
 				return 0;
 			}
 			s.pop();
 			if (str[i - 1] == '[')
-				ans += num;
+				sum += num;
 			num /= 3;
 		}
 	}
-
-	if (!s.empty()) 
-		cout << "0";
-	else 
-		cout << ans;
-
+	if (s.empty())
+		cout << sum;
+	else
+		cout << 0;
 	return 0;
 }
