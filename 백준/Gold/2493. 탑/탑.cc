38adef6839d8b7d4e3aa94d4347133cf7373b0ce
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-17 20:52:46 +0900

    [Gold V] Title: 탑, Time: 84 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,5 +1,5 @@
-#include <iostream>
-#include <stack>
+#include<iostream>
+#include<stack>
 using namespace std;
 
 int main(void)
@@ -8,22 +8,22 @@ int main(void)
 	cin.tie(0);
 
 	int n;
-	stack<pair<int, int>> v;
+	stack<pair<int, int>> s;
 
 	cin >> n;
-	v.push({ 100000001, 0 });
 	for (int i = 1; i <= n; i++) {
 		int h;
 		cin >> h;
 
-		while (v.top().first < h)
-			v.pop();
-		cout << v.top().second << " ";
-		v.push({ h,i });
+		while (!s.empty() && s.top().first < h) {
+			s.pop();
+		}
+		if (s.empty())
+			cout << "0 ";
+		else
+			cout << s.top().second << " ";
+		s.push({ h,i });
 	}
-	return 0;
 
-	//1. 자신보다 높이가 낮은 탑은 가려지므로, pop 해도 된다는 점
-	//2. stack에 pair 형태로 높이와 순번을 같이 저장한다는 점
-	//3. 처음에 가장 높은 탑을 세워서 empty 체크를 안해도 된다는 점
+	return 0;
 }
