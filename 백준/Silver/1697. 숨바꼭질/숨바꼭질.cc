39c22e53930f3fbc381eb289a1cc141e54706e86
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-18 16:59:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,25 +1,37 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
-#define X first
-#define Y second
-int dist[100002];
-int n,k;
-int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> k;
-  fill(dist, dist+100001,-1);
-  dist[n] = 0;
-  queue<int> Q;
-  Q.push(n);
-  while(dist[k] == -1){
-    int cur = Q.front(); Q.pop();
-    for(int nxt : {cur-1, cur+1, 2*cur}){
-      if(nxt < 0 || nxt > 100000) continue;
-      if(dist[nxt] != -1) continue;
-      dist[nxt] = dist[cur]+1;
-      Q.push(nxt);
-    }        
-  }
-  cout << dist[k];
+
+queue<int> q;
+int dist[100001];
+
+int main(void)
+{
+	int n, k;
+	cin >> n >> k;
+
+	for (int i = 0; i < 100001; i++) {
+		dist[i] = -1;
+	}
+
+	q.push(n);
+	dist[n] = 0;
+	
+	while (dist[k] == -1) {
+		int cur = q.front();
+		q.pop();
+
+		for (int next : {cur - 1, cur + 1, 2 * cur}) {
+			
+			if (next < 0 || next >= 100001)
+				continue;
+			if (dist[next] != -1)
+				continue;
+			dist[next] = dist[cur] + 1;
+			q.push(next);
+		}
+	}
+
+	cout << dist[k];
+	return 0;
 }
