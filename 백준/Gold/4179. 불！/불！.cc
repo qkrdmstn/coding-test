d80df8ec131c70136926f7e277b1a75129ec6a88
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-18 16:44:01 +0900

    [Gold III] Title: 불!, Time: 36 ms, Memory: 10940 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -1,78 +1,82 @@
 #include <iostream>
-#include <string>
 #include <queue>
+#include <string>
 using namespace std;
 
-int r, c;
-string map[1001];
 queue<pair<int, int>> q1;
 queue<pair<int, int>> q2;
+string map[1001];
 int dist1[1001][1001];
 int dist2[1001][1001];
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
+int dx[4] = { 0, 1, 0, -1 };
+int dy[4] = { 1, 0, -1, 0 };
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	cin >> r >> c;
+    int n, m;
+    cin >> n >> m;
 
-	for (int i = 0; i < r; i++) {
-		cin >> map[i];
-		for (int j = 0; j < c; j++) {
-			dist1[i][j] = -1;
-			dist2[i][j] = -1;
-			if (map[i][j] == 'F') {
-				q1.push({ i,j });
-				dist1[i][j] = 0;
-			}
-			if (map[i][j] == 'J') {
-				q2.push({ i,j });
-				dist2[i][j] = 0;
-			}
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        cin >> map[i];
+        for (int j = 0; j < m; j++) {
+            dist1[i][j] = -1;
+            dist2[i][j] = -1;
+            if (map[i][j] == 'F') {
+                q1.push({ i,j });
+                dist1[i][j] = 0;
+            }
+            else if (map[i][j] == 'J') {
+                q2.push({ i,j });
+                dist2[i][j] = 0;
+            }
+        }
+    }
 
-	while (!q1.empty()) { //불에 대한 BFS
-		pair<int, int> cur = q1.front();
-		q1.pop();
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
+    //불 BFS
+    while (!q1.empty()) {
+        pair<int, int> cur = q1.front();
+        q1.pop();
 
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c)
-				continue;
-			if (map[nx][ny] == '#' || dist1[nx][ny] != -1)
-				continue;
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
 
-			dist1[nx][ny] = dist1[cur.first][cur.second] + 1;
-			q1.push({ nx, ny });
-		}
-	}
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+            if (dist1[nx][ny] != -1 || map[nx][ny] == '#')
+                continue;
 
-	while (!q2.empty()) { //지훈이에 대한 BFS
-		pair<int, int> cur = q2. front();
-		q2.pop();
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
+            dist1[nx][ny] = dist1[cur.first][cur.second] + 1;
+            q1.push({ nx, ny });
+        }
+    }
 
-			if (nx < 0 || nx >= r || ny < 0 || ny >= c) {
-				cout << dist2[cur.first][cur.second] + 1;
-				return 0;
-			}
-			if (map[nx][ny] == '#' || dist2[nx][ny] != -1)
-				continue;
-			if (dist1[nx][ny] != -1 && dist2[cur.first][cur.second] + 1 >= dist1[nx][ny])
-				continue;
+    //지훈 BFS
+    while (!q2.empty()) {
+        pair<int, int> cur = q2.front();
+        q2.pop();
 
-			dist2[nx][ny] = dist2[cur.first][cur.second] + 1;
-			q2.push({ nx, ny });
-		}
-	}
+        for (int i = 0; i < 4; i++) {
+            int nx = cur.first + dx[i];
+            int ny = cur.second + dy[i];
 
-	cout << "IMPOSSIBLE";
-	return 0;
+            //탈출
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                cout << dist2[cur.first][cur.second] + 1;
+                return 0;
+            }
+            if (dist2[nx][ny] != -1 || map[nx][ny] == '#')
+                continue;
+            if (dist1[nx][ny] != -1 && dist1[nx][ny] <= dist2[cur.first][cur.second] + 1)
+                continue;
+
+            dist2[nx][ny] = dist2[cur.first][cur.second] + 1;
+            q2.push({ nx, ny });
+        }
+    }
+    cout << "IMPOSSIBLE";
+    return 0;
 }
