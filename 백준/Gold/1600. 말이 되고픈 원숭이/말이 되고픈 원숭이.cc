9bcda98d95cda253854bacd3d6040f6a7dcfbe3c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-21 22:12:26 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 60 ms, Memory: 7364 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.cc
@@ -1,54 +1,80 @@
-// Authored by : heheHwang
-// Co-authored by : BaaaaaaaaaaarkingDog
-// http://boj.kr/40081492d92a4e1d9c805d23bf485942
-#include <bits/stdc++.h>
+#include<iostream>
+#include<queue>
+#include<tuple>
 using namespace std;
 
-int board[202][202];
-int vis[32][202][202];
-int dmx[4] = { 0, 0, 1, -1 };
-int dmy[4] = { 1, -1, 0, 0 };
-int dkx[8] = { -2, -2, -1, -1, 1, 1, 2, 2 };
-int dky[8] = { 1, -1, 2, -2, 2, -2, 1, -1 };
-queue<tuple<int, int, int>> Q;
-int bX, bY, Kn;
+int k, w, h;
+queue<tuple<int, int, int>> q;
+int map[201][201];
+int dist[201][201][32];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+int dhx[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+int dhy[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
 
-int main(void) {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> k;
+	cin >> w >> h;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			cin >> map[i][j];
+			for (int l = 0; l <= k; l++) {
+				dist[i][j][l] = -1;
+			}
+		}
+	}
 
-  cin >> Kn >> bY >> bX;
-  for (int i = 0; i < bX; i++)
-    for (int j = 0; j < bY; j++)
-      cin >> board[i][j];
-  Q.push({ 0, 0, 0 });
-  vis[0][0][0] = 1;
-  while (!Q.empty()) {
-    int vk, vx, vy;
-    tie(vk, vx, vy) = Q.front();
-    Q.pop();
-    if (vk < Kn) {
-      for (int i = 0; i < 8; i++) {
-        int nx = vx + dkx[i], ny = vy + dky[i];
-        if (nx < 0 || ny < 0 || bX <= nx || bY <= ny) continue;
-        if (board[nx][ny]) continue;
-        if (vis[vk + 1][nx][ny]) continue;
-        vis[vk + 1][nx][ny] = vis[vk][vx][vy] + 1;
-        Q.push({ vk + 1, nx, ny });
-      }
-    }
-    for (int i = 0; i < 4; i++) {
-      int nx = vx + dmx[i], ny = vy + dmy[i];
-      if (nx < 0 || ny < 0 || bX <= nx || bY <= ny) continue;
-      if (board[nx][ny]) continue;
-      if (vis[vk][nx][ny]) continue;
-      vis[vk][nx][ny] = vis[vk][vx][vy] + 1;
-      Q.push({ vk, nx, ny });
-    }
-  }
-  int ans = 0x7f7f7f7f;
-  for (int i = 0; i < Kn + 1; i++)
-    if(vis[i][bX - 1][bY - 1]) ans = min(ans, vis[i][bX - 1][bY - 1]);
-  if (ans != 0x7f7f7f7f) cout << ans - 1;
-  else cout << -1;
+	dist[0][0][0] = 0;
+	q.push({ 0,0,0 });
+
+	while (!q.empty())
+	{
+		int curX, curY, curK;
+		tie(curX, curY, curK) = q.front();
+		q.pop();
+		if (curK < k) {
+			for (int dir = 0; dir < 8; dir++) {
+				int nx = curX + dhx[dir];
+				int ny = curY + dhy[dir];
+				int nk = curK + 1;
+
+				if (nx < 0 || nx >= h || ny < 0 || ny >= w)
+					continue;
+				if (map[nx][ny] != 0 || dist[nx][ny][nk] != -1)
+					continue;
+
+				dist[nx][ny][nk] = dist[curX][curY][curK] + 1;
+				q.push({ nx, ny, nk });
+			}
+		}
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = curX + dx[dir];
+			int ny = curY + dy[dir];
+			int nk = curK;
+
+			if (nx < 0 || nx >= h || ny < 0 || ny >= w)
+				continue;
+			if (map[nx][ny] != 0 || dist[nx][ny][nk] != -1)
+				continue;
+
+			dist[nx][ny][nk] = dist[curX][curY][curK] + 1;
+			q.push({ nx, ny, nk });
+		}
+	}
+
+	int ans = 10000000;
+	for (int i = 0; i <= k; i++) {
+		if (dist[h - 1][w - 1][i] != -1 && dist[h - 1][w - 1][i] < ans) {
+			ans = dist[h - 1][w - 1][i];
+		}
+	}
+	if (ans != 10000000)
+		cout << ans;
+	else
+		cout << -1;
+	return 0;
 }
