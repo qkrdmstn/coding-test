3559d81e8eb5e4fb382702b2766ff4566e1651cb
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-02-21 20:08:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n, k;
+queue<int> q;
+int map[100001];
+int dist[100001];
+
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n >> k;
+	for (int i = 0; i < 100001; i++)
+	{
+		dist[i] = 100001;
+	}
+
+	q.push(n);
+	dist[n] = 0;
+
+	while (dist[k] == 100001) {
+		int cur = q.front();
+		q.pop();
+
+		for (int next : {2 * cur, cur - 1, cur + 1}) {
+
+			if (next < 0 || next > 100000)
+				continue;
+			int nextDist = (next == 2 * cur) ? dist[cur] : dist[cur] + 1;
+			//cout << cur << ", " << dist[cur] << ", " << next << ", " << nextDist << "\n";
+
+			if (nextDist < dist[next] ) {
+				q.push(next);
+				dist[next] = nextDist;
+				continue;
+			}
+			if (dist[next] != 100001)
+				continue;
+
+			q.push(next);
+			dist[next] = nextDist;
+		}
+	}
+	//for(int i=n; i<=k+1; i++)
+	//	cout << dist[i] << " ";
+	cout << dist[k];
+	return 0;
+}
