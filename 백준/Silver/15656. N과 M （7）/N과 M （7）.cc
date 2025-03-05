a4385d2378e25725bdf81d50e6e7ba7dd96bf3a3
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-03-05 17:32:29 +0900

    [Silver III] Title: N과 M (7), Time: 424 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[8];
+int ans[8];
+
+void func(int cnt)
+{
+	if (cnt == m) {
+		for (int i = 0; i < cnt; i++)
+			cout << ans[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	else {
+		for (int i = 0; i < n; i++) {
+
+			ans[cnt] = arr[i];
+			func(cnt + 1);
+		}
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	sort(arr, arr + n);
+
+	func(0);
+	return 0;
+}
