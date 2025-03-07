4e4d6e3decce453abb411732862ee65be3f7cc54
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-03-07 16:17:32 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 44 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int n;
+int weight[8];
+int durability[8];
+bool isBroken[8];
+int ans;
+
+//손에 든 계란 말고 모두 깨진 경우
+bool flag(int start)
+{
+	for (int i = 0; i < n; i++) {
+		if(i==start) continue;
+		if (!isBroken[i])
+			return false;
+	}
+	return true;
+}
+
+//손에 쥐고 있는 계란 및 깨진 계란의 개수를 인자로 받는다.
+void func(int start, int cnt)
+{
+	//cout << "start: " << start << " " << cnt << "\n";
+	//cout << "B:  ";
+	//for (int i = 0; i < n; i++)
+	//	cout << isBroken[i] << " ";
+	//cout << "\n"; 
+	if (start == n || flag(start)) {
+		//cout << "!!!!\n";
+		if (cnt > ans)
+			ans = cnt;
+		return;
+	}
+	else {
+
+
+		//내리 칠 계란 순회
+		for (int i = 0; i < n; i++) {
+			//손에 쥐고 있는 계란이거나 내리칠 계란이 깨진 계란이면 continue
+			if (i == start || isBroken[i] || isBroken[start])
+				continue;
+
+			durability[start] -= weight[i];
+			if (!isBroken[start] && durability[start] <= 0) {
+				isBroken[start] = true;
+				cnt++;
+			}
+			durability[i] -= weight[start];
+			if (!isBroken[i] && durability[i] <= 0) {
+				isBroken[i] = true;
+				cnt++;
+			}
+			//cout << "B:  ";
+			//for (int i = 0; i < n; i++)
+			//	cout << isBroken[i] << " ";
+			//cout << "\n";
+			//다음 계란 들기
+			func(start + 1, cnt);
+
+			durability[start] += weight[i];
+			if (isBroken[start] && durability[start] > 0) {
+				isBroken[start] = false;
+				cnt--;
+			}
+			durability[i] += weight[start];
+			if (isBroken[i] && durability[i] > 0) {
+				isBroken[i] = false;
+				cnt--;
+			}
+			//cout << "AB:  ";
+			//for (int i = 0; i < n; i++)
+			//	cout << isBroken[i] << " ";
+			//cout << "\n";
+		}
+
+		if (isBroken[start])
+			func(start + 1, cnt);
+	}
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> durability[i] >> weight[i];
+
+	func(0, 0);
+	cout << ans;
+
+	return 0;
+}
