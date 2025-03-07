46d0cb1b9409b7a937c22d77c2742c20cc86e513
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-03-07 16:25:52 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.cc
@@ -7,10 +7,10 @@ int n;
 int weight[8];
 int durability[8];
 bool isBroken[8];
-int ans;
+int ans, cnt;
 
 //손에 든 계란 말고 모두 깨진 경우
-bool flag(int start)
+bool IsAllBroken(int start)
 {
 	for (int i = 0; i < n; i++) {
 		if(i==start) continue;
@@ -21,48 +21,49 @@ bool flag(int start)
 }
 
 //손에 쥐고 있는 계란 및 깨진 계란의 개수를 인자로 받는다.
-void func(int start, int cnt)
+void func(int start)
 {
-	if (start == n || flag(start)) {
+	if (start == n || IsAllBroken(start)) {
 		if (cnt > ans)
 			ans = cnt;
 		return;
 	}
 	else {
 
+		//손에 든 계란이 깨졌으면 넘어가기
 		if (isBroken[start]) {
-			func(start + 1, cnt);
+			func(start + 1);
+			return;
 		}
-		else {
-			//내리 칠 계란 순회
-			for (int i = 0; i < n; i++) {
-				//손에 쥐고 있는 계란이거나 내리칠 계란이 깨진 계란이면 continue
-				if (i == start || isBroken[i] || isBroken[start])
-					continue;
 
-				durability[start] -= weight[i];
-				if (!isBroken[start] && durability[start] <= 0) {
-					isBroken[start] = true;
-					cnt++;
-				}
-				durability[i] -= weight[start];
-				if (!isBroken[i] && durability[i] <= 0) {
-					isBroken[i] = true;
-					cnt++;
-				}
-				//다음 계란 들기
-				func(start + 1, cnt);
+		//내리 칠 계란 순회
+		for (int i = 0; i < n; i++) {
+			//손에 쥐고 있는 계란이거나 내리칠 계란이 깨진 계란이면 continue
+			if (i == start || isBroken[i])
+				continue;
 
-				durability[start] += weight[i];
-				if (isBroken[start] && durability[start] > 0) {
-					isBroken[start] = false;
-					cnt--;
-				}
-				durability[i] += weight[start];
-				if (isBroken[i] && durability[i] > 0) {
-					isBroken[i] = false;
-					cnt--;
-				}
+			durability[start] -= weight[i];
+			if (!isBroken[start] && durability[start] <= 0) {
+				isBroken[start] = true;
+				cnt++;
+			}
+			durability[i] -= weight[start];
+			if (!isBroken[i] && durability[i] <= 0) {
+				isBroken[i] = true;
+				cnt++;
+			}
+			//다음 계란 들기
+			func(start + 1);
+
+			durability[start] += weight[i];
+			if (isBroken[start] && durability[start] > 0) {
+				isBroken[start] = false;
+				cnt--;
+			}
+			durability[i] += weight[start];
+			if (isBroken[i] && durability[i] > 0) {
+				isBroken[i] = false;
+				cnt--;
 			}
 		}
 	}
@@ -77,7 +78,7 @@ int main(void)
 	for (int i = 0; i < n; i++)
 		cin >> durability[i] >> weight[i];
 
-	func(0, 0);
+	func(0);
 	cout << ans;
 
 	return 0;
