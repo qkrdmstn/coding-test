315cc4a64e63f8acc421193584bf3e82f86e1ac9
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-03-31 15:54:59 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -4,8 +4,8 @@ using namespace std;
 #define Y second
 
 int n;
-//d[n]: n번째 자리의 끝자리가 X:0/Y:1 인 경우의 수
-pair<long, long> d[92];
+//d[n]: n번째 자리의 이친수의 수
+long long d[92];
 
 int main(void)
 {
@@ -14,14 +14,11 @@ int main(void)
 
 	cin >> n;
 
-	d[1] = { 0,1 };
-	d[2] = { 1,0 };
-
-	//d[i-1]로 d[i]를 만드는 과정에서 끝이 0으로 끝나는 자리엔 1과 0이 모두 올 수 있고,
-	//1로 끝나는 자리엔 1만 올 수 있다.
+	d[1] = 1;
+	d[2] = 1;
 	for (int i = 3; i <= n; i++)
-		d[i] = { d[i - 1].X + d[i - 1].Y , d[i - 1].X};
-	cout << d[n].X + d[n].Y;
+		d[i] = d[i-1] + d[i-2];
+	cout << d[n];
 
 	//d[i-1].X + d[i-1].Y = d[i-1], d[i-1].X = d[i-2].X + d[i-2].Y = d[i-2] 
 	//=> d[i] = d[i-1] + d[i-2]도 같은 식
