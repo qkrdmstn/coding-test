fc8c0e2d1aed464bf96193f43e8ccc1c289d0d63
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-04-21 21:48:40 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, ans = 0;;
+	cin >> n;
+	
+	for (int i = 5; i <= n; i *= 5) {
+		ans += n / i;
+	}
+	cout << ans;
+	return 0;
+}
