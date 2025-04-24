fc2f78cf8f1a9c1572069838f2fea98a157a054f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-04-24 18:05:34 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -5,42 +5,34 @@ using namespace std;
 int n, m;
 int a[500005];
 
-int lowerIndex(int num)
-{
+int lowerIndex(int num) {
 	int start = 0;
 	int end = n;
 	while (start < end) {
 		int mid = (start + end) / 2;
-		if (num <= a[mid])
-			end = mid;
-		else
-			start = mid + 1;
+		if (num <= a[mid]) end = mid;
+		else start = mid + 1;
 	}
 	return start;
 }
 
-int upperIndex(int num)
-{
+int upperIndex(int num) {
 	int start = 0;
 	int end = n;
 	while (start < end) {
 		int mid = (start + end) / 2;
-		if (num < a[mid])
-			end = mid;
-		else
-			start = mid + 1;
+		if (a[mid] <= num) start = mid + 1;
+		else end = mid;
 	}
 	return start;
 }
 
-int main(void)
-{
+int main(void) {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
-	
+
 	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> a[i];
+	for (int i = 0; i < n; i++) cin >> a[i];
 	sort(a, a + n);
 
 	cin >> m;
@@ -49,6 +41,5 @@ int main(void)
 		cin >> num;
 		cout << upperIndex(num) - lowerIndex(num) << ' ';
 	}
-
 	return 0;
 }
