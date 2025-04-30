dd77e20faa86d809f00f3eec86054754bcb8ee63
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-04-30 14:39:43 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -15,15 +15,20 @@ int main(void)
 	sort(a, a + n);
 	
 	int ans = 2'000'000'001;
-	for (int i = 0; i < n; i++) {
-		int idx1 = lower_bound(a, a + n, a[i] + m) - a;
-		int idx2 = lower_bound(a, a + n, a[i] - m) - a;
 
-		if (idx1 < n && abs(a[i] - a[idx1]) >= m && abs(ans) > abs(a[i] - a[idx1]))
-			ans = a[i] - a[idx1];
-		if (idx2 < n && abs(a[i] - a[idx2]) >= m && abs(ans) > abs(a[i] - a[idx2]))
-			ans = a[i] - a[idx2];
+	int st = 0, en = 0;
+	for (st = 0; st < n; st++) {
+		while (a[en] - a[st] < m)
+			en++;
+		if (en >= n)
+			break;
+
+		ans = min(ans, a[en] - a[st]);
 	}
 	cout << abs(ans);
+
+	/*
+	투포인터 풀이법
+	*/
 	return 0;
 }
