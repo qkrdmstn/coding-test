4d239c8d751274dcf6fd931c34b3244b28389b46
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-05-12 16:15:45 +0900

    [Platinum V] Title: 이진 검색 트리, Time: 128 ms, Memory: 14748 KB -BaekjoonHub

--- 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.cc
+++ 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.cc
@@ -13,25 +13,39 @@ int main(void)
 	ios::sync_with_stdio(0);
 	
 	cin >> n;
-	s.insert(n + 1);
-	s.insert(0);
 
-	int p;
-	cin >> p;
-	p++;
-	h[p] = 1;
-	s.insert(p);
+	int p; cin >> p;
+	h[p] = 1; s.insert(p);
 	ans += h[p];
 	n--;
 
 	while (n--) {
-		cin >> p; p++;
+		cin >> p;
 		auto it = s.lower_bound(p);
-		h[p] = max(h[*prev(it)], h[*it]) + 1;
+
+		if (it != s.begin() && it != s.end())
+			h[p] = max(h[*prev(it)], h[*it]) + 1;
+		else if (it == s.begin())
+			h[p] = h[*it] + 1;
+		else if (it == s.end())
+			h[p] = h[*prev(it)] + 1;
 		s.insert(p);
 
 		ans += h[p];
 	}
 	cout << ans;
+
 	return 0;
+
+	/*
+	A의 높이 H[A]는 max(H[A보다 작은 수 중 최댓값], H[A보다 큰 수 중 최소값]) + 1
+	로 표현 가능.
+	이때, lower_bound를 통해 A보다 작은 수 중 최댓값과 A보다 큰 수 중 최소값을 계산할 수 있는데
+	예외 처리가 필요
+	1. it가 begin()을 가리켜서 prev가 범위를 벗어나는 경우
+	2. it가 end()를 가리켜서 it가 범위를 벗어나는 경우
+
+	따라서, while 내부에 if문을 통해 예외처리를 진행
+	set에 0과 n+1 값을 미리 삽입하여 가독성 높은 코드로 최적화 가능
+	*/
 }
