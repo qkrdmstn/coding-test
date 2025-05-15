ee65f6a437ad95c976ff4a80d9254fa1d2eaed72
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-05-15 20:47:07 +0900

    [Gold I] Title: 수업, Time: 244 ms, Memory: 10684 KB -BaekjoonHub

---
+++ 백준/Gold/19700. 수업/수업.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int n;
+pair<int, int> a[500'005];
+
+int main(void)
+{
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> a[i].first >> a[i].second;
+
+	sort(a, a + n, greater<pair<int, int>>());
+
+	multiset<int> s;
+
+	for (int i = 0; i < n; i++) {
+		auto it = s.lower_bound(-a[i].second + 1);
+		if (it == s.end()) s.insert(-1);
+		else {
+			int val = *it;
+			s.erase(it);
+			s.insert(val - 1);
+		}
+	}
+	cout << s.size();
+	/*
+	 * 키의 내림차순으로 정렬한 뒤 수강생을 팀에 배정하면
+	 * 수강생 S를 인원이 n명인 팀에 배정하면 S의 등수는 (n + 1)등이 된다.
+	 * 따라서 수강생 S가 x등 안에 들고 싶다면,
+	 * 팀원이 (x - 1)명 이하인 팀이 있다면 그 중 팀원이 가장 많은 팀에 배정하고
+	 * 팀원이 (x - 1)명 이하인 팀이 없다면 새로운 팀을 배정한다.
+	 * multiset이 기본적으로 오름차순으로 정렬되고, (x - 1)명 이하인 팀 중에서 팀원 수가 가장 많은 팀을 원하기 때문에
+	 * 팀원 수에 -(마이너스)를 붙이고, lower_bound로 찾으면 된다.
+	 */
+	return 0;
+}
