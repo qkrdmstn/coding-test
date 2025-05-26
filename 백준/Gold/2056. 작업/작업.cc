565fb5785688def9f58f9108b974b5246958290c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-05-26 15:21:06 +0900

    [Gold IV] Title: 작업, Time: 60 ms, Memory: 6712 KB -BaekjoonHub

--- 백준/Gold/2056. 작업/작업.cc
+++ 백준/Gold/2056. 작업/작업.cc
@@ -8,8 +8,8 @@ const int MXN = 32'005;
 int n;
 vector<int> adj[MXN];
 int indeg[MXN];
-int dur[MXN];
-int t[MXN];
+int dur[MXN]; //각 작업에 걸리는 시간
+int t[MXN]; //각 작업이 끝나는 시간
 queue<int> q;
 
 int main(void)
@@ -21,6 +21,13 @@ int main(void)
 	for (int i = 1; i <= n; i++) {
 		int cnt;
 		cin >> dur[i] >> cnt;
+
+		//indegree == 0인 작업
+		if (cnt == 0) {
+			q.push(i);
+			t[i] = dur[i];
+		}
+
 		while (cnt--) {
 			int u;
 			cin >> u;
@@ -29,17 +36,13 @@ int main(void)
 		}
 	}
 
-	for (int i = 1; i <= n; i++) {
-		if (indeg[i] == 0) {
-			q.push(i);
-			t[i] = dur[i];
-		}
-	}
-
+	//위상 정렬 순회
 	while (!q.empty()){
 		int cur = q.front();
 		q.pop();
 		for (int nxt : adj[cur]) {
+			//다음 작업의 끝나는 시간 = 
+			//선행 작업 중 가장 늦게 끝난 작업의 시간 + 현재 작업의 소요 시간
 			t[nxt] = max(t[nxt], t[cur] + dur[nxt]);
 			indeg[nxt]--;
 			if (indeg[nxt] == 0)
@@ -47,6 +50,7 @@ int main(void)
 		}
 	}
 	
+	//작업 중 가장 늦게 끝난 시간 찾기
 	int ans = 0;
 	for (int i = 1; i <= n; i++)
 		ans = max(ans, t[i]);
