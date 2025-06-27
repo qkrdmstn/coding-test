4ed85cbac5011c6c643191a11deaf107dc27edc5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-06-27 20:37:57 +0900

    [level 2] Title: 완전범죄, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -19,4 +19,13 @@ int solution(vector<vector<int>> info, int n, int m) {
     answer -= dp[info.size()][m-1];
     if(answer >= n) answer = -1;
     return answer;
+    
+    /*
+    knapsak problem 응용
+    a의 흔적을 가치, b의 흔적을 무게로 두는 배낭 문제로 추상화하여,
+    m만큼의 제한이 있는 가방에 a의 흔적을 최대로 담는 방식으로 dp를 수행.
+    
+    이후, a의 총합에서 a의 흔적을 빼고 남은 갯수가 최소 흔적의 개수가 된다.
+    최소 흔적의 개수가 n 이상이면 -1을 반환하고 아니면 값을 반환한다.
+    */
 }
