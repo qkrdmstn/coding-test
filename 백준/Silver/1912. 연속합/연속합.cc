e95265ba8616720a321ecaa304b4e4cee1e03ad5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-02 17:29:50 +0900

    [Silver II] Title: 연속합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -1,28 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int n;
-//d[i]: i번째 항으로 끝나는 연속합 중 최대
-int d[100010];
-int arr[100010];
-
+int n, ans = -1005;
+int a[100005];
 int main(void)
 {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-	}
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
 
-	for (int i = 1; i <= n; i++) {
-		d[i] = max(0, d[i - 1]) + arr[i];
-	}
-
-	int ans = d[1];
-	for (int i = 2; i <= n; i++) {
-		ans = max(d[i], ans);
+	int curSum = 0;
+	for (int i = 0; i < n; i++) {
+		curSum += a[i];
+		ans = max(ans, curSum);
+		if (curSum < 0)
+			curSum = 0;
 	}
 	cout << ans;
 	return 0;
