4f47224ae1a552f118fe6d7fe8024275ea341038
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-04 16:26:13 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.08 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int sortBy, extBy;
+int GetIndexOfValue(string str)
+{
+    if(str == "code")
+        return 0;
+    else if(str == "date")
+        return 1;
+    else if(str == "maximum")
+        return 2;
+    else if(str == "remain")
+        return 3;
+}
+
+bool cmp(vector<int> a, vector<int> b)
+{
+    return a[sortBy] < b[sortBy];
+}
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    vector<vector<int>> answer;
+    
+    sortBy = GetIndexOfValue(sort_by);
+    extBy = GetIndexOfValue(ext);
+    
+    for(int i=0; i<data.size(); i++){
+        if(data[i][extBy] < val_ext)
+            answer.push_back(data[i]);
+    }
+    
+    sort(answer.begin(), answer.end(), cmp);
+    return answer;
+}
