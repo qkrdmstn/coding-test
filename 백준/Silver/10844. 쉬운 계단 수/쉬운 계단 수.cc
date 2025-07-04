f46b320354500a90fe771660696babcaba6e69d1
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-04 15:38:53 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,32 +1,33 @@
 #include <iostream>
 using namespace std;
 
+typedef long long ll;
+const int mod = 1'000'000'000;
+
 int n;
-//d[i][k] = 길이가 i일 때, 마지막 수가 k일 경우의 계단 수의 개수
-//d[i][k] = d[i-1][k-1] + d[i-1][k+1] 이고, k가 0과 9일 때 예외 처리를 진행
-long long d[102][10];
+ll d[102][10];
 
 int main(void)
 {
-	cin.tie(0);
 	ios::sync_with_stdio(0);
+	cin.tie(0);
 
 	cin >> n;
 	for (int i = 1; i <= 9; i++)
 		d[1][i] = 1;
-
+	
 	for (int i = 2; i <= n; i++) {
-		for (int k = 0; k <= 9; k++) {
-			if (k != 0) d[i][k] += d[i - 1][k - 1];
-			if (k != 9) d[i][k] += d[i - 1][k + 1];
-			d[i][k] %= 1000000000;
+		for (int j = 0; j <= 9; j++) {
+			if (j != 0) d[i][j] += d[i - 1][j - 1];
+			if (j != 9) d[i][j] += d[i - 1][j + 1];
+			d[i][j] %= mod;
 		}
 	}
 
-	long long ans = 0;
+	ll ans = 0;
 	for (int i = 0; i <= 9; i++)
 		ans += d[n][i];
-	ans %= 1000000000;
+	ans %= mod;
 	cout << ans;
 	return 0;
 }
