1eb7cfccf9b02b30e03a7b1ece4e1c83f67fa183
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-09 23:26:19 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -1,43 +1,53 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 int n;
-int a[100005];
+int arr[100005];
+
+int LowerBound(int num)
+{
+	int st = 0;
+	int ed = n - 1;
+	while (st < ed) {
+		int mid = (st + ed) / 2;
+		if (arr[mid] < num) st = mid + 1;
+		else ed = mid;
+	}
+
+	return st;
+}
 
 int main(void)
 {
 	ios::sync_with_stdio(0);
-	cin.tie();
+	cin.tie(0);
 
 	cin >> n;
 	for (int i = 0; i < n; i++)
-		cin >> a[i];
+		cin >> arr[i];
 
-	int ans1 = 1e9;
+	int ans1 = 1e9; 
 	int ans2 = 1e9;
 	for (int i = 0; i < n; i++) {
-		int l = lower_bound(a, a + n, -a[i]) - a;
-		//index 범위 확인 && 자기 자신인지 확인 && 최소값 확인
-		if (l + 1 < n && l + 1 != i && abs(a[i] + a[l + 1]) < abs(ans1 + ans2)) {
-			ans1 = a[i];
-			ans2 = a[l+1];
+		int l = LowerBound(-arr[i]);
+
+		if (l - 1 != i && l - 1 >= 0 && abs(arr[i] + arr[l - 1]) < abs(ans1 + ans2)) {
+			ans1 = arr[i];
+			ans2 = arr[l - 1];
 		}
-		if (l < n && l != i && abs(a[i] + a[l]) < abs(ans1 + ans2)) {
-			ans1 = a[i];
-			ans2 = a[l];
+
+		if (l != i && abs(arr[i] + arr[l]) < abs(ans1 + ans2)) {
+			ans1 = arr[i];
+			ans2 = arr[l];
 		}
-		if (l - 1 >= 0 && l - 1 != i && abs(a[i] + a[l - 1]) < abs(ans1 + ans2)) {
-			ans1 = a[i];
-			ans2 = a[l - 1];
-		}			
+
+		if (l + 1 != i && l + 1 < n && abs(arr[i] + arr[l + 1]) < abs(ans1 + ans2)) {
+			ans1 = arr[i];
+			ans2 = arr[l + 1];
+		}
 	}
+
 	if (ans1 > ans2) swap(ans1, ans2);
 	cout << ans1 << ' ' << ans2;
 	return 0;
-
-	/*
-	lower_bound: key 값과 같거나 큰 값이 배열에서 처음으로 등장하는 위치
-	upper_bound: key 값을 초과하는 값이 배열에서 처음으로 등장하는 위치
-	*/
 }
