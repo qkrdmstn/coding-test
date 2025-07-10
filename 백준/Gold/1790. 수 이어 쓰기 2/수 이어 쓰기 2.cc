14a5243ecc94190cf1e27cccd777ea6fabd62c70
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-10 15:18:26 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
@@ -3,43 +3,33 @@
 using namespace std;
 
 typedef long long ll;
+ll n, k;
 
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-    ll n, k;
-    cin >> n >> k;
+	cin >> n >> k;
 
-    ll start = 1;
-    ll digit = 1;
+	ll digit = 1;
+	ll start = 1;
+	while (true) {
+		ll num = 9 * start;
+		ll totalDigit = num * digit;
+		if (k <= totalDigit) break;
+		k -= totalDigit;
+		start *= 10;
+		digit++;
+	}
 
-    /*
-    1-9는 9개 -> 9개 * 1자리 = 9
-    10-99는 90개 => 180자리
-    100-999는 900개 => 2700자리
-    */
+	ll num = start + (k - 1) / digit;
+	if (num > n) {
+		cout << -1;
+		return 0;
+	}
 
-    //k번째 수가 어느 범위에 있는지 확인
-    while (true) {
-        ll numbers = 9 * start;
-        ll totalDigit = numbers * digit;
-        if (k <= totalDigit) break;
-        k -= totalDigit;
-        start *= 10;
-        digit++;
-    }
-
-    //k번째 자리에 있는 수를 변환
-    ll num = start + (k - 1) / digit;
-    if (num > n) {
-        cout << -1;
-        return 0;
-    }
-
-    //num을 string으로 바꿔 앞에서 k번째 수 찾기
-    string s = to_string(num);
-    cout << s[(k - 1) % digit];
-
-    return 0;
+	string str = to_string(num);
+	cout << str[(k - 1) % digit];
+	return 0;
 }
