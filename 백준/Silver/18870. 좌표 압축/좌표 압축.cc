67426d6b68dca0c70e4f0bc1b67f98e2aa42926f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2025-07-10 15:22:02 +0900

    [Silver II] Title: 좌표 압축, Time: 408 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,53 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 int n;
-int distinctArrLen = 0;
-int a[1000005];
-int sortedArr[1000005];
-int distinctArr[1000005];
-
-int find(int target)
-{
-	int start = 0;
-	int end = distinctArrLen;
-
-	while (start <= end) {
-		int mid = (start + end) / 2;
-		if (distinctArr[mid] == target)
-			return mid;
-		else if (target < distinctArr[mid])
-			end = mid - 1;
-		else
-			start = mid + 1;
-	}
-	return -1;
-}
-
+int arr[1000005];
 int main(void)
 {
-	cin.tie(0);
 	ios::sync_with_stdio(0);
-	
+	cin.tie(0);
+
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		sortedArr[i] = a[i];
-	}
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
 
-	//정렬 및 중복 제거
-	sort(sortedArr, sortedArr + n);
-	
-	for (int i= 0; i < n; i++) {
-		if (i != 0 && sortedArr[i - 1] == sortedArr[i]) continue;
-		distinctArr[distinctArrLen] = sortedArr[i];
-		distinctArrLen++;
-	}
+	vector<int> v(arr, arr + n);
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()), v.end());
 
-	for (int i = 0; i < n; i++) {
-		cout << find(a[i]) << ' ';
-	}
-
+	for (int i = 0; i < n; i++)
+		arr[i] = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << ' ';
 	return 0;
 }
