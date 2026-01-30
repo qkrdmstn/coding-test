e053a6bf8855c69bd5c5c7b4062fd5576722ed5b
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-01-30 23:11:38 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -1,17 +1,17 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
 int solution(int n, int w, int num) {
     int answer = 0;
-    
-    int diff = (w - ((num-1) % w) - 1) * 2 + 1;
-    while(num <= n){
-        answer++;
-        if(answer % 2 == 1)
-            num += diff;
+    while(num <= n)
+    {
+        if(num % w == 0)
+            num++;
         else
-            num += 2*w-diff;
+            num += (w - (num % w)) * 2 + 1;
+        answer++;
     }
     return answer;
 }
