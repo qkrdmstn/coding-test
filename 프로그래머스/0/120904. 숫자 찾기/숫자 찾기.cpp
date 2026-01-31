447503a70dd4388f0aa6fb2c62dd06aa8e0647ba
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-01-31 14:38:38 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = -1;
+    string str = to_string(num);
+    for(int i=0; i<str.length(); i++){
+        if(str[i] - '0' == k){
+            answer = i+1;
+            break;
+        }
+    }
+    return answer;
+}
