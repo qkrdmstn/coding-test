ececce4868f0e3d855195b71f737d5db18314957
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-01-31 12:47:02 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int solution(string str1, string str2) {
     int answer = 2;
-    for(int i=0; i<str1.length(); i++){
+    for(int i=0; i < str1.length(); i++){
         int cnt = 0;
         for(int j=0; j<str2.length(); j++){
             if(str1[i] == str2[j]){
