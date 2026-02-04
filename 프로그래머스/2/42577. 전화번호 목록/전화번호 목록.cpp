d3a95ed6a5271d8117e9ef37c001eac44a28eda4
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-04 16:08:37 +0900

    [level 2] Title: 전화번호 목록, Time: 1466.32 ms, Memory: 238 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,26 +1,26 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <iostream>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    bool answer = true;
-    unordered_map<string, int> partOfPhoneMap;
-    
+    //모든 전화번호의 부분 집합을 키 값으로 hash에 count 합니다.
+    unordered_map<string, int> prefixCount;
     for(int i=0; i<phone_book.size(); i++)
     {
         string s = "";
         for(int j=0; j<phone_book[i].length(); j++)
         {
             s += phone_book[i][j];
-            partOfPhoneMap[s]++;
+            prefixCount[s]++;
         }
     }
     
+    //온전한 문자열을 키값으로 hash 테이블 데이터에 접근합니다.
+    //이때, 자기 자신을 포함하므로, count가 2 이상이어야 합니다.
      for(int i=0; i<phone_book.size(); i++)
      {
-         if(partOfPhoneMap[phone_book[i]] >= 2)
+         if(prefixCount[phone_book[i]] >= 2)
              return false;
      }
     return true;
