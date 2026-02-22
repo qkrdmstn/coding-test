5bf61405c30619dba56d454a2ce578d283a5ed4f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-22 17:01:56 +0900

    [level 3] Title: 등굣길, Time: 0.07 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/3/42898. 등굣길/등굣길.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int solution(int m, int n, vector<vector<int>> puddles) {
     int answer = 0;
+    //dp[i][j]: (i,j)에 도착하는 경우의 수
     vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
     vector<vector<bool>> isPuddle(m + 1, vector<bool>(n + 1,false));
 
+    //물에 잠긴 위치를 bool로 표시합니다.
     for(auto p: puddles)
     {
         isPuddle[p[0]][p[1]] = true;
@@ -18,7 +20,12 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     {
         for(int j=1; j<=n; j++)
         {
+            //물에 잠겼거나 시작 위치는 건너뜁니다.
             if(isPuddle[i][j] || (i==1 && j == 1)) continue;
+            
+            //dp[i][j] = dp[i-1][j] + dp[i][j-1]입니다.
+            //즉, 윗칸과 왼쪽칸에 도달하는 경우의 수를 더합니다.
+            //이때 범위에 대한 예외처리를 진행합니다
             int up = 0, left = 0;
             if(i-1 >= 1)
                 up = dp[i-1][j];
