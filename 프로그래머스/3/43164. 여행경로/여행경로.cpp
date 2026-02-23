b3055177e5b7e8bfa62bf9dde87ce4ef0bc7826d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-23 23:10:10 +0900

    [level 3] Title: 여행경로, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -3,34 +3,29 @@
 #include <algorithm>
 using namespace std;
 
-bool func(string cur, int n, vector<bool>& used, vector<vector<string>>& tickets, vector<string>& arr, vector<vector<string>>& answers)
+bool DFS(string curCity, vector<vector<string>>& tickets, vector<bool>& used, vector<string>& answer)
 {
-    if(arr.size() == n + 1)
+    answer.push_back(curCity);
+    
+    //모든 티켓을 사용한 경우 올바른 해에 도달한 것입니다.
+    if(answer.size() == tickets.size() + 1) return true;
+    for(int i=0; i<tickets.size(); i++)
     {
-        answers.push_back(arr);
-        return true;
-    }
-    for(int i=0; i<n; i++)
-    {
-        if(used[i] || tickets[i][0] != cur) continue;
+        if(tickets[i][0] != curCity || used[i]) continue;
         used[i] = true;
-        arr.push_back(tickets[i][1]);
-        if(func(tickets[i][1], n, used, tickets, arr, answers)) return true;
-        arr.pop_back();
+        if(DFS(tickets[i][1], tickets, used, answer)) return true;
         used[i] = false;
     }
+    //현재 경우 모든 티켓을 사용하지 못했다면 이전 상태로 복귀합니다.
+    answer.pop_back();
     return false;
 }
 
 vector<string> solution(vector<vector<string>> tickets) {
     vector<string> answer;
-    int n = tickets.size();
+    vector<bool> used(tickets.size(), false);
     
-    vector<bool> used(n);
-    vector<vector<string>> answers;
     sort(tickets.begin(), tickets.end());
-    
-    answer.push_back("ICN");
-    func("ICN", n, used, tickets, answer, answers);
+    DFS("ICN", tickets, used, answer);
     return answer;
 }
