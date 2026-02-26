b5722ad0c2246682c5c94c05c157963f8c9d0423
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-26 22:37:29 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -8,21 +8,21 @@ int main(void)
 	int n;
 	cin >> n;
 	vector<int> arr(n + 1, 0);
-	
-	//d[i][0]: i-1번째를 밟지 않고 i번째에 도착하는 최댓값
-	//d[i][1]: i-1번째를 밟고 i번째에 도착하는 최댓값
-	vector<vector<int>> d(n + 1, vector<int>(2, 0));
+	//d[i]: i번째 계단을 밟지 않을 때 놓친 점수가 최솟값.
+	vector<int> d(n + 1, 0);
 
-	for (int i = 1; i <= n; i++) 
-		cin >> arr[i];
-
-	d[1][0] = d[1][1] = arr[1];
-	for (int i = 2; i <= n; i++)
+	int total = 0;
+	for (int i = 1; i <= n; i++)
 	{
-		//i-1번째를 밟으려면 i-1번째를 밟았을 때 i-2번째를 밟지 않아야 함.
-		d[i][1] = d[i-1][0] + arr[i];
-		//i-1번째를 밟지 않는다면 i-2번째는 밟아도/밟지 않아도 무관함
-		d[i][0] = max(d[i-2][1], d[i-2][0]) + arr[i];
+		cin >> arr[i];
+		total += arr[i];
 	}
-	cout << max(d[n][0], d[n][1]);
+
+	d[1] = arr[1];
+	d[2] = arr[2];
+	//i번째를 밟지 않으려면 i-1번째를 무조건 밟아야 하고, i-1번째를 무조건 밟으려면 i-2와 i-3번째 둘 중 하나는 밟지 않아야 한다.
+	for(int i=3; i<=n; i++)
+		d[i] = min(d[i-2], d[i-3]) + arr[i];
+	//마지막 계단을 무조건 밟으려면 n-1 번째 또는 n-2번째를 밟지 않아야 한다.
+	cout << total - min(d[n-1], d[n-2]);
 }
