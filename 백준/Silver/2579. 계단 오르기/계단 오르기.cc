52fc0e5c540b3443c6aa320061d64bb39e51c095
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-26 22:27:01 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,28 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int n, total;
-int stair[301];
-int table[301];
-
+const int INF = 0x3f3f3f3f;
 int main(void)
 {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
-
+	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		cin >> stair[i];
-		total += stair[i];
-	}
+	vector<int> arr(n + 1, 0);
 	
-	table[1] = stair[1];
-	table[2] = stair[2];
-	table[3] = stair[3];
+	//d[i][0]: i-1번째를 밟지 않고 i번째에 도착하는 최댓값
+	//d[i][1]: i-1번째를 밟고 i번째에 도착하는 최댓값
+	vector<vector<int>> d(n + 1, vector<int>(2, 0));
 
-	for (int i = 4; i <= n; i++)
-		table[i] = min(table[i-2], table[i-3]) + stair[i];
+	for (int i = 1; i <= n; i++) 
+		cin >> arr[i];
 
-	cout << total - min(table[n - 1], table[n - 2]);
-	return 0;
+	d[1][0] = d[1][1] = arr[1];
+	for (int i = 2; i <= n; i++)
+	{
+		//i-1번째를 밟으려면 i-1번째를 밟았을 때 i-2번째를 밟지 않아야 함.
+		d[i][1] = d[i-1][0] + arr[i];
+		//i-1번째를 밟지 않는다면 i-2번째는 밟아도/밟지 않아도 무관함
+		d[i][0] = max(d[i-2][1], d[i-2][0]) + arr[i];
+	}
+	cout << max(d[n][0], d[n][1]);
 }
