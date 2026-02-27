edf6388d28f32788f1a167559e0c146872dc10ee
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-02-27 21:58:17 +0900

    [Silver I] Title: IOIOI, Time: 32 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -6,25 +6,20 @@ using namespace std;
 
 int main(void)
 {
-	int n, m;
+	int n, m, ans = 0;
 	string str;
 	cin >> n >> m >> str;
 
-	int len = 2*n + 1;
-	int ans = 0;
-	for (int st = 0; st <= m - len; st++)
+	for (int s = 0; s < m; s++)
 	{
-		if(str[st] == 'O') continue;
-		int ed = st;
-		while (str[ed] != str[ed + 1])
+		if (str[s] == 'O') continue;
+		int cnt = 0;
+		while (s + 2 < m && str[s + 1] == 'O' && str[s + 2] == 'I')
 		{
-			ed++;
-			if(ed-st+1 == len) break;
-		}
-		if (ed - st + 1 == len)
-		{
-			ans++;
-			//st = ed - 1;
+			s+=2;
+			cnt++;
+			if(cnt >= n)
+				ans++;
 		}
 	}
 	cout << ans;
