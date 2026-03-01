5b65af58c4783518248674a42d72a32b9f6bcf9a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-01 16:33:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -2,38 +2,38 @@
 #include <vector>
 using namespace std;
 
-int n, k;
-int weights[105];
-int values[105];
-
-void Input()
-{
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++)
-    {
-        cin >> weights[i] >> values[i];
-    }
-}
-
 int main(void)
 {
-    Input();
-    //dp[i][j]: 물품이 i번째까지 있고, j만큼의 무게 제한일 때 가치의 최댓값.
-    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= k; j++)
-        {
-            int wi = weights[i];
-            int vi = values[i];
+	int n, k;
+	cin >> n >> k;
 
-            //현재 물품이 무게 제한을 넘는다면 담지 못한다.
-            if (wi > j)
-                dp[i][j] = dp[i - 1][j];
-            //무게 제한을 넘지 않는다면 물건을 담거나 담지 않는 것 중 최댓값을 선택.
-            else
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wi] + vi);
-        }
-    }
-    cout << dp[n][k] << endl;
+	//d[i][j]: 아이템이 i개, 최대 무게가 j일 때 최대 가치
+	vector<vector<int>> d(n + 1, vector<int>(k + 1, 0));
+	vector<pair<int, int>> items(n + 1, { 0,0 }); //{무게, 가치}
+	for (int i = 1; i <= n; i++)
+	{
+		int w, v;
+		cin >> w >> v;
+		items[i] = {w, v};
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		int weight = items[i].first;
+		int value = items[i].second;
+
+		for (int j = k; j >= 0; j--)
+		{
+			//최대 무게보다 아이템 무게가 무거우면 담지 않고 이전 상태 가져오기
+			if(j < weight) 
+				d[i][j] = d[i-1][j];
+			//담을 수 있으면, 1. 현재 아이템을 담고 가치를 높이는 것과
+			//2. 담지 않고 이전 상태 가져오는 것 중 더 큰 것을 선택
+			else
+				d[i][j] = max(d[i-1][j], d[i-1][j-weight] + value);
+		}
+	}
+
+	cout << d[n][k];
+	return 0;
 }
