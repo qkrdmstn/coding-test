b76fd11b05d2b119d775fa984d3566feb2e3513b
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-03 19:48:27 +0900

    [Gold III] Title: 웜홀, Time: 700 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Gold/1865. 웜홀/웜홀.cc
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+const int INF = 0x3f3f3f3f;
+int main(void)
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m, w;
+		cin >> n >> m >> w;
+		vector<vector<int>> dist(n + 1, vector<int>(n + 1, INF));
+		for (int i = 1; i <= n; i++)
+			dist[i][i] = 0;
+		for (int i = 0; i < m; i++)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+			dist[a][b] = min(dist[a][b], c);
+			dist[b][a] = min(dist[b][a], c);
+		}
+		for (int i = 0; i < w; i++)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+			dist[a][b] = min(dist[a][b], -c);
+		}
+
+
+		for (int k = 1; k <= n; k++)
+		{
+			for (int i = 1; i <= n; i++)
+			{
+				for (int j = 1; j <= n; j++)
+				{
+					if (dist[i][k] + dist[k][j] < dist[i][j])
+						dist[i][j] = dist[i][k] + dist[k][j];
+				}
+			}
+		}
+
+		bool ans = false;
+		for (int k = 1; k <= n; k++)
+		{
+			for (int i = 1; i <= n; i++)
+			{
+				if (dist[i][k] + dist[k][i] < 0)
+				{
+					ans = true;
+					break;
+				}
+			}
+			if (ans) break;
+		}
+		if (ans) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	return 0;
+}
