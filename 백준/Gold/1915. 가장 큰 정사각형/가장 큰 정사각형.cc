4b0f51dfc233e7808675fae96ccbc8666c08aee2
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-07 16:28:08 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 40 ms, Memory: 7832 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,43 +1,45 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <vector>
 using namespace std;
-
-int n, m;
-string a;
-int board[1005][1005];
-int d[1005][1005];
 
 int main(void)
 {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
-
+	int n, m;
 	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		cin >> a;
-		for (int j = 0; j < m; j++) {
-			board[i + 1][j + 1] = a[j] - '0';
+
+	vector<string> s(n);
+	for(int i=0; i<n; i++)
+		cin >> s[i];
+
+	//d[i][j]: i,j를 오른쪽하단 꼭짓점으로 하는 가장 큰 정사각형의 한 변의 길이
+	vector<vector<int>> d(n, vector<int>(m, 0));
+
+	//위, 왼쪽 가장자리는 최대 1의 크기
+	for(int i=0; i<n; i++)
+		d[i][0] = s[i][0] - '0';
+	for (int i = 0; i < m; i++)
+		d[0][i] = s[0][i] - '0';
+
+	//i,j는 위, 왼쪽, 왼쪽 위의 정사각형 크기 중 가장 작은 것 + 1
+	for (int i = 1; i < n; i++)
+	{
+		for (int j = 1; j < m; j++)
+		{
+			if (s[i][j] == '1')
+				d[i][j] = min(d[i-1][j], min(d[i][j-1], d[i-1][j-1])) + 1;
+			else
+				d[i][j] = 0;
 		}
 	}
 
+	//가장 변의 길이가 긴 정사각형의 넓이 계산
 	int ans = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			if (board[i][j]) {
-				d[i][j] = min({ d[i - 1][j], d[i - 1][j - 1], d[i][j - 1] }) + 1;
-				ans = max(ans, d[i][j]);
-			}
-		}
+	for (int i = 0; i < n; i++)
+	{
+		for(int j=0; j<m; j++)
+			ans = max(ans, d[i][j]);
 	}
 	cout << ans * ans;
-
-	/*
-	설명:
-	d[i][j]: i*j 크기의 board일 때, 정사각형의 최대 크기
-	누적하면 현재 위치에서 사각형이 만들어지는지 확인 X,
-	-> d[i][j] = 0일 경우 0으로 냅둠
-	-> 1일 경우에 최소값 + 1로 해주기
-	*/
 	return 0;
 }
