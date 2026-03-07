a6468032e819ebc4cb0e949a5f217d12e299aa3d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-07 15:54:51 +0900

    [Gold IV] Title: 팰린드롬?, Time: 216 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,45 +1,47 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int n, t;
-int arr[2005];
-int d[2005][2005];
 
 int main(void)
 {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
 
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
 	cin >> n;
-	for (int i = 1; i <= n; i++)
+	vector<int> arr(n+1);
+	vector<vector<int>> d(n+1, vector<int>(n + 1, 0));
+	for(int i=1; i<=n; i++)
 		cin >> arr[i];
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 1; i <= n; i++)
+	{
+		//길이가 1인 수열은 무조건 팰린드롬
 		d[i][i] = 1;
-		if (arr[i - 1] == arr[i]) d[i - 1][i] = 1;
+		//길이가 2인 수열은 앞뒤가 같으면 팰린드롬
+		if(i+1 <= n && arr[i] == arr[i+1])
+			d[i][i+1] = 1;
 	}
 
-	for (int diff = 2; diff < n; diff++) {
-		for (int i = 0; i <= n - diff; i++) {
-			int s = i; int e = i + diff;
-			if (arr[s] == arr[e] && d[s + 1][e - 1]) d[s][e] = 1;
+	//길이가 3인 팰린드롬은 시작과 끝이 같고 그 사이 수열이 팰린드롬이면 팰린드롬이다.
+	for (int diff = 2; diff < n; diff++)
+	{
+		for (int i = 1; i <= n - diff; i++)
+		{
+			int j=i+diff;
+			if(arr[i]==arr[j] && d[i+1][j-1] == 1)
+				d[i][j] = 1;
 		}
 	}
 
-	cin >> t;
-	while (t--) {
+	int m;
+	cin >> m;
+	while (m--)
+	{
 		int s, e;
 		cin >> s >> e;
-		cout << d[s][e] << '\n';
+		cout << d[s][e]<<"\n";
 	}
-
-	/*
-	설명:
-	d[s][e]: s번째 수부터 e번째 수가 팰린드롬 수인지 나타냄
-	길이가 1인 수는 무조건 팰린드롬 => d[i][i] = 1;
-	길이가 2인 수는 직전과 같으면 팰린드롬 => arr[i-1] == arr[i] : d[i-1][i] = 1;
-	길이가 n인 수는 시작과 끝이 같고, 그 사이에 있는 수가 팰린드롬이면 팰린드롬
-	-> arr[s] == arr[e] && d[s + 1][e - 1] : d[s][e] = 1;
-	*/
 	return 0;
 }
