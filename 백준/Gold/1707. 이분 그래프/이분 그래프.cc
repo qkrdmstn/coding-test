64bc2dde1a4d6e2d9c64275ddd9a425aaf3e4670
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-07 16:53:27 +0900

    [Gold IV] Title: 이분 그래프, Time: 724 ms, Memory: 5792 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,69 +1,58 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 using namespace std;
-
-int k, v, e;
-vector<int> adj[20'005];
-int group[20'005];
-
-/*
-시작 노드의그룹을 0으로 지정하고, 인접 노드를 (각 노드 그룹 + 1) % 2로 지정
--> 인접 노드는 0과 1을 번갈아가며 갖게 됨
-이때, 다음 노드가 현재 노드와 같은 지점을 만나면 false를 반환
-무사히 모든 노드를 순회하면 true를 반환
-*/
-bool solve()
-{
-	fill(group, group + v + 1, -1);
-
-	for (int i = 1; i <= v; i++) {
-		if (group[i] != -1) continue;
-
-		queue<int> q;
-		q.push(i);
-		group[i] = 0;
-
-		while (!q.empty()) {
-			int cur = q.front();
-			q.pop();
-
-			for (auto nxt : adj[cur]) {
-				if (group[nxt] != -1) {
-					if (group[nxt] == group[cur])
-						return false;
-					else
-						continue;
-				}
-
-				group[nxt] = (group[cur] + 1) % 2;
-				q.push(nxt);
-			}
-		}
-	}
-	return true;
-}
 
 int main(void)
 {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
-
+	int k;
 	cin >> k;
-	while (k--) {
+
+	while (k--)
+	{
+		int v, e;
 		cin >> v >> e;
+		vector<vector<int>> adj(v+1, vector<int>());
 
-		for (int i = 1; i <= v; i++)
-			adj[i].clear();
-
-		for (int i = 0; i < e; i++) {
-			int n1, n2;
-			cin >> n1 >> n2;
-			adj[n1].push_back(n2);
-			adj[n2].push_back(n1);
+		//인접 리스트 생성 (양방향)
+		for (int i = 0; i < e; i++)
+		{
+			int a, b;
+			cin >> a >> b;
+			adj[a].push_back(b);
+			adj[b].push_back(a);
 		}
 
-		if (solve()) cout << "YES\n";
+		//BFS를 활용해 인접 노드에 다른 색깔을 칠합니다.
+		//다음 노드가 이미 나와 같은 그룹에 속한다면 이분 그래프가 아닙니다.
+		bool isPossible = true;
+		queue<int> q;
+		vector<int> color(v+1, -1);
+		//그래프가 끊긴 경우를 고려해 모든 정점에서 시작합니다.
+		for (int i = 1; i <= v; i++)
+		{
+			//이미 방문한 정점은 건너뜁니다.
+			if(color[i] >= 0) continue;
+			q.push(i);
+			color[i] = 0;
+			while (!q.empty())
+			{
+				int cur = q.front();
+				q.pop();
+
+				if(!isPossible) break;
+				for (auto nxt : adj[cur])
+				{
+					//다음 노드가 이미 나와 같은 색을 가진다면 인접 노드와 같은 집합입니다.
+					if(color[nxt] == color[cur])
+						isPossible = false;
+					if(color[nxt] >= 0) continue;
+					q.push(nxt);
+					color[nxt] = (color[cur] + 1) % 2;
+				}
+			}
+		}
+		if(isPossible) cout << "YES\n";
 		else cout << "NO\n";
 	}
 	return 0;
