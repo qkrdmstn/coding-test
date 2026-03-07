ef8a87d659d6ac0403224dd15fa1c041c2de1ac4
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-07 16:23:14 +0900

    [level 2] Title: 완전범죄, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -5,29 +5,28 @@ using namespace std;
 
 int solution(vector<vector<int>> info, int n, int m) {
     int answer = 0;
-    int itemSize = info.size();
+    int s = info.size();
     
-    //d[i][j]: 아이템이 i개, B의 사용 가능 흔적이 j개일 때 a의 최소 누적 흔적
-    vector<vector<int>> d(itemSize + 1, vector<int>(m, 0));
+    //d[i][j]: 아이템 개수가 i개, 남기는 것이 가능한 B의 흔적 개수가 j개일 때 
+    //a의 최소 흔적
+    vector<vector<int>> d(s+1, vector<int>(m+1, 0));
+    for(int i=1; i<=s; i++)
+        d[i][0] = d[i-1][0] + info[i-1][0]; //누적, a[i-1]은 i번째 아이템을 의미
     
-    //B의 사용 가능 흔적이 0이면 a의 흔적 남기기
-    for(int i=1; i<=itemSize; i++)
-        d[i][0] = d[i-1][0] + info[i-1][0];
-    
-    for(int i=1; i<= itemSize; i++)
+    for(int i=1; i<=s; i++)
     {
-        for(int j=1; j< m; j++)
+        int curItemIdx = i-1;
+        for(int j=1; j<=m; j++)
         {
-            //B의 흔적이 초과라면 a의 흔적을 남겨야 한다.
-            if(j < info[i-1][1])
-                d[i][j] = d[i-1][j] + info[i-1][0];
-            //a의 흔적을 남길지 b의 흔적일 남길지 최소인 것을 선택
+            if(j < info[curItemIdx][1])
+                d[i][j] = d[i-1][j] + info[curItemIdx][0]; //A의 흔적 남기기
+            //이번 아이템을 a가 훔치는 것과 b가 훔치는 것 중 a의 흔적이 더 적게 남는 것을 선택
             else
-                d[i][j] = min(d[i-1][j - info[i-1][1]], d[i-1][j] + info[i-1][0]);
+                d[i][j] = min(d[i-1][j-info[curItemIdx][1]], d[i-1][j] + info[curItemIdx][0]);
         }
     }
-    
-    if(d[itemSize][m-1] >= n) answer = -1;
-    else answer= d[itemSize][m-1];
+    //answer = 아이템이 s개, B의 흔적이 m-1개까지 가능한 경우
+    answer = d[s][m-1];
+    if(answer >= n) answer = -1;
     return answer;
 }
