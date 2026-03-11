d285e177176339c1404c2df2345c9bec2d28dc9f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-11 14:54:55 +0900

    [Gold IV] Title: N-Queen, Time: 2352 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,39 +1,43 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int n, ans;
-bool isUsed[16];
-bool isUsedRightDown[30];
-bool isUsedRightUp[30];
-
-void func(int queenCnt)
+void DFS(int cur, int cnt, vector<bool>& used1, vector<bool>& used2, vector<bool>& used3, int n, int& ans)
 {
-	if (queenCnt == n) {
-		ans++;
+	if (cur == n)
+	{
+		if(cnt==n)
+			ans++;
 		return;
 	}
-	else {
-		for (int i = 0; i < n; i++) {
-			if (isUsed[i] || isUsedRightUp[queenCnt + i] || isUsedRightDown[queenCnt - i + n - 1])
-				continue;
-			isUsed[i] = true;
-			isUsedRightUp[queenCnt + i] = true;
-			isUsedRightDown[queenCnt - i + n - 1] = true;
-			func(queenCnt + 1);
-			isUsed[i] = false;
-			isUsedRightUp[queenCnt + i] = false;
-			isUsedRightDown[queenCnt - i + n - 1] = false;
-		}
+
+	//행 순회
+	for (int i = 0; i < n; i++)
+	{
+		int idx2 = cur+i;
+		int idx3 = cur - i + (n-1);
+		if(used1[i] || used2[idx2] || used3[idx3]) continue;
+		used1[i] = true;
+		used2[idx2] = true;
+		used3[idx3] = true;
+		DFS(cur+1, cnt+1, used1, used2, used3, n, ans);
+		used1[i] = false;
+		used2[idx2] = false;
+		used3[idx3] = false;
 	}
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
 
+	int n;
 	cin >> n;
-	func(0);
+	vector<bool> used1(n,false); //행 사용 여부
+	vector<bool> used2(2*n-1,false); //오른쪽 위 방향 대각선
+	vector<bool> used3(2*n-1,false); //오른쪽 아래 방향 대각선
+
+	int ans = 0;
+	DFS(0, 0, used1, used2,used3, n, ans);
 	cout << ans;
 	return 0;
 }
