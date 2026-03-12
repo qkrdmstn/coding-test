83ced3c7a84f8ba36cd79622a44d234a0a79c869
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-12 14:24:06 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.cc
+++ 백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -4,82 +4,86 @@
 #include <algorithm>
 using namespace std;
 
-//상좌우하 순으로 방문
-int dx[4] = {-1,0,0,1};
-int dy[4] = {0,-1,1,0};
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
-const int INF = 0x3f3f3f3f;
-int BFS(int curSize, pair<int, int>& curPos, vector<vector<int>>& board, int n)
+int EatFish(int& x, int& y, int size, vector<vector<int>>& board, int n)
 {
-	queue<pair<int ,int>> q;
+	queue<pair<int, int>> q;
 	vector<vector<int>> dist(n, vector<int>(n, -1));
+	vector<pair<int, int>> fish;
 
-	q.push({curPos.first, curPos.second});
-	dist[curPos.first][curPos.second] = 0;
-	int minDist = INF;
-	vector<pair<int ,int>> eatFish;
-
+	int minDist = 0x3f3f3f3f;
+	q.push({ x, y });
+	dist[x][y] = 0;
 	while (!q.empty())
 	{
 		auto cur = q.front(); q.pop();
-
-		if(minDist < dist[cur.first][cur.second]) break;
-		if (board[cur.first][cur.second] > 0 && board[cur.first][cur.second] <= 6 && board[cur.first][cur.second] < curSize)
+		if (dist[cur.first][cur.second] > minDist) break;
+		if (board[cur.first][cur.second] > 0 && board[cur.first][cur.second] < size)
 		{
-			minDist = dist[cur.first][cur.second];
-			eatFish.push_back(cur);
+			minDist = min(minDist, dist[cur.first][cur.second]);
+			fish.push_back({ cur.first, cur.second });
 		}
+
 		for (int i = 0; i < 4; i++)
 		{
 			int nx = cur.first + dx[i];
 			int ny = cur.second + dy[i];
-
-			if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-			if(dist[nx][ny] >= 0 || board[nx][ny] > curSize) continue; //나보다 크면 못 지나감
-
-			q.push({nx, ny});
+			if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+			if (board[nx][ny] > size || dist[nx][ny] >= 0) continue;
+			q.push({ nx, ny });
 			dist[nx][ny] = dist[cur.first][cur.second] + 1;
 		}
 	}
+	if (fish.empty()) return -1;
 
-	if(minDist == INF) return -1;
-	sort(eatFish.begin(), eatFish.end());
-	board[curPos.first][curPos.second] = 0;
-	board[eatFish[0].first][eatFish[0].second] = 9;
-	curPos = eatFish[0];
+	//가장 위쪽, 왼쪽인 물고기 순으로 정렬합니다.
+	sort(fish.begin(), fish.end());
+
+	//물고기를 먹습니다.
+	x = fish[0].first, y = fish[0].second;
+	board[x][y] = 0;
 
 	return minDist;
 }
 
 int main(void)
 {
-	int n;
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	int n, x, y;
 	cin >> n;
 
-	pair<int, int> curPos;
 	vector<vector<int>> board(n, vector<int>(n));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			cin >> board[i][j];
-			if(board[i][j] == 9)
-				curPos = {i, j};
+			if (board[i][j] == 9)
+			{
+				x = i;
+				y = j;
+				board[i][j] = 0;
+			}
 		}
 	}
 
-	int curSize = 2;
+	int size = 2;
 	int cnt = 0;
 	int time = 0;
 	while (true)
 	{
-		int dist = BFS(curSize, curPos, board, n);
+
+		int dist = EatFish(x, y, size, board, n);
 		if(dist == -1) break;
-		time += dist;
+		time+=dist;
 		cnt++;
-		if (cnt == curSize)
+		if (cnt == size)
 		{
-			curSize++;
+			size++;
 			cnt = 0;
 		}
 	}
