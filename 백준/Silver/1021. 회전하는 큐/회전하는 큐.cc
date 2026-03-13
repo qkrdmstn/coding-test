6fa2ed9aa163d14ae67182822388b14a707457fd
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-13 14:16:10 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -2,45 +2,54 @@
 #include <deque>
 using namespace std;
 
-int n, m;
-deque<int> dq;
-
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+	int N, M;
+	cin >> N >> M;
 
-	cin >> n >> m;
-	for (int i = 1; i <= n; i++) {
+	deque<int> dq;
+	for(int i=1; i<=N; i++)
 		dq.push_back(i);
-	}
-
-	int cnt = 0;
-	for (int i = 0; i < m; i++) {
+	
+	int ans = 0;
+	while (M--)
+	{
 		int num;
 		cin >> num;
 
-		int index = 0;
-		for (int j = 0; j < dq.size(); j++) {
-			if (num == dq[j]) {
-				index = j;
+		int idx = 0;
+		int size = dq.size();
+		for (int i = 0; i < size; i++)
+		{
+			if (dq[i] == num)
+			{
+				idx = i + 1;
 				break;
 			}
 		}
 
-		while (dq.front() != num) {
-			if (index - 1 < dq.size() / 2) {
+		if (idx <= size / 2 + 1)
+		{
+			//왼쪽으로 회전
+			while (dq.front() != num)
+			{
 				dq.push_back(dq.front());
 				dq.pop_front();
+				ans++;
 			}
-			else {
+		}
+		else
+		{
+			//오른쪽으로 회전
+			while (dq.front() != num)
+			{
 				dq.push_front(dq.back());
 				dq.pop_back();
+				ans++;
 			}
-			cnt++;
 		}
 		dq.pop_front();
 	}
-	cout << cnt;
+	cout << ans;
 	return 0;
 }
