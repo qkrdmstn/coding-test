96083854aea406ed17408594f5a62b7c9a30d794
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-13 15:06:39 +0900

    [level 3] Title: 순위, Time: 1.58 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -5,32 +5,35 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
-    vector<vector<bool>> board(n+1, vector<bool>(n+1, false));
-    for(auto r: results)
-        board[r[0]][r[1]] = true; //r[0]가 r[1]을 이김
     
+    vector<vector<int>> dist(n+1, vector<int>(n+1, 0));
+    for(auto &r: results)
+        dist[r[0]][r[1]] = 1;
+
     for(int k=1; k<=n; k++)
     {
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=n; j++)
             {
-                if(board[i][k] && board[k][j])
-                    board[i][j] = true;
+                if(dist[i][k] == 1 && dist[k][j] == 1)
+                    dist[i][j] = 1;
             }
         }
     }
-
+    
+    vector<int> cnt(n+1, 0);
     for(int i=1; i<=n; i++)
     {
-        int count = 0;
         for(int j=1; j<=n; j++)
         {
-            if(board[i][j] || board[j][i]) //i가 j 한테 이기거나, j가 i한테 이겼다면(i의 승패가 확실한 경우.)
-                count++;
+            cnt[i] += dist[i][j];
+            cnt[j] += dist[i][j];
         }
-        if(count == n-1) //자신을 제외한 모든 사람과의 승패가 확실한 경우 순위를 확정짓는다.
-            answer++;
+    }
+    for(int i=1; i<=n; i++)
+    {
+        if(cnt[i] == n-1) answer++;
     }
     return answer;
 }
