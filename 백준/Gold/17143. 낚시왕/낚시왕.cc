251aa8e1a8d2c218bee0a3c2dde957469729a954
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-16 18:13:57 +0900

    [Gold I] Title: 낚시왕, Time: 196 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,98 +1,87 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int dr[4] = {-1,1,0,0};
-int dc[4] = {0,0,1,-1};
-struct Shark
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0,0,1,-1};
+
+struct shark
 {
-	int s; //속력
-	int d; //방향
-	int z; //크기
+	int s;
+	int d;
+	int z;
 };
 
-void MoveSharks(vector<vector<Shark>>& board, int r, int c)
+void MoveSharks(vector<vector<shark>>& board, int R, int C)
 {
-	vector<vector<Shark>> nextBoard(r, vector<Shark>(c, { 0,0,0 }));
-	for (int i = 0; i < r; i++)
+	vector<vector<shark>> nxtBoard(R, vector<shark>(C, {0,0,0}));
+	for (int i=0; i<R; i++)
 	{
-		for (int j = 0; j < c; j++)
+		for (int j = 0; j < C; j++)
 		{
-			int s = board[i][j].s;
-			int d = board[i][j].d;
-			int z = board[i][j].z;
-			if (z == 0) continue;
+			if(board[i][j].z == 0) continue;
 
-			//모듈러 연산으로 중복되는 이동거리를 줄입니다.
-			int moveCount = s;
-			if (d == 0 || d == 1) moveCount %= ((r - 1) * 2);
-			else moveCount %= ((c - 1) * 2);
+			shark& sh = board[i][j];
+			int dist = 0;
+			//수직 이동인 경우
+			if (sh.d <= 1) dist = sh.s % (2 * (R - 1));
+			//수평 이동인 경우
+			else dist = sh.s % (2 * (C - 1));
 
-			//나머지만큼 직접 이동 시뮬레이션 합니다.
-			int cr = i;
-			int cc = j;
-			while (moveCount > 0)
+			int x = i, y = j;
+			int dir = sh.d;
+			while (dist--)
 			{
-				int nr = cr + dr[d];
-				int nc = cc + dc[d];
-
-				//범위를 벗어나는 경우 방향을 전환합니다.
-				if (nr < 0 || nr >= r || nc < 0 || nc >= c)
+				int nx = x + dx[dir];
+				int ny = y + dy[dir];
+				if (nx < 0 || nx >= R || ny < 0 || ny >= C)
 				{
-					if (d == 0) d = 1;
-					else if (d == 1) d = 0;
-					else if (d == 2) d = 3;
-					else if (d == 3) d = 2;
-					nr = cr + dr[d];
-					nc = cc + dc[d];
+					if (dir == 0) dir = 1;
+					else if (dir == 1) dir = 0;
+					else if (dir == 2) dir = 3;
+					else if (dir == 3) dir = 2;
+					nx = x + dx[dir];
+					ny = y + dy[dir];
 				}
-				cr = nr;
-				cc = nc;
-				moveCount--;
+				x = nx;
+				y = ny;
+				sh.d = dir;
 			}
+			if(board[i][j].z > nxtBoard[x][y].z)
+				nxtBoard[x][y] = board[i][j];
 
-			//한 칸에 두 마리 상어가 있는 경우 더 큰 상어만 남습니다.
-			if (z > nextBoard[cr][cc].z)
-				nextBoard[cr][cc] = { s, d, z };
 		}
 	}
-	board = nextBoard;
+	board = nxtBoard;
 }
 
 int main(void)
 {
-	int r, c, m;
-	cin >> r >> c >> m;
+	int R, C, M;
+	cin >> R >> C >> M;
+	vector<vector<shark>> board(R, vector<shark>(C, {0,0,0}));
 
-	//{크기, 속력, 방향}
-	vector<vector<Shark>> board(r, vector<Shark>(c, {0,0,0}));
-	for (int i = 0; i < m; i++)
+	for (int i = 0; i < M; i++)
 	{
 		int x, y, s, d, z;
 		cin >> x >> y >> s >> d >> z;
-		board[x-1][y-1] = {s,d-1,z};
+		board[x - 1][y - 1] = {s, d-1, z};
 	}
 
 	int ans = 0;
-	for(int fisher = 0; fisher < c; fisher++)
+	for (int fisher = 0; fisher < C; fisher++)
 	{
-		//1. 현재 열의 상어를 잡습니다.
-		for (int i = 0; i < r; i++)
+		//1. 상어 잡기
+		for (int i = 0; i < R; i++)
 		{
-			int s = board[i][fisher].s;
-			int d = board[i][fisher].d;
-			int z = board[i][fisher].z;
-			//상어가 없는 위치라면 건너뜁니다.
-			if(z == 0) continue;
-			ans += z;
-			board[i][fisher] = {0,0,0};
+			if(board[i][fisher].z == 0) continue;
+			ans += board[i][fisher].z;
+			board[i][fisher].z = 0;
 			break;
 		}
 
-		//2. 상어가 이동합니다.
-		MoveSharks(board, r, c);
+		MoveSharks(board, R, C);
 	}
-	cout << ans << "\n";
+	cout << ans;
 	return 0;
 }
