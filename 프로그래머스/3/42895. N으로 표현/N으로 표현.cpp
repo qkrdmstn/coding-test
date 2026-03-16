6da134fd7ff5dd853c8ca39b2a1e93be8bde4d34
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-16 15:58:25 +0900

    [level 3] Title: N으로 표현, Time: 3.51 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,34 +1,35 @@
 #include <string>
 #include <vector>
 #include <unordered_set>
-
 using namespace std;
 
 int solution(int N, int number) {
-    if(N == number) return 1;
-    vector<unordered_set<int>> dp(9, unordered_set<int>());
-    int num = N;
+    if(number == N) return 1;
+    unordered_set<int> s[9];
+    
+    int cur = N;
     for(int i=1; i<=8; i++)
     {
-        dp[i].insert(num);
-        num = num * 10 + N;
+        s[i].insert(cur);
+        cur = cur * 10 + N;
     }
     
-    for(int i=2; i<=8; i++)
+    for(int i=1; i<=8; i++)
     {
         for(int j=1; j<i; j++)
         {
-            for(auto d: dp[j])
+            for(auto &d: s[i-j])
             {
-                for(auto p: dp[i-j])
+                for(auto &p: s[j])
                 {
-                    dp[i].insert(d+p);
-                    dp[i].insert(d-p);
-                    dp[i].insert(d*p);
-                    if(p != 0) dp[i].insert(d/p);
+                    s[i].insert(d+p);
+                    s[i].insert(d-p);
+                    s[i].insert(d*p);
+                    if(p!=0) s[i].insert(d/p);
                 }
             }
-            if(dp[i].find(number) != dp[i].end()) return i;
+            if(s[i].count(number) != 0)
+                return i;
         }
     }
     return -1;
