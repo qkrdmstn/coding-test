680770dd70c6983d967be497f70b9929375005f2
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-16 16:52:20 +0900

    [Gold I] Title: 다리 만들기 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.cc
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.cc
@@ -1,25 +1,26 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 #include <tuple>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-int dx[4] = { 0,1,0,-1 };
+int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 
-int NamingIsland(vector<vector<int>>& board, int n, int m)
+int LabelingIsland(vector<vector<int>>& board, int N, int M)
 {
-	int cnt = 1;
-	queue<pair<int, int>> q;
-	vector<vector<bool>> visIsland(n, vector<bool>(m, false));
-	for (int i = 0; i < n; i++)
+	int cnt = 0;
+	vector<vector<bool>> vis(N, vector<bool>(M));
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < M; j++)
 		{
-			if (visIsland[i][j] || board[i][j] == 0) continue;
+			if (vis[i][j] || board[i][j] == 0) continue;
+			queue<pair<int, int>> q;
+			vis[i][j] = true;
 			q.push({ i, j });
-			visIsland[i][j] = true;
+			cnt++;
 			while (!q.empty())
 			{
 				auto cur = q.front(); q.pop();
@@ -28,108 +29,106 @@ int NamingIsland(vector<vector<int>>& board, int n, int m)
 				{
 					int nx = cur.first + dx[dir];
 					int ny = cur.second + dy[dir];
-					if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-					if (board[nx][ny] == 0 || visIsland[nx][ny]) continue;
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+					if (vis[nx][ny] || board[nx][ny] == 0) continue;
 					q.push({ nx, ny });
-					visIsland[nx][ny] = true;
+					vis[nx][ny] = true;
 				}
 			}
-			cnt++;
 		}
 	}
-
 	return cnt;
 }
 
-void GenBridges(vector<tuple<int, int, int>>& edge, vector<vector<int>>& board,int n, int m)
+void BuildEdge(vector<tuple<int, int, int>>& edge, vector<vector<int>>& board, int N, int M)
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < M; j++)
 		{
 			if (board[i][j] == 0) continue;
 
 			for (int dir = 0; dir < 4; dir++)
 			{
-				int r = i;
-				int c = j;
+				int x = i;
+				int y = j;
 				int dist = 0;
+				int curLabel = board[i][j];
 
 				while (true)
 				{
-					r += dx[dir];
-					c += dy[dir];
-					if (r < 0 || r >= n || c < 0 || c >= m) break;
-					if (board[r][c] == 0) dist++;
-					else
+					int nx = x + dx[dir];
+					int ny = y + dy[dir];
+					if (nx < 0 || nx >= N || ny < 0 || ny >= M) break;
+					if (board[nx][ny] != curLabel && board[nx][ny] != 0 && dist >= 2)
 					{
-						if (board[r][c] == board[i][j]) break;
-						if (board[r][c] != board[i][j])
-						{
-							if (dist >= 2)
-								edge.push_back({ dist, board[i][j], board[r][c] });
-							break;
-						}
+						edge.push_back({ dist, board[i][j], board[nx][ny] });
+						break;
 					}
+					if(board[nx][ny] != 0) break;
+					dist++;
+					x = nx;
+					y = ny;
 				}
 			}
 		}
 	}
 }
 
-int find(int u, vector<int>& parent)
+int find(int u, vector<int>& p)
 {
-	if(parent[u] < 0) return u;
-	else return parent[u] = find(parent[u], parent);
+	if(p[u] < 0) return u;
+	return p[u] = find(p[u], p);
 }
 
-bool uni(int u, int v, vector<int>& parent)
+bool uni(int u, int v, vector<int>& p)
 {
-	u = find(u, parent), v = find(v, parent);
-	if(u==v)
-		return false;
-	parent[u] = v;
+	u = find(u, p); v = find(v, p);
+	if(u == v) return false;
+	p[u] = v;
 	return true;
 }
 
 int main(void)
 {
-	int n, m;
-	cin >> n >> m;
-	vector<vector<int>> board(n,vector<int>(m));
-	for (int i = 0; i < n; i++)
+	int N, M;
+	cin >> N >> M;
+
+	vector<vector<int>> board(N, vector<int>(M));
+	for (int i = 0; i < N; i++)
 	{
-		for(int j=0; j<m; j++)
+		for (int j = 0; j < M; j++)
 			cin >> board[i][j];
 	}
 
-	// 1. BFS를 활용해 각 섬에 대해 번호를 지정합니다.
-	int islandCnt = NamingIsland(board, n, m) - 1;
+	//1. BFS를 활용한 각 섬 라벨링
+	int islandCnt = LabelingIsland(board, N, M);
 
-	// 2. 각 섬마다 상하좌우 직선방향 검사를 통해 다른 섬에 도달하는 경우 다리를 세웁니다.  
+	//2. 다리 연결하기
 	vector<tuple<int, int, int>> edge;
-	GenBridges(edge, board, n, m);
+	BuildEdge(edge, board, N, M);
 
-
-	// 3. 크루스칼 알고리즘으로 최소 비용으로 모든 섬을 연결하는 최소신장트리를 구성합니다.
+	//3. 최소신장트리 만들기
 	sort(edge.begin(), edge.end());
-	vector<int> parent(islandCnt + 1, -1);
 	int ans = 0;
-	int cnt = 0;
-	for (auto& e : edge)
+	int edgeCnt = 0;
+	vector<int> p(islandCnt+1, -1);
+	for (auto &e: edge)
 	{
 		int c, a, b;
 		tie(c, a, b) = e;
-		if (uni(a, b, parent))
+		
+		if (uni(a, b, p))
 		{
 			ans += c;
-			cnt++;
+			edgeCnt++;
 		}
-		if(cnt == islandCnt-1) break;
+		if(edgeCnt == islandCnt - 1) break;
 	}
+	if(edgeCnt != islandCnt - 1)
+		cout << -1;
+	else
+		cout << ans;
 
-	// 4. 선택된 다리의 개수가 v-1가 아니라면 모든 섬을 연결하지 못합니다.
-	if(cnt != islandCnt -1) cout << -1;
-	else cout << ans << "\n";
 	return 0;
 }
