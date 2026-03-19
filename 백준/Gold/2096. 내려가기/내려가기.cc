1f4b0b258aee8a3eb8860c47baa37fbdc4bc0253
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-19 17:36:01 +0900

    [Gold V] Title: 내려가기, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -7,8 +7,8 @@ int main(void)
 {
 	int n;
 	int board[3];
-	int maxDp[2][3];
-	int minDp[2][3];
+	int maxDp[2][3]; //dp[1][i]: i번째 위치에 도착할 때 최대/최소 값
+	int minDp[2][3]; 
 
 	cin >> n;
 	for (int i = 0; i < 3; i++)
@@ -17,6 +17,7 @@ int main(void)
 		maxDp[0][i] = minDp[0][i] = board[i];
 	}
 
+	//메모리 절약을 위해 입력 즉시 테이블 값 계산
 	for (int i = 1; i < n; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -27,6 +28,7 @@ int main(void)
 			int minNum = 0x3f3f3f3f;
 			int maxNum = 0;
 
+			//j번째 자리로 내려갈 수 있는 이전행의 수를 순회
 			for (int diff = 0; diff < 3; diff++)
 			{
 				int idx = j + offset[diff];
@@ -34,10 +36,11 @@ int main(void)
 				minNum = min(minNum, minDp[0][idx]);
 				maxNum = max(maxNum, maxDp[0][idx]);
 			}
+			//가장 큰/작은 수를 각각 dp 테이블에 합산
 			minDp[1][j] = board[j] + minNum;
 			maxDp[1][j] = board[j] + maxNum;
 		}
-
+		//현재 테이블 값 이전으로 저장
 		for (int j = 0; j < 3; j++)
 		{
 			minDp[0][j] = minDp[1][j];
