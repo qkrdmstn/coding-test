e93afb4bb5ddeba8631e6ce88d642e8cd003f4d5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-20 18:02:51 +0900

    [Gold III] Title: 소문난 칠공주, Time: 20 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.cc
@@ -6,7 +6,7 @@ using namespace std;
 string seat[5];
 int princess[7];
 bool isPrincess [5][5];
-int ans, cntS;
+int ans;
 
 queue<pair<int, int>> q;
 int dx[4] = { 0,0,-1,1 };
@@ -18,10 +18,10 @@ pair<int, int> Convert1Dto2D(int i)
 	return { i / 5, i % 5 };
 }
 
-//BFS를 활용해 모든 공주가 이웃한지 확인
+//BFS를 활용해 모든 공주가 이웃한지 확인 및 이다솜파 Count
 bool IsAdjacency()
 {
-	int cnt = 0;
+	int cnt = 0, cntS = 0;
 	for (int i = 0; i < 5; i++) {
 		for (int j = 0; j < 5; j++)
 			visited[i][j] = false;
@@ -30,11 +30,16 @@ bool IsAdjacency()
 	pair<int, int> pos = Convert1Dto2D(princess[0]);
 	visited[pos.first][pos.second] = true;
 	q.push(pos);
-	cnt++;
 
 	while (!q.empty()) {
 		pair<int, int> cur = q.front();
 		q.pop();
+
+		//이다솜파 Count
+		if (seat[cur.first][cur.second] == 'S')
+			cntS++;
+		cnt++;
+
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = cur.first + dx[dir];
 			int ny = cur.second + dy[dir];
@@ -43,21 +48,22 @@ bool IsAdjacency()
 				continue;
 			if (!isPrincess[nx][ny] || visited[nx][ny])
 				continue;
+
 			q.push({ nx, ny });
 			visited[nx][ny] = true;
-			cnt++;
 		}
 	}
 	
-	if (cnt == 7)
+	if (cnt == 7 && cntS >= 4)
 		return true;
 	return false;
 }
 
+//백트래킹으로 중복 없는 조합을 선택
 void func(int cnt, int start)
 {
 	if (cnt == 7) {
-		if (cntS >= 4 && IsAdjacency()) {
+		if (IsAdjacency()) {
 			ans++;
 		}
 		return;
@@ -68,15 +74,9 @@ void func(int cnt, int start)
 			if (isPrincess[pos.first][pos.second])
 				continue;
 			isPrincess[pos.first][pos.second] = true;
-			if (seat[pos.first][pos.second] == 'S')
-				cntS++;
 			princess[cnt] = i;
-			
 			func(cnt + 1, i + 1);
-			
 			isPrincess[pos.first][pos.second] = false;
-			if (seat[pos.first][pos.second] == 'S')
-				cntS--;
 		}
 	}
 }
@@ -90,5 +90,11 @@ int main(void)
 		cin >> seat[i];
 	func(0, 0);
 	cout << ans;
+
+	/*
+	2단계로 구현.
+	1. 2차원 데이터를 1차원으로 변환한 뒤, 백트래킹(DFS)를 활용해서 중복 없는 조합을 선택
+	2. BFS를 활용해서 선택된 조합이 모두 이웃한지 확인하여 최종적인 cnt 증가
+	*/
 	return 0;
 }
