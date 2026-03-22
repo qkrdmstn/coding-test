a2670874a150ce89aaf586e6b997e344eee131ff
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-22 16:55:34 +0900

    [Silver II] Title: 제곱수의 합, Time: 24 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -7,17 +7,16 @@ int main(void)
 	int n;
 	cin >> n;
 
-	//dp[i]: i를 제곱의 합으로 표현했을 때 최소항의 개수
-	vector<int> dp(n + 1, 0x3f3f3f3f13);
-	for (int i = 1; i * i <= n; i++)
-		dp[i * i] = 1;
+	vector<int> dp(n + 1, n);
+	for(int i=1; i*i <=n; i++)
+		dp[i*i] = 1;
 
 	for (int i = 2; i <= n; i++)
 	{
-		//dp[i] = dp[i - 제곱수] + 1(제곱수의 항 개수) 중 가장 작은 것
 		for (int j = 1; j * j < i; j++)
-			dp[i] = min(dp[i], dp[i - j * j] + 1);
+			dp[i] = min(dp[i], dp[i-j*j] + 1);
 	}
 	cout << dp[n];
+
 	return 0;
 }
