594b3ca14c1e25765fe3f223aae73579cc54c132
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-24 12:55:08 +0900

    [Gold V] Title: 탑, Time: 92 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,28 +1,29 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include <stack>
 using namespace std;
+
+const int INF = 0x3f3f3f3f;
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
 	cin.tie(0);
+	ios::sync_with_stdio(0);
 
 	int n;
-	stack<pair<int, int>> s;
-
 	cin >> n;
-	for (int i = 1; i <= n; i++) {
+
+	stack<pair<int, int>> s; //{높이, 번호}
+	s.push({ INF, 0 });
+	for (int i = 1; i <= n; i++)
+	{
 		int h;
 		cin >> h;
-
-		while (!s.empty() && s.top().first < h) {
+		while (!s.empty() && s.top().first < h)
+		{
 			s.pop();
 		}
-		if (s.empty())
-			cout << "0 ";
-		else
-			cout << s.top().second << " ";
-		s.push({ h,i });
+		cout << s.top().second << " ";
+		s.push({ h, i });
 	}
 
 	return 0;
