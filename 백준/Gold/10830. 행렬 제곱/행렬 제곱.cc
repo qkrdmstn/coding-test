9ca062e27619c271c60a2de1f398d6372f5656d6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-24 13:20:15 +0900

    [Gold IV] Title: 행렬 제곱, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cc
@@ -3,38 +3,39 @@
 using namespace std;
 
 typedef long long ll;
-typedef vector<vector<ll>> matrix;
+typedef vector<vector<ll>> Mat;
+const int MOD = 1000;
 
-matrix Product(matrix& a, matrix& b, int n)
+Mat Product(Mat a, Mat b, int n)
 {
-	matrix r(n, vector<ll>(n));
-	for (int i = 0; i < n; i++)
+	Mat res(n, vector<ll>(n));
+	for (int k = 0; k < n; k++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int i = 0; i < n; i++)
 		{
-			for (int k = 0; k < n; k++)
+			for (int j = 0; j < n; j++)
 			{
-				r[i][j] = (r[i][j] + a[i][k] * b[k][j]) % 1000;
+				res[i][j] = (res[i][j] + (a[i][k] * b[k][j]) % MOD) % MOD;
 			}
 		}
 	}
-
-	return r;
+	return res;
 }
 
-matrix Pow(matrix& a, ll exp, int n)
+Mat Pow(Mat m, ll exp, int n)
 {
-	if(exp == 1) return a;
+	if(exp == 1) return m;
+	
 	if (exp % 2 == 0)
 	{
-		matrix half = Pow(a, exp/2, n);
+		Mat half = Pow(m, exp / 2, n);
 		return Product(half, half, n);
 	}
 	else
 	{
-		matrix half = Pow(a, (exp-1)/2, n);
-		matrix tmp = Product(half, half, n);
-		return Product(tmp, a, n);
+		Mat half = Pow(m, (exp - 1) / 2, n);
+		Mat tmp = Product(half, half, n);
+		return Product(tmp, m, n);
 	}
 }
 
@@ -44,24 +45,23 @@ int main(void)
 	ll b;
 	cin >> n >> b;
 
-	matrix a(n, vector<ll>(n));
-
+	Mat m(n, vector<ll>(n));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
 			int num;
 			cin >> num;
-			a[i][j] = num % 1000;;
+			m[i][j] = num % MOD;
 		}
 	}
 
-	matrix result = Pow(a, b, n);
+	Mat res = Pow(m, b, n);
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
-			cout << result[i][j] <<" ";
-		cout << '\n';
+			cout << res[i][j]<< " ";
+		cout << "\n";
 	}
 	return 0;
 }
