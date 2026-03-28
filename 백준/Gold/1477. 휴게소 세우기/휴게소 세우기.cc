af37932293c91294fec4c28cac701c5b65e83a27
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-28 15:17:46 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.cc
@@ -3,36 +3,31 @@
 #include <algorithm>
 using namespace std;
 
-int check(int len, vector<int>& pos, int n)
+int Check(int len, vector<int>& pos)
 {
+	int prev = 0;
 	int cnt = 0;
-	int curPos = 0;
-	for (int i = 0; i <= n; i++)
+	for (int i = 0; i < pos.size(); i++)
 	{
-		int dist = pos[i] - curPos;
-		int num = dist / len;
-		for (int j = 1; j <= num; j++)
-		{
-			if (curPos + j * len >= pos[i]) continue;
-			cnt++;
-		}
-		curPos = pos[i];
+		int dist = pos[i] - prev;
+		cnt += dist / len;
+		if (dist % len == 0)
+			cnt--;
+		prev = pos[i];
 	}
 	return cnt;
 }
 
 int main(void)
 {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	int n, m, l;
 	cin >> n >> m >> l;
-
-	vector<int> pos;
+	vector<int> pos(n);
 	for (int i = 0; i < n; i++)
-	{
-		int p;
-		cin >> p;
-		pos.push_back(p);
-	}
+		cin >> pos[i];
 	pos.push_back(l);
 	sort(pos.begin(), pos.end());
 
@@ -42,12 +37,12 @@ int main(void)
 	while (st <= ed)
 	{
 		int mid = st + (ed - st) / 2;
-		if (check(mid, pos, n) > m)
+		if (Check(mid, pos) > m)
 			st = mid + 1;
 		else
 		{
-			ans = mid;
 			ed = mid - 1;
+			ans = mid;
 		}
 	}
 	cout << ans;
