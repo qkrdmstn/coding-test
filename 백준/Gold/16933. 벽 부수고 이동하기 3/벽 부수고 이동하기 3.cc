7b18e7da6e5f811f34610e3a5a23c8b7c0e94498
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-28 15:43:53 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1116 ms, Memory: 95424 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.cc
@@ -1,61 +1,63 @@
-#include<iostream>
-#include <vector>
+#include <iostream>
 #include <queue>
 #include <tuple>
-#include <algorithm>
-#include <string>
 using namespace std;
 
-int dx[4] = { 1,0,-1,0};
-int dy[4] = { 0,1,0,-1};
-string board[1000];
-int dist[1000][1000][11][2] = { -1, };
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
 
-int BFS(int n, int m, int k)
+int n, m, k;
+string board[1005];
+int dist[1005][1005][11][2];
+
+int BFS()
 {
 	queue<tuple<int, int, int, int>> q;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			for (int l = 0; l <= k; l++)
-				fill(dist[i][j][l], dist[i][j][l] + 2, -1);
 
 	q.push({ 0,0,k,0 });
 	dist[0][0][k][0] = 1;
 	while (!q.empty())
 	{
-		int cx, cy, ck, cd;
-		tie(cx, cy, ck, cd) = q.front(); q.pop();
+		int cx, cy, ck, ct;
+		tie(cx, cy, ck, ct) = q.front(); q.pop();
 
-		if(cx == n-1 && cy == m-1) return dist[cx][cy][ck][cd];
+		//목적지 도착
+		if (cx == n - 1 && cy == m - 1)
+			return dist[cx][cy][ck][ct];
 
 		for (int i = 0; i < 4; i++)
 		{
-			int nx = cx + dx[i];
-			int ny = cy + dy[i];
-			int nd = 1 - cd;
-			if(nx < 0 || nx >=n || ny < 0 || ny >= m) continue;
+			int nx = cx + dx[i], ny = cy + dy[i];
+			int nt = 1 - ct;
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
 
+			//벽을 부술 수 있는 상태
 			if (board[nx][ny] == '1' && ck > 0)
 			{
-				if (cd == 0)
+				//낮
+				if (ct == 0)
 				{
 					int nk = ck - 1;
-					if (dist[nx][ny][nk][nd] >= 0) continue;
-					q.push({ nx, ny, nk, nd });
-					dist[nx][ny][nk][nd] = dist[cx][cy][ck][cd] + 1;
+					if (dist[nx][ny][nk][nt] >= 1) continue;
+					q.push({ nx, ny, nk, nt });
+					dist[nx][ny][nk][nt] = dist[cx][cy][ck][ct] + 1;
 				}
+				//밤
 				else
 				{
-					if (dist[cx][cy][ck][nd] >= 0) continue;
-					q.push({ cx, cy, ck, nd });
-					dist[cx][cy][ck][nd] = dist[cx][cy][ck][cd] + 1;
+					//제자리에서 낮을 기다림.
+					int nk = ck;
+					if (dist[cx][cy][nk][nt] >= 1) continue;
+					q.push({ cx, cy, nk, nt });
+					dist[cx][cy][nk][nt] = dist[cx][cy][ck][ct] + 1;
 				}
 			}
 			else if (board[nx][ny] == '0')
 			{
-				if(dist[nx][ny][ck][nd] >= 0) continue;
-				q.push({nx, ny, ck, nd});
-				dist[nx][ny][ck][nd] = dist[cx][cy][ck][cd] + 1;
+				int nk = ck;
+				if (dist[nx][ny][nk][nt] >= 1) continue;
+				q.push({ nx, ny, nk, nt });
+				dist[nx][ny][nk][nt] = dist[cx][cy][ck][ct] + 1;
 			}
 		}
 	}
@@ -64,12 +66,11 @@ int BFS(int n, int m, int k)
 
 int main(void)
 {
-	int n, m, k;
-
 	cin >> n >> m >> k;
+
 	for (int i = 0; i < n; i++)
 		cin >> board[i];
 
-	cout << BFS(n, m, k);
+	cout << BFS();
 	return 0;
 }
