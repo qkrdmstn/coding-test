22db971db75b8282d23da8442ac00a3dc8c959d7
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-28 17:00:45 +0900

    [Gold V] Title: 틱택토, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.cc
+++ 백준/Gold/7682. 틱택토/틱택토.cc
@@ -1,78 +1,82 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-bool Check(string s)
+bool Check(string a)
 {
-	int xCnt = 0, oCnt = 0;
 	bool xWin = false, oWin = false;
+	int xCnt = 0, oCnt = 0;
 
 	for (int i = 0; i < 9; i++)
 	{
-		if (s[i] == 'X') xCnt++;
-		else if (s[i] == 'O') oCnt++;
+		if(a[i] == 'X') xCnt++;
+		else if(a[i] == 'O') oCnt++;
 	}
-	if (xCnt != oCnt && xCnt != oCnt + 1) return false;
+	if(oCnt > xCnt) return false;
 
-	if (s[0] == s[4] && s[4] == s[8])
+	//가로
+	if (a[0] == a[1] && a[1] == a[2])
 	{
-		if (s[0] == 'X') xWin = true;
-		else if (s[0] == 'O') oWin = true;
+		if(a[0] == 'X') xWin = true;
+		else if(a[0] == 'O') oWin = true;
 	}
-	if (s[2] == s[4] && s[4] == s[6])
+	if (a[3] == a[4] && a[4] == a[5])
 	{
-		if (s[2] == 'X') xWin = true;
-		else if (s[2] == 'O') oWin = true;
+		if (a[3] == 'X') xWin = true;
+		else if (a[3] == 'O') oWin = true;
 	}
-
-	if (s[0] == s[1] && s[1] == s[2])
+	if	(a[6] == a[7] && a[7] == a[8])
 	{
-		if (s[0] == 'X') xWin = true;
-		else if (s[0] == 'O') oWin = true;
-	}
-	if (s[3] == s[4] && s[4] == s[5])
-	{
-		if (s[3] == 'X') xWin = true;
-		else if (s[3] == 'O') oWin = true;
-	}
-	if (s[6] == s[7] && s[7] == s[8])
-	{
-		if (s[6] == 'X') xWin = true;
-		else if (s[6] == 'O') oWin = true;
+		if (a[6] == 'X') xWin = true;
+		else if (a[6] == 'O') oWin = true;
 	}
 
-	if (s[0] == s[3] && s[3] == s[6])
+	//세로
+	if(a[0] == a[3] && a[3] == a[6])
 	{
-		if (s[0] == 'X') xWin = true;
-		else if (s[0] == 'O') oWin = true;
+		if (a[0] == 'X') xWin = true;
+		else if (a[0] == 'O') oWin = true;
 	}
-	if (s[1] == s[4] && s[4] == s[7])
+	if	(a[1] == a[4] && a[4] == a[7]) 
 	{
-		if (s[1] == 'X') xWin = true;
-		else if (s[1] == 'O') oWin = true;
+		if (a[1] == 'X') xWin = true;
+		else if (a[1] == 'O') oWin = true;
 	}
-	if (s[2] == s[5] && s[5] == s[8])
+	if	(a[2] == a[5] && a[5] == a[8])
 	{
-		if (s[2] == 'X') xWin = true;
-		else if (s[2] == 'O') oWin = true;
+		if (a[2] == 'X') xWin = true;
+		else if (a[2] == 'O') oWin = true;
 	}
 
-	if (xWin && !oWin && xCnt == oCnt + 1) return true;
-	if (oWin && !xWin && xCnt == oCnt) return true;
-	if (!xWin && !oWin && xCnt == 5 && oCnt == 4) return true;
+	//대각선
+	if (a[0] == a[4] && a[4] == a[8])
+	{
+		if (a[0] == 'X') xWin = true;
+		else if (a[0] == 'O') oWin = true;
+	}
+	if (a[2] == a[4] && a[4] == a[6])
+	{
+		if (a[2] == 'X') xWin = true;
+		else if (a[2] == 'O') oWin = true;
+	}
 
-	return false;
+	if(xWin && oWin) return false;
+	if(xWin && xCnt != oCnt + 1) return false;
+	if(oWin && oCnt != xCnt) return false;
+	if(!xWin && !oWin && xCnt + oCnt != 9) return false;
+	return true;
 }
 
 int main(void)
 {
+
+	string s;
 	while (true)
 	{
-		string s;
 		cin >> s;
-		if (s == "end") break;
-
-		if (Check(s)) cout << "valid\n";
+		if(s=="end") break;
+		if(Check(s)) cout << "valid\n";
 		else cout << "invalid\n";
 	}
 	return 0;
