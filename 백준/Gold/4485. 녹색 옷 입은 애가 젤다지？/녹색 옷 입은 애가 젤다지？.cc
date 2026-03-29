28c23740b8b29db564176b5c0613384f65a753b6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-29 13:39:37 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -1,57 +1,55 @@
 #include <iostream>
-#include <tuple>
 #include <queue>
-#include <algorithm>
+#include <vector>
+#include <tuple>
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
 int main(void)
 {
-	int n;
-	int cnt = 1;
+	int n, step = 0;
 	while (true)
 	{
-		cin >> n;
-		if(n == 0) break;
+		step++;
 
-		int board[125][125];
+		cin >> n;
+		if (n == 0) break;
+
+		vector<vector<int>> board(n, vector<int>(n));
 		for (int i = 0; i < n; i++)
 		{
-			for(int j=0; j<n; j++)
+			for (int j = 0; j < n; j++)
 				cin >> board[i][j];
 		}
 
+		vector<vector<int>> dist(n, vector<int>(n, INF));
 		priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
-		int dist[125][125];
-		for(int i=0; i<n; i++)
-			fill(dist[i], dist[i] + n, INF);
-		pq.push({ board[0][0], 0, 0});
+
+		pq.push({ board[0][0], 0, 0 });
 		dist[0][0] = board[0][0];
 
 		while (!pq.empty())
 		{
-			int cost, cx, cy;
-			tie(cost, cx, cy) = pq.top(); pq.pop();
-			if(dist[cx][cy] != cost) continue;
+			int curX, curY, cost;
+			tie(cost, curX, curY) = pq.top(); pq.pop();
+			if(dist[curX][curY] != cost) continue;
 
-			for (int dir = 0; dir < 4; dir++)
+			for (int i = 0; i < 4; i++)
 			{
-				int nx = cx + dx[dir];
-				int ny = cy + dy[dir];
-				if(nx <0 || nx >= n || ny < 0 || ny >= n) continue;
-
-				int nxtCost = board[nx][ny];
-				if(dist[cx][cy] + nxtCost >= dist[nx][ny]) continue;
-				dist[nx][ny] = dist[cx][cy] + nxtCost;
-				pq.push({dist[nx][ny], nx, ny});
+				int nx = curX + dx[i], ny = curY + dy[i];
+				if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+				if (dist[curX][curY] + board[nx][ny] < dist[nx][ny])
+				{
+					dist[nx][ny] = dist[curX][curY] + board[nx][ny];
+					pq.push({dist[nx][ny], nx, ny});
+				}
 			}
 		}
 
-		cout << "Problem " << cnt << ": " << dist[n-1][n-1] << "\n";
-		cnt++;
+		cout << "Problem " << step << ": " << dist[n-1][n-1] << "\n";
 	}
-
 	return 0;
 }
