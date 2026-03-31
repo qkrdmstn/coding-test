cfb3b1b26157fb29780e1bd706cf56f8d50b96fc
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-03-31 17:07:44 +0900

    [Silver II] Title: 좌표 압축, Time: 580 ms, Memory: 256348 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -3,24 +3,54 @@
 #include <algorithm>
 using namespace std;
 
-int n;
-int arr[1000005];
+const long long OFFSET = 1000000000;
+int LowBound(int x, vector<int>& arr, int n)
+{
+	
+	int st = 0;
+	int ed = n-1;
+	int ans = 0;
+	while (st <= ed)
+	{
+		int mid = st + (ed - st) / 2;
+		if (arr[mid] >= x)
+		{
+			ed = mid - 1;
+			ans = mid;
+		}
+		else
+			st = mid + 1;
+	}
+
+	return ans;
+}
 int main(void)
 {
-	ios::sync_with_stdio(0);
 	cin.tie(0);
+	ios::sync_with_stdio(0);
 
+	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
+
+	vector<int> arr(n);
+	for(int i=0; i<n; i++)
 		cin >> arr[i];
 
-	vector<int> v(arr, arr + n);
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
+	vector<bool> vis(2000000001, false);
+	vector<int> sortedArr;
+	for (auto a : arr)
+	{
+		if(vis[a + OFFSET]) continue;
+		sortedArr.push_back(a);
+		vis[a + OFFSET] = true;
+	}
+	sort(sortedArr.begin(), sortedArr.end());
 
-	for (int i = 0; i < n; i++)
-		arr[i] = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << ' ';
+	for (auto& a : arr)
+	{
+		int idx = LowBound(a, sortedArr, sortedArr.size());
+		cout << idx << " ";
+	}
+
 	return 0;
 }
