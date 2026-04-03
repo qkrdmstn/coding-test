1045b191e17bf31394b78d93f74c165d214f9783
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-03 16:55:06 +0900

    [Gold IV] Title: 트리의 지름, Time: 8 ms, Memory: 3332 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cc
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -1,53 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <tuple>
+#include <algorithm>
 using namespace std;
 
-const int MXN = 10'005;
+vector<pair<int, int>> adj[10'005];
+int maxNode, maxDist;
 
-int n;
-int mxDist, mxNode;
-vector<pair<int, int>> adj[MXN];
-bool vis[MXN];
-
-void dfs(int cur, int dist)
+void DFS(int st, int dist, vector<bool>& vis)
 {
-	if (mxDist < dist) {
-		mxDist = dist;
-		mxNode = cur;
+	if (dist > maxDist)
+	{
+		maxDist = dist;
+		maxNode = st;
 	}
-	for (auto nxt : adj[cur]) {
-		if (vis[nxt.first]) continue;
-		vis[nxt.first] = true;
-		dfs(nxt.first, dist + nxt.second);
+
+	for (auto nxt : adj[st])
+	{
+		int node, cost;
+		tie(node, cost) = nxt;
+		if(vis[node]) continue;
+		vis[node] = true;
+		DFS(node, dist + cost, vis);
+		vis[node] = false;
 	}
 }
-
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
+	int n;
 	cin >> n;
-	for (int i = 0; i < n - 1; i++) {
-		int p, c, w;
-		cin >> p >> c >> w;
-		adj[p].push_back({ c,w });
-		adj[c].push_back({ p,w });
+	for (int i = 0; i < n - 1; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+		adj[a].push_back({b, c});
+		adj[b].push_back({a, c});
 	}
-	
-	vis[1] = true;
-	dfs(1, 0);
-	
-	fill(vis, vis + n + 1, false);
-	vis[mxNode] = true;
-	dfs(mxNode, 0);
-	cout << mxDist;
-	return 0;
 
-	/*
-	트리의 지름은 아무 한 점에서 가장 멀리 간 다음, 
-	그 점과 가장 멀리있는 점까지의 거리를 측정하면 된다.
-	*/
+	vector<bool> vis(n+1, false);
+	vis[1] = true;
+	DFS(1, 0, vis);
+
+
+	fill(vis.begin(), vis.end(), false);
+	vis[maxNode] = true;
+	DFS(maxNode, 0, vis);
+	cout << maxDist;
+	return 0;
 }
