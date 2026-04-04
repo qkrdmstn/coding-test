e4468e1e4d6f4269f19498aa58d70f19420582ff
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-04 20:13:19 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 1484 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.cc
@@ -1,54 +1,59 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int m, n;
-int space[105][10005];
-
-void compress(int a[])
+int LowerBound(int x, vector<int> arr)
 {
-	vector<int> v(a, a + n);
-	sort(v.begin(), v.end());
-	v.erase(unique(v.begin(), v.end()), v.end());
+	int st = 0;
+	int ed = arr.size() - 1;
 
-	for (int i = 0; i < n; i++)
-		a[i] = lower_bound(v.begin(), v.end(), a[i]) - v.begin();
-}
-
-bool IsBalanced(int a[], int b[])
-{
-	for (int i = 0; i < n; i++) {
-		if (a[i] != b[i])
-			return false;
+	int ans = 0;
+	while (st <= ed)
+	{
+		int mid = st + (ed - st) / 2;
+		if (arr[mid] >= x)
+		{
+			ans = mid;
+			ed = mid - 1;
+		}
+		else
+			st = mid + 1;
 	}
-	return true;
+
+	return ans;
 }
 
 int main(void)
 {
-	cin.tie(0);
-	ios::sync_with_stdio(0);
-	
+	int m, n;
 	cin >> m >> n;
-	for (int i = 0; i < m; i++) {
+	vector<vector<int>> space(m, vector<int>(n));
+	for (int i = 0; i < m; i++)
+	{
 		for(int j=0; j<n; j++)
-			cin >> space[i][j];	
-		compress(space[i]);
+			cin >> space[i][j];
+	}
+
+	vector<vector<int>> sortedSpace(space);
+	for(int i=0; i<m; i++)
+		sort(sortedSpace[i].begin(), sortedSpace[i].end());
+
+	for (int i = 0; i < m; i++)
+	{
+		for(int j=0; j<n; j++)
+			//space[i][j] = lower_bound(sortedSpace[i].begin(), sortedSpace[i].end(), space[i][j]) - sortedSpace[i]. begin();
+			space[i][j] = LowerBound(space[i][j], sortedSpace[i]);
 	}
 
 	int ans = 0;
-	for (int i = 0; i < m - 1; i++) {
-		for (int j = i + 1; j < m; j++) {
-			if(IsBalanced(space[i], space[j]))
-				ans++;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = i + 1; j < m; j++)
+		{
+			if(space[i] == space[j]) ans++;
 		}
 	}
 	cout << ans;
-
 	return 0;
-
-	/*
-	두 우주를 좌표 압축했을 때, 동일하면 균등한 우주.
-	*/
 }
