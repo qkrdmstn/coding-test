bcf4d4521ace15db245dcec5d2aaa7693ace174a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-06 20:18:36 +0900

    [Gold IV] Title: ⚾, Time: 272 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.cc
+++ 백준/Gold/17281. ⚾/⚾.cc
@@ -1,101 +1,99 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
 int n;
-int info[51][9];
-int ans = 0;
-int Play(vector<int>& order)
+vector<vector<int>> res(50, vector<int>(9));
+int Simulation(vector<int>& seq)
 {
-	int cur = 0;
-	int round = 0;
-	int score = 0;
-
-	for (int rnd = 0; rnd < n; rnd++)
-	{
-		int out = 0;
-		int b1 = 0, b2 = 0, b3 = 0;
-		while (out < 3)
-		{
-			int player = order[cur];
-			int hit = info[rnd][player];
-			if (hit == 0)
-				out++;
-			else if (hit == 1)
-			{
-				score += b3;
-				b3 = b2;
-				b2 = b1;
-				b1 = 1;
-			}
-			else if (hit == 2)
-			{
-				score += b3 + b2;
-				b3 = b1;
-				b2 = 1;
-				b1 = 0;
-			}
-			else if (hit == 3)
-			{
-				score += b3 + b2 + b1;
-				b3 = 1;
-				b2 = b1 = 0;
-			}
-			else if (hit == 4)
-			{
-				score += b3 + b2 + b1 + 1;
-				b3 = b2 = b1 = 0;
-			}
-			cur = (cur + 1) % 9;
-		}
-	}
-	return score;
+    int curPlayerIdx = 0;
+    
+    int score = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int out = 0;
+        int base[3] = {0,0,0};
+        while (out < 3)
+        {
+            int curPlayer = seq[curPlayerIdx];
+            int state = res[i][curPlayer];
+            if (state == 0)
+                out++;
+            else if (state == 1)
+            {
+                score += base[2];
+                base[2] = base[1];
+                base[1] = base[0];
+                base[0] = 1;
+            }
+            else if (state == 2)
+            {
+                score += base[2] + base[1];
+                base[2] = base[0];
+                base[1] = 1;
+                base[0] = 0;
+            }
+            else if (state == 3)
+            {
+                score += base[2] + base[1] + base[0];
+                base[2] = 1;
+                base[1] = 0;
+                base[0] = 0;
+            }
+            else if (state == 4)
+            {
+                score += base[2] + base[1] + base[0] + 1;
+                base[2] = 0;
+                base[1] = 0;
+                base[0] = 0;
+            }
+            curPlayerIdx = (curPlayerIdx + 1)%9;
+        }
+    }
+    return score;
 }
 
-void DFS(vector<bool>& vis, vector<int>& res)
+void DFS(vector<bool>& used, vector<int>& seq, int& ans)
 {
-	if (res.size() == 9)
-	{
-		ans = max(ans, Play(res));
-		return;
-	}
+    if (seq.size() == 9)
+    {
+        ans = max(ans, Simulation(seq));
+        return;
+    }
 
-	//0번 선수는 4번째 고정
-	if (res.size() == 3)
-	{
-		res.push_back(0);
-		DFS(vis, res);
-		res.pop_back();
-		return;
-	}
+    if (seq.size() == 3)
+    {
+        seq.push_back(0);
+        DFS(used, seq, ans);
+        seq.pop_back();
+        return;
+    }
 
-	for (int i = 0; i < 9; i++)
-	{
-		if (i == 0 || vis[i]) continue;
-		vis[i] = true;
-		res.push_back(i);
-		DFS(vis, res);
-		res.pop_back();
-		vis[i] = false;
-	}
+    for (int i = 0; i < 9; i++)
+    {
+        if (used[i]) continue;
+        used[i] = true;
+        seq.push_back(i);
+        DFS(used, seq, ans);
+        seq.pop_back();
+        used[i] = false;
+    }
 }
-
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 9; j++)
+            cin >> res[i][j];
+    }
 
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < 9; j++)
-			cin >> info[i][j];
-	}
+    vector<bool> used(9, false);
+    vector<int> seq;
+    int ans = 0;
 
-	vector<bool> vis(9, false);
-	vector<int> res;
-	DFS(vis, res);
-	cout << ans;
-	return 0;
+    used[0] = true;
+    DFS(used, seq, ans);
+    cout << ans;
+    return 0;
 }
