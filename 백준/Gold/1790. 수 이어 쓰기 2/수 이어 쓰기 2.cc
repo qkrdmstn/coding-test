c2030607bdebe3dd020801a3f1c6a3873d985819
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-09 22:53:24 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.cc
@@ -3,33 +3,30 @@
 using namespace std;
 
 typedef long long ll;
-ll n, k;
-
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
+	int n, k;
 	cin >> n >> k;
 
 	ll digit = 1;
 	ll start = 1;
-	while (true) {
-		ll num = 9 * start;
-		ll totalDigit = num * digit;
+	while (true)
+	{
+		ll totalDigit = 9 * start * digit;
 		if (k <= totalDigit) break;
 		k -= totalDigit;
 		start *= 10;
 		digit++;
 	}
 
-	ll num = start + (k - 1) / digit;
-	if (num > n) {
+	ll number = start + (k - 1) / digit;
+	if (number > n)
+	{
 		cout << -1;
 		return 0;
 	}
 
-	string str = to_string(num);
-	cout << str[(k - 1) % digit];
+	string ans = to_string(number);
+	cout << ans[(k - 1) % digit];
 	return 0;
 }
