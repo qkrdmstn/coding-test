89cd7d55ce8d2fd513e24a50cf6c971e762852f1
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-16 14:55:54 +0900

    [Gold III] Title: 문자판, Time: 64 ms, Memory: 6548 KB -BaekjoonHub

--- 백준/Gold/2186. 문자판/문자판.cc
+++ 백준/Gold/2186. 문자판/문자판.cc
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
-string board[101];
-string target;
+
+string board[105];
 int dp[105][105][105];
+string target;
 
 int dx[4] = { 0,1,0,-1 };
 int dy[4] = { 1,0,-1,0 };
+
 int main(void)
 {
 	int n, m, k;
-
 	cin >> n >> m >> k;
-
 	for (int i = 0; i < n; i++)
 		cin >> board[i];
 	cin >> target;
@@ -28,36 +27,36 @@ int main(void)
 		}
 	}
 
-	for (int i = len - 2; i >= 0; i--)
+	for (int l = len - 2; l >= 0; l--)
 	{
-		for (int x = 0; x < n; x++)
+		for (int i = 0; i < n; i++)
 		{
-			for (int y = 0; y < m; y++)
+			for (int j = 0; j < m; j++)
 			{
-				if (board[x][y] == target[i])
+				if (board[i][j] != target[l]) continue;
+
+				for (int dir = 0; dir < 4; dir++)
 				{
-					for (int dir = 0; dir < 4; dir++)
+					for (int dist = 1; dist <= k; dist++)
 					{
-						for (int dist = 1; dist <= k; dist++)
-						{
-							int nx = x + dx[dir] * dist;
-							int ny = y + dy[dir] * dist;
-							if(nx < 0 || nx >=n || ny < 0 || ny >= m) continue;
-							if(board[nx][ny] == target[i+1])
-								dp[x][y][i] += dp[nx][ny][i+1];
-						}
+						int nx = i + dx[dir] * dist;
+						int ny = j + dy[dir] * dist;
+						if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+						if (board[nx][ny] == target[l + 1])
+							dp[i][j][l] += dp[nx][ny][l+1];
 					}
 				}
 			}
 		}
 	}
+
 	int ans = 0;
-	for (int x = 0; x < n; x++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int y = 0; y < m; y++)
+		for (int j = 0; j < m; j++)
 		{
-			if (board[x][y] == target[0])
-				ans += dp[x][y][0];
+			if(board[i][j] == target[0])
+				ans += dp[i][j][0];
 		}
 	}
 	cout << ans;
