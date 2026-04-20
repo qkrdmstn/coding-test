7e055bad8ffe8ef588c54496536ac2c85e9bef25
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-20 15:03:31 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -1,65 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <unordered_map>
 using namespace std;
+
+int n, m;
+int board[105];
 
 int main(void)
 {
-	int n, m;
 	cin >> n >> m;
-
-	unordered_map<int, int> upMap;
-	unordered_map<int, int> downMap;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n + m; i++)
 	{
-		int x, y;
-		cin >> x >> y;
-		upMap.insert({ x, y });
-	}
-	for (int i = 0; i < m; i++)
-	{
-		int u, v;
-		cin >> u >> v;
-		downMap.insert({ u, v });
+		int a, b;
+		cin >> a >> b;
+		board[a] = b;
 	}
 
-	vector<int> dist(101, -1);
 	queue<int> q;
+	vector<int> dist(101, -1);
+
 	q.push(1);
 	dist[1] = 0;
 	while (!q.empty())
 	{
 		int cur = q.front(); q.pop();
 		if (cur == 100)
-			break;
-		for (int i = 1; i <= 6; i++)
 		{
-			int nxt = cur + i;
+			cout << dist[cur];
+			return 0;
+		}
+
+		for (int diff = 1; diff <= 6; diff++)
+		{
+			int nxt = cur + diff;
 			if (nxt > 100) continue;
 			if (dist[nxt] >= 0) continue;
 
-
 			dist[nxt] = dist[cur] + 1;
-
-			// 도착 지점에 사다리 또는 뱀이 존재할 경우
-			if (upMap.find(nxt) != upMap.end())
+			if (board[nxt] != 0)
 			{
-				int nnxt = upMap[nxt];
-				if (dist[nnxt] >= 0) continue;
-				q.push(nnxt);
-				dist[nnxt] = dist[cur] + 1;
-			}
-			else if (downMap.find(nxt) != downMap.end())
-			{
-				int nnxt = downMap[nxt];
-				q.push(nnxt);
+				int nnxt = board[nxt];
 				if (dist[nnxt] >= 0) continue;
 				dist[nnxt] = dist[cur] + 1;
+				q.push(nnxt);
 			}
-			else q.push(nxt);
+			else
+				q.push(nxt);
 		}
 	}
-	cout << dist[100];
+	cout << -1;
 	return 0;
 }
