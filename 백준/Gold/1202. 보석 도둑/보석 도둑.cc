33b7b9a85057a15d726304619d9f7a5a5299af30
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-04-20 16:12:15 +0900

    [Gold II] Title: 보석 도둑, Time: 424 ms, Memory: 12272 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,41 +1,59 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
-#include <set>
-using namespace std;
+#include <vector>
+using  namespace std;
+
+/*
+가방을 오름차순 정렬 후, 각 가방에 담을 수 있는 보석을 가치 기준 최대 힙에 넣는다.
+최대 힙에서 보석을 하나 뽑아 택한다.
+*/
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	if(a.first == b.first)
+		return a.second > b.second;
+	return a.first < b.first;
+}
 
 int main(void)
 {
 	int n, k;
 	cin >> n >> k;
 
-	vector<pair<int, int>> jwel(n);
-	multiset<int>bag;
+	vector<pair<int, int>> jewel;
+	vector<int> bag;
+
 	for (int i = 0; i < n; i++)
-		cin >> jwel[i].second >> jwel[i].first;
+	{
+		int m, v;
+		cin >> m >> v;
+		jewel.push_back({ m, v });
+	}
 	for (int i = 0; i < k; i++)
 	{
 		int c;
 		cin >> c;
-		bag.insert(c);
+		bag.push_back(c);
 	}
 
-	sort(jwel.begin(), jwel.end(), greater<pair<int, int>>());
+	sort(jewel.begin(), jewel.end(), cmp);
+	sort(bag.begin(), bag.end());
 
+	int cur = 0;
 	long long ans = 0;
-	int getCnt = 0;
-	for (auto j : jwel)
+	priority_queue<int> pq;
+	for (int i = 0; i < k; i++)
 	{
-		int m, v;
-		tie(v, m) = j;
-		auto lower = bag.lower_bound(m);
-		if (lower != bag.end())
+		while (cur < n && jewel[cur].first <= bag[i])
 		{
-			getCnt++;
-			ans += v;
-			bag.erase(lower);
+			pq.push(jewel[cur].second);
+			cur++;
 		}
-		if(getCnt >= k) break;
+		if (!pq.empty())
+		{
+			ans += pq.top();
+			pq.pop();
+		}
 	}
 	cout << ans;
 	return 0;
