253885e8947007d0f6196c1915ebb7122501102d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-01 15:30:18 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.43 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -1,20 +1,21 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 int solution(vector<vector<string>> clothes) {
     int answer = 1;
     
-    //각 종류에 대한 옷을 count 합니다.
-    unordered_map<string, int> kindOfClothes;
+    unordered_map<string, int> m;
+    unordered_set<string> s;
     for(int i=0; i<clothes.size(); i++)
-        kindOfClothes[clothes[i][1]]++;
+    {
+        m[clothes[i][1]]++;
+        s.insert(clothes[i][1]);
+    }
     
-    //착용하지 않는 경우까지 포함하여 경우의 수를 구합니다.
-    for(auto c: kindOfClothes)
-        answer *= c.second + 1;
-    
-    //한 벌도 착용하지 않는 경우를 제외합니다.
+    for(auto &k: s)
+        answer *= (m[k] + 1);
     return answer - 1;
 }
