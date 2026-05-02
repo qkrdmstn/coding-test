6b4f9f7ab3744c23a246bcc95365af5946e1e79a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-02 15:46:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.14 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -3,42 +3,36 @@
 #include <queue>
 using namespace std;
 
-
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-    priority_queue
-        <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>
-        requestPQ; //{요청 시간, 작업 시간}
-     priority_queue
-        <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>
-         waitPQ; //{작업 시간, 요청 시간}
-
-    for(int i=0; i<jobs.size(); i++)
-        requestPQ.push({jobs[i][0], jobs[i][1]});
     
+    //{작업 시간, 요청 시간}
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> waitPQ;
+    //{요청 시간, 작업 시간}
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> reqPQ;
+    
+    for(auto &job :jobs)
+        reqPQ.push({job[0], job[1]});
+
     int curTime = 0;
-    while(!requestPQ.empty() || !waitPQ.empty())
+    while(!reqPQ.empty() || !waitPQ.empty())
     {
-        //대기 큐는 비어있는데, 요청할 작업이 남아 있다면 다음 요청 시간으로 건너 뛰기
-        if(waitPQ.empty() && !requestPQ.empty())
+        while(!reqPQ.empty() && reqPQ.top().first <= curTime)
         {
-            curTime = requestPQ.top().first;
-            waitPQ.push({requestPQ.top().second, requestPQ.top().first});
-            requestPQ.pop();
+            waitPQ.push({reqPQ.top().second, reqPQ.top().first});
+            reqPQ.pop();
         }
-        int duration = waitPQ.top().first;
-        int request = waitPQ.top().second;
-        waitPQ.pop();
         
-        curTime += duration; 
-        answer += (curTime - request); 
-        
-        //현재 시간보다 요청 시간이 먼저인 작업들을 대기 큐에 삽입합니다.
-        while(!requestPQ.empty() && requestPQ.top().first <= curTime)
+        if(waitPQ.empty() && !reqPQ.empty())
         {
-            waitPQ.push({requestPQ.top().second, requestPQ.top().first});
-            requestPQ.pop();
+            curTime = reqPQ.top().first;
+            continue;
         }
+        
+        auto job = waitPQ.top(); waitPQ.pop();
+        curTime += job.first;
+        answer += curTime - job.second;
     }
-    return answer / jobs.size();
+    answer /= jobs.size();
+    return answer;
 }
