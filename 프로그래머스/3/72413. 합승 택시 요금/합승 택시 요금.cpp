d56fbe68e19bfe91c0b9a69ffa787e4f736b91a6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-10 23:34:33 +0900

    [level 3] Title: 합승 택시 요금, Time: 6.53 ms, Memory: 7.59 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
@@ -9,33 +8,32 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     int answer = 0;
     
     vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
-    
-    for(auto& fare: fares)
+    for(auto fare: fares)
     {
         dist[fare[0]][fare[1]] = fare[2];
         dist[fare[1]][fare[0]] = fare[2];
     }
+    
     for(int i=1; i<=n; i++)
         dist[i][i] = 0;
     
     for(int k=1; k<=n; k++)
     {
-        for(int st=1; st<=n; st++)
+        for(int i=1; i<=n; i++)
         {
-            for(int ed=1; ed<=n; ed++)
+            for(int j=1; j<=n; j++)
             {
-                if(dist[st][k] + dist[k][ed] < dist[st][ed])
-                    dist[st][ed] = dist[st][k] + dist[k][ed];
+                if(dist[i][j] > dist[i][k] + dist[k][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
     }
     
     answer = dist[s][a] + dist[s][b];
-    for(int i=1; i<=n; i++)
+    for(int k=1; k<=n; k++)
     {
-        if(dist[s][i] == INF || dist[i][a] == INF || dist[i][b] == INF) continue;
-        if(dist[s][i] + dist[i][a] + dist[i][b] < answer)
-            answer = dist[s][i] + dist[i][a] + dist[i][b];
+        if(dist[s][k] == INF || dist[k][a] == INF || dist[k][b] == INF) continue;
+        answer = min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
     }
     return answer;
 }
