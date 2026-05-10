3f0f03cdc471f1ae7c85b811a7bc8b64e6830979
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-10 20:29:07 +0900

    [level 3] Title: 이중우선순위큐, Time: 31.85 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,48 +1,49 @@
 #include <string>
 #include <vector>
-#include <sstream>
-#include <queue>
 #include <unordered_map>
+#include <queue>
+#include <iostream>
+#include <sstream>
 using namespace std;
 
 vector<int> solution(vector<string> operations) {
     vector<int> answer;
-    
     priority_queue<int, vector<int>, greater<int>> minHeap;
     priority_queue<int> maxHeap;
     unordered_map<int, int> m;
-    
-    for(string &op:operations)
+    for(const string& operation: operations)
     {
-        stringstream ss(op);
-        char opr;
+        stringstream ss(operation);
+        char op;
         int num;
-        ss >> opr >> num;
+        ss >> op >> num;
         
-        if(opr == 'I')
+        if(op == 'I')
         {
-            minHeap.push(num);
             maxHeap.push(num);
+            minHeap.push(num);
             m[num]++;
         }
-        else if(opr == 'D')
+        else if(op == 'D')
         {
-            if(num == 1 && !maxHeap.empty())
-            {
-                m[maxHeap.top()]--;
-                maxHeap.pop();
-            }
-            else if(num == -1 && !minHeap.empty())
+            if(num == -1 && !minHeap.empty())
             {
                 m[minHeap.top()]--;
                 minHeap.pop();
             }
-            while(!maxHeap.empty() && m[maxHeap.top()] <= 0)
+            else if(num == 1 && !maxHeap.empty())
+            {
+                m[maxHeap.top()]--;
                 maxHeap.pop();
-            while(!minHeap.empty() && m[minHeap.top()] <= 0)
-                minHeap.pop();
+            }
         }
+        
+        while(!maxHeap.empty() && m[maxHeap.top()] <= 0)
+            maxHeap.pop();
+        while(!minHeap.empty() && m[minHeap.top()] <= 0)
+            minHeap.pop();
     }
+    
     if(maxHeap.empty() || minHeap.empty()) answer = {0,0};
     else answer = {maxHeap.top(), minHeap.top()};
     return answer;
