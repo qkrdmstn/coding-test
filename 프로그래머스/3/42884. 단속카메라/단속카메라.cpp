7c5c21d993a1ff07c5e62debc90e2e5eb5dbb849
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-11 16:51:23 +0900

    [level 3] Title: 단속카메라, Time: 0.41 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 bool cmp(vector<int>& a, vector<int>& b)
 {
-    if(a[1] == b[1])
-        return a[0] < b[0];
     return a[1] < b[1];
 }
-
 int solution(vector<vector<int>> routes) {
     int answer = 0;
     
     sort(routes.begin(), routes.end(), cmp);
     
-    int idx = 0;
-    while(idx < routes.size())
+    answer = 1;
+    int lastPos = routes[0][1];
+    for(auto& route: routes)
     {
-        answer++;
-        int pos = routes[idx][1];
-        while(idx < routes.size() && routes[idx][0] <= pos)
-            idx++;
+        if(route[0] > lastPos)
+        {
+            answer++;
+            lastPos = route[1];
+        }
     }
     return answer;
 }
