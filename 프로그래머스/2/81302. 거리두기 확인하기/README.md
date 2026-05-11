6e50e3539aa48768b9f19f5586dd975ecac95afa
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-11 17:15:01 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.02 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302#fnref1) 
 
 ### 성능 요약
 
-메모리: 4.06 MB, 시간: 0.02 ms
+메모리: 3.64 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 08일 16:05:31
+2026년 05월 11일 17:14:57
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -1,34 +1,35 @@
 #include <string>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
-bool BFS(vector<string>& place)
+bool BFS(vector<string> place)
 {
     for(int i=0; i<5; i++)
     {
         for(int j=0; j<5; j++)
         {
             if(place[i][j] != 'P') continue;
-            
             queue<pair<int, int>> q;
             vector<vector<int>> dist(5, vector<int>(5, -1));
             q.push({i, j});
             dist[i][j] = 0;
+            
             while(!q.empty())
             {
                 auto cur = q.front(); q.pop();
-                if(place[cur.first][cur.second] == 'P' && dist[cur.first][cur.second] > 0 &&dist[cur.first][cur.second] <= 2)
-                    return false;
-                if(dist[cur.first][cur.second] > 2) break;
-                for(int dir=0; dir<4; dir++)
+                if(dist[cur.first][cur.second] >= 2) continue;
+                for(int dir = 0; dir<4; dir++)
                 {
                     int nx = cur.first + dx[dir];
                     int ny = cur.second + dy[dir];
                     if(nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
                     if(dist[nx][ny] >= 0 || place[nx][ny] == 'X') continue;
+                    if(place[nx][ny] == 'P') return false;
+                    
                     q.push({nx, ny});
                     dist[nx][ny] = dist[cur.first][cur.second] + 1;
                 }
@@ -37,11 +38,13 @@ bool BFS(vector<string>& place)
     }
     return true;
 }
-
 vector<int> solution(vector<vector<string>> places) {
     vector<int> answer;
     
-    for(auto& place: places)
-        answer.push_back(BFS(place));
+    for(int i=0; i<5; i++)
+    {
+        if(BFS(places[i])) answer.push_back(1);
+        else answer.push_back(0);
+    }
     return answer;
 }
