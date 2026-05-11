6168621dbc9a83da4c6986e183bc63e71553b762
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-11 16:39:27 +0900

    [level 3] Title: 외벽 점검, Time: 10.60 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -1,34 +1,30 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
+const int INF = 0x3f3f3f3f;
 int foo(vector<int>& dist, vector<int>& weak)
 {
-    int ans = 0x3f3f3f3f;
-    for(int s = 0; s < 15; s++)
+    int ans = INF;
+    for(int s=0; s<weak.size()/2; s++)
     {
-        int cnt = 0;
-        int idx = s;
-        int cur = weak[idx];
-        for(int i=0; i<dist.size(); i++)
+        int friendIdx = 0;
+        int finishLine = weak[s] + dist[friendIdx];
+        for(int i=s; i<s+weak.size()/2; i++)
         {
-            int end = cur + dist[i];
-            while(weak[idx] <= end && idx < weak.size())
+            if(finishLine < weak[i])
             {
-                idx++;
-                cnt++;            
+                friendIdx++;
+                if(friendIdx >= dist.size()) break;
+                finishLine = weak[i] + dist[friendIdx];
             }
-            cur = weak[idx];
-            
-            if(cnt >= weak.size()/2)
-                ans = min(ans, i+1);
         }
+        if(friendIdx != dist.size())
+            ans = min(ans, friendIdx + 1);
     }
-    
     return ans;
 }
-
 void DFS(vector<bool>& used, vector<int>& dist, vector<int>& res, vector<int>& weak, int& ans)
 {
     if(res.size() == dist.size())
@@ -36,29 +32,27 @@ void DFS(vector<bool>& used, vector<int>& dist, vector<int>& res, vector<int>& w
         ans = min(ans, foo(res, weak));
         return;
     }
-    
     for(int i=0; i<dist.size(); i++)
     {
         if(used[i]) continue;
         used[i] = true;
         res.push_back(dist[i]);
-        DFS(used,dist,res,weak, ans);
+        DFS(used, dist, res, weak, ans);
         res.pop_back();
         used[i] = false;
     }
-    
 }
-
 int solution(int n, vector<int> weak, vector<int> dist) {
-    int answer = 0x3f3f3f3f;
+    int answer = INF;
     
     int sz = weak.size();
     for(int i=0; i<sz; i++)
         weak.push_back(weak[i] + n);
-
-    vector<bool> used(dist.size(), false);
+    
     vector<int> res;
-    DFS(used,dist,res,weak, answer);
-    if(answer == 0x3f3f3f3f) return -1;
+    vector<bool> used(dist.size(), false);
+    DFS(used, dist, res, weak, answer);
+    
+    if(answer == INF) return -1;
     return answer;
 }
