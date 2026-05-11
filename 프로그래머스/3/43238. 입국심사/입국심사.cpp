12c8de840d97a21c1d6d8e18f99d93df5a3653a2
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-11 17:23:47 +0900

    [level 3] Title: 입국심사, Time: 21.28 ms, Memory: 7.07 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,34 +1,30 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
 
-long long func(long long m, vector<int>& times)
+long long func(long long time, vector<int> times)
 {
-    long long sum = 0;
-    for(auto& t: times)
-        sum += m/t;
-    return sum;
+    long long cnt = 0;
+    for(int i=0; i<times.size(); i++)
+        cnt += time/times[i];
+    return cnt;
 }
-
 long long solution(int n, vector<int> times) {
     long long answer = 0;
     
     sort(times.begin(), times.end());
     long long s = 1;
-    long long e = (long long)times.back() * n;
-    
+    long long e = (long long)n * times.back();
     while(s <= e)
     {
-        long long m = s + (e-s)/2;
+        long long m = s + (e - s) / 2;
         if(func(m, times) >= n)
         {
             answer = m;
             e = m - 1;
         }
-        else
-            s = m + 1;
+        else s = m + 1;
     }
-    return answer; 
+    return answer;
 }
