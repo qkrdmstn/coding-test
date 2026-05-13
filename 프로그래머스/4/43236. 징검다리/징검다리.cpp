fdec58dfa5c492afb5c89392e53a1e081c3c9b45
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-13 17:17:32 +0900

    [level 4] Title: 징검다리, Time: 5.46 ms, Memory: 5.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ 프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -3,40 +3,43 @@
 #include <algorithm>
 using namespace std;
 
-int GetHowManyRemoveRock(int minDist, vector<int>& rocks, int distance)
-{ 
-    int removeRockCnt = 0;
-    int prevRockPos = 0;
+// 각 바위 사이의 거리가 설정된 minDist보다 크거나 같도록 배치할 때, 
+// 몇 개의 바위를 부숴야 하는지 확인합니다.
+long long check(long long minDist, vector<int> rocks)
+{
+    long long cur = 0;
+    int cnt = 0;
     for(int i=0; i<rocks.size(); i++)
     {
-        if((rocks[i] - prevRockPos) < minDist)
-            removeRockCnt++;
+        int dist = rocks[i] - cur;
+        if(dist < minDist)
+            cnt++;
         else
-            prevRockPos = rocks[i];
+            cur = rocks[i];
     }
-    if(distance - prevRockPos < minDist)
-        removeRockCnt++;
-    return removeRockCnt;
+    return cnt;
 }
 
 int solution(int distance, vector<int> rocks, int n) {
     int answer = 0;
-    
+    rocks.push_back(distance);
     sort(rocks.begin(), rocks.end());
     
-    int st = 1;
-    int ed = distance;
-    
+    // 이분탐색으로 바위 사이의 최소 거리를 설정합니다.
+    long long st = 1;
+    long long ed = distance;
     while(st <= ed)
     {
-        int m = (st + ed) / 2;
-        if(GetHowManyRemoveRock(m, rocks, distance) <= n)
+        long long m = st + (ed-st)/2;
+        // 주어진 개수보다 많은 바위를 부술 경우 최소 거리를 좁힙니다.
+        if(check(m, rocks) > n)
+            ed = m-1;
+        // 주어진 개수보다 적은 바위를 부술 경우 최소 거리를 늘릴 수 있습니다.
+        else
         {
             answer = m;
             st = m+1;
         }
-        else
-            ed = m-1;
     }
     return answer;
 }
