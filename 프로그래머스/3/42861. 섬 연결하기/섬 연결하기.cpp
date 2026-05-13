2570c2b61d7797cc2d6850d9c0ffb3db2f5601e3
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-13 22:16:30 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,48 +1,44 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <tuple>
 using namespace std;
 
-int Find(int x, vector<int>& p)
+int find(int x, vector<int>& p)
 {
-    if(p[x] < 0)
-        return x;
-    return p[x] = Find(p[x], p);
+    if(p[x] == -1) return x;
+    return p[x] = find(p[x], p);
 }
 
-bool IsDiffParent(int u, int v, vector<int>& p)
+bool uni(int u, int v, vector<int>& p)
 {
-    u = Find(u, p);  
-    v = Find(v, p);
-    if(u==v) return false;
-    if(p[u] == p[v]) p[u]--;
-    if(p[u] < p[v]) p[v] = u; //u랭크가 더 높으면 v의 부모를 u로
-    else p[u] = v; //반대라면, u의 부모를 v로
+    u = find(u, p);
+    v = find(v, p);
+    if(u == v) return false;
+    p[u] = v;
     return true;
 }
 
+bool cmp(vector<int>& a, vector<int>& b)
+{
+    return a[2] < b[2];
+}
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-    vector<tuple<int, int, int>> edges;
+    
+    vector<int> p(n, -1);
+    sort(costs.begin(), costs.end(), cmp);
+    
+    int cnt = 0;
     for(int i=0; i<costs.size(); i++)
     {
-        int u = costs[i][0], v = costs[i][1], c = costs[i][2];
-        edges.push_back({c, u, v}); //{비용, 정점1, 정점2}
-    }
-    
-    //비용 오름차순으로 정렬
-    sort(edges.begin(), edges.end());
-
-    vector<int> p(n + 1, -1);
-    int cnt = 0;
-    for(auto e: edges)
-    {
-        int c, u, v;
-        tie(c, u, v) = e;
-        if(!IsDiffParent(u, v, p)) continue;
-        answer+=c;
-        cnt++;
+        int a = costs[i][0];
+        int b = costs[i][1];
+        int cost = costs[i][2];
+        if(uni(a, b, p)) 
+        {
+            cnt++;
+            answer += cost;
+        }
         if(cnt == n-1) break;
     }
     return answer;
