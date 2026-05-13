115ff12b7fc793941c0c3cbf9b8d20c0e80b714d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-13 20:21:17 +0900

    [level 4] Title: 도둑질, Time: 5.18 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -6,20 +6,20 @@ using namespace std;
 int solution(vector<int> money) {
     int answer = 0;
     
-    int n = money.size();
-    vector<int> dp1(n, 0);
-    vector<int> dp2(n, 0);
+    vector<int> dp1(money.size(), 0);
+    vector<int> dp2(money.size(), 0);
     
-    dp1[0] = money[0];
-    dp1[1] = money[0];
-    for(int i=2; i<n-1; i++)
-        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i]);
-    
+    // 1. 첫 번째 집을 터는 경우
+    dp1[0] = dp1[1] = money[0];
+    for(int i=2; i<money.size() - 1; i++)
+        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1]);
+
+    // 2. 두 번째 집을 터는 경우
     dp2[0] = 0;
     dp2[1] = money[1];
-    for(int i=2; i<n; i++)
-        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i]);
-    
-    answer = max(dp1[n-2], dp2[n-1]);
+    for(int i=2; i<money.size(); i++)
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1]);
+
+    answer = max(dp1[money.size()-2], dp2[money.size()-1]);
     return answer;
 }
