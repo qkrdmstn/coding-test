e677f35414de5917971e93ef0e76b8482b1db68e
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-13 23:21:04 +0900

    [level 3] Title: 아이템 줍기, Time: 0.07 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -4,21 +4,24 @@
 #include <iostream>
 using namespace std;
 
-
-void BuildBoard(vector<vector<int>>& board, vector<vector<int>>& rectangle)
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
+void BuildBoard(vector<vector<int>>& rectangle, vector<vector<int>>& board)
 {
-    for(auto& r: rectangle)
+    for(auto& rect: rectangle)
     {
-        int minX = r[0], minY = r[1], maxX = r[2], maxY = r[3];
+        int minX = rect[0], minY = rect[1], maxX = rect[2], maxY = rect[3];
         for(int x=minX; x<=maxX; x++)
         {
             for(int y=minY; y<=maxY; y++)
             {
+                // 1: 가장자리, 2: 사각형 내부
                 if(x > minX && x < maxX && y > minY && y < maxY)
-                    board[x][y] = 2; //내부
-                else{
-                    if(board[x][y] != 2) //다른 사각형의 내부면 제외
-                        board[x][y] = 1; //테두리
+                    board[x][y] = 2;
+                else
+                {
+                    // 다른 사각형의 내부로 이미 매핑되었다면 무시.
+                    if(board[x][y] != 2) board[x][y] = 1;
                 }
             }
         }
@@ -26,42 +29,43 @@ void BuildBoard(vector<vector<int>>& board, vector<vector<int>>& rectangle)
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int dx[4] = {0, 1, 0, -1};
-    int dy[4] = {1, 0, -1, 0};
+    int answer = 0;
     
-    //면과 선을 구분하기 위해 좌표계를 2배 확장합니다.
-    for(auto& r: rectangle){
-        for(int& val: r) val*=2;
+    // 선과 면을 구분하기 위해 좌표계 2배 확장
+    for(auto& rect: rectangle)
+    {
+        for(auto& x: rect)
+            x *= 2;
     }
-    characterX *=2; characterY *=2;
-    itemX *= 2; itemY *= 2;
+    characterX *= 2, characterY *= 2;
+    itemX *= 2, itemY *= 2;
     
-    //보드 채우기
-    vector<vector<int>> board(110, vector<int>(110));
-    vector<vector<int>> dist(110, vector<int>(110, -1));
-    BuildBoard(board, rectangle);
+    // board에 사각형 내부, 가장자리를 표시합니다.
+    vector<vector<int>> board(110, vector<int>(110, 0));
+    BuildBoard(rectangle, board);
 
-    //BFS
+    // BFS를 통해 board에 표시된 가장자리 위치를 순회합니다.
     queue<pair<int, int>> q;
-    q.push({characterX,characterY});
+    vector<vector<int>> dist(110, vector<int>(110, -1));
+    q.push({characterX, characterY});
     dist[characterX][characterY] = 0;
     while(!q.empty())
     {
-        int curX = q.front().first;
-        int curY = q.front().second;
-        q.pop();
-        if(curX == itemX && curY == itemY) return dist[curX][curY] / 2;
+        auto cur = q.front(); q.pop();
+    
+        if(cur.first == itemX && cur.second == itemY)
+            return dist[cur.first][cur.second] / 2; // 두 배 확장한 좌표계를 되돌립니다.
+
         for(int dir=0; dir<4; dir++)
         {
-            int nx = curX + dx[dir];
-            int ny = curY + dy[dir];
-            if(nx < 0 || nx > 101 || ny < 0 || ny > 101) continue;
+            int nx = cur.first + dx[dir];
+            int ny = cur.second + dy[dir];
+            if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101) continue;
             if(dist[nx][ny] >= 0 || board[nx][ny] != 1) continue;
-            dist[nx][ny] = dist[curX][curY] + 1;
-            q.push({nx, ny});
             
+            q.push({nx,ny});
+            dist[nx][ny] = dist[cur.first][cur.second] + 1;
         }
     }
-    
-    return 0;
+    return answer;
 }
