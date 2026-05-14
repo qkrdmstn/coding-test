eed18fbfad6f166c776a678c9235daf85f0b45b5
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-14 14:30:18 +0900

    [level 3] Title: 순위, Time: 1.07 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -5,19 +5,17 @@ using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
+    vector<vector<bool>> adj(n+1, vector<bool>(n+1, false));
+    for(auto& res: results)
+        adj[res[0]][res[1]] = true;
     
-    vector<vector<int>> dist(n+1, vector<int>(n+1, 0));
-    for(auto &r: results)
-        dist[r[0]][r[1]] = 1;
-
     for(int k=1; k<=n; k++)
     {
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=n; j++)
             {
-                if(dist[i][k] == 1 && dist[k][j] == 1)
-                    dist[i][j] = 1;
+                if(adj[i][k] && adj[k][j]) adj[i][j] = true;
             }
         }
     }
@@ -27,10 +25,15 @@ int solution(int n, vector<vector<int>> results) {
     {
         for(int j=1; j<=n; j++)
         {
-            cnt[i] += dist[i][j];
-            cnt[j] += dist[i][j];
+            if(i==j) continue;
+            if(adj[i][j])
+            {
+                cnt[i]++;
+                cnt[j]++;
+            }
         }
     }
+    
     for(int i=1; i<=n; i++)
     {
         if(cnt[i] == n-1) answer++;
