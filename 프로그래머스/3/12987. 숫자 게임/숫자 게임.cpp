414f0d2cf8e3b441e5aafee6afa70f0f54927f39
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-19 17:10:10 +0900

    [level 3] Title: 숫자 게임, Time: 37.25 ms, Memory: 14.8 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.cpp
@@ -1,23 +1,26 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <algorithm>
 using namespace std;
 
 int solution(vector<int> A, vector<int> B) {
     int answer = 0;
     
-    multiset<int> bSet(B.begin(), B.end());
+    // 중복 가능한 set 자료구조에 넣어 upper_bound를 사용
+    multiset<int> b(B.begin(), B.end());
     for(auto& a: A)
     {
-        auto it = bSet.upper_bound(a);
-        if(it != bSet.end())
+        // a 값보다 큰 수가 있다면 승점 + 1, 그 수 지우기
+        auto it = b.upper_bound(a);
+        if(it != b.end())
         {
-            bSet.erase(it);
             answer++;
+            b.erase(it);
         }
+        // 없다면 가장 작은 수 지우기
         else
-            bSet.erase(bSet.begin());
+            b.erase(b.begin());
     }
+    
     return answer;
 }
