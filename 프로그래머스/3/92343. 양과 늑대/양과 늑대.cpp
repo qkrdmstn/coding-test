fbee1541c0b396925cd12f4afdbab33b8ab9fe52
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-19 15:58:00 +0900

    [level 3] Title: 양과 늑대, Time: 0.47 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -2,35 +2,40 @@
 #include <vector>
 
 using namespace std;
-void dfs(int sheep, int wolf, vector<int>& info, vector<vector<int>>& child, vector<int> candidates, int& ans)
+
+// DFS로 현재 상태로부터 다음 방문 가능한 노드들을 업데이트하며 그곳을 순회합니다.
+void DFS(int sheep, int wolf, vector<int>& info, vector<vector<int>>& child, vector<int>& candidate, int& ans)
 {
     ans = max(ans, sheep);
     
-    for(auto& c: candidates)
+    // 현재 방문 가능한 후보들 순회
+    for(auto& cand: candidate)
     {
-        int newSheep = sheep + (info[c] == 0 ? 1 : 0);
-        int newWolf = wolf + (info[c] == 1 ? 1 : 0);
+        // 다음 노드 방문 시, 양과 늑대 세기
+        int newSheep = (info[cand] == 0) ? sheep + 1 : sheep;
+        int newWolf = (info[cand] == 1) ? wolf + 1 : wolf;
+        if(newWolf >= newSheep) continue; // 늑다 >= 양이면 다음 노드 방문 못함
         
-        if(newWolf >= newSheep) continue;
-        
-        vector<int> nxt;
-        for(auto& c1: candidates)
+        // 다음 후보지들 정리
+        vector<int> nxtCand;
+        for(auto& curCand: candidate)
         {
-            if(c1 != c) nxt.push_back(c1);
+            // 지금 정해진 곳 제외한 나머지 후보지들 그대로 넘기기
+            if(curCand != cand) nxtCand.push_back(curCand);
         }
-        for(auto& ch: child[c])
-            nxt.push_back(ch);
-        dfs(newSheep, newWolf, info, child, nxt, ans);
-        
+        // 현재 노드의 자식 노드들도 후보지에 넣기
+        for(auto& ch: child[cand])
+            nxtCand.push_back(ch);
+        DFS(newSheep, newWolf, info, child, nxtCand, ans);
     }
 }
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
-    
     vector<vector<int>> child(info.size());
     for(auto& e: edges)
         child[e[0]].push_back(e[1]);
     
-    dfs(1, 0, info, child, child[0], answer);
+    DFS(1, 0, info, child, child[0], answer);
+    
     return answer;
 }
