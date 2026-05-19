215ffa40f9f945c8cda50218a0fe1641a7ea61bc
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-19 16:31:24 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 38.98 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -4,30 +4,28 @@
 using namespace std;
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
-    vector<int> answer(enroll.size(), 0);
-    unordered_map<string, int> nameToIdx;
+    vector<int> answer;
     unordered_map<string, string> parent;
-
     for(int i=0; i<enroll.size(); i++)
-    {
         parent[enroll[i]] = referral[i];
-        nameToIdx[enroll[i]] = i;
-    }
     
+    unordered_map<string, int> money;
     for(int i=0; i<seller.size(); i++)
     {
-        string name = seller[i];
-        int money = amount[i] * 100;
-        
-        while(name != "-" && money > 0)
+        string cur = seller[i];
+        int totalMoney = amount[i] * 100;
+        while(cur != "-" && totalMoney >= 1)
         {
-            int toParent = money / 10;
-            int myMoney = money - toParent;
-            answer[nameToIdx[name]] += myMoney;
-            name = parent[name];
-            money = toParent;
-            if(money == 0) break;
+            int fee = totalMoney / 10;;
+            money[cur] += (totalMoney - fee);
+            
+            cur = parent[cur];
+            totalMoney = fee; 
         }
+        money[cur] += totalMoney;
     }
+    
+    for(auto& e: enroll)
+        answer.push_back(money[e]);
     return answer;
 }
