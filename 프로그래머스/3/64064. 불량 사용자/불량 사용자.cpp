cf169a5c58002ddbcfc353b4a534c39c358d51d3
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-22 15:51:41 +0900

    [level 3] Title: 불량 사용자, Time: 26.19 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,74 +1,58 @@
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 using namespace std;
 
-bool check(vector<string>& candidate_id, vector<string>& banned_id)
+// result가 제재 아이디가 될 수 있는지 확인합니다.
+bool IsPossible(vector<string>& result, vector<string>& banned_id)
 {
-    for(int i=0; i<banned_id.size(); i++)
+    int n = result.size();
+    for(int i=0; i<n; i++)
     {
-        if(banned_id[i].length() != candidate_id[i].length()) return false;
-        
-        int len = banned_id[i].length();
+        if(result[i].length() != banned_id[i].length()) return false;
+        int len = result[i].length();
         for(int j=0; j<len; j++)
         {
             if(banned_id[i][j] == '*') continue;
-            if(banned_id[i][j] != candidate_id[i][j]) return false;
+            if(result[i][j] != banned_id[i][j]) return false;
         }
     }
-    
     return true;
 }
 
-void dfs2(vector<bool>& used, vector<string>& candidate,vector<string>& banned_id, vector<string>& res, bool& ans)
+// DFS를 활용해 user_id 중 banned_id와 같은 개수를 뽑아 나열하는 경우를 확인합니다.
+void DFS(vector<bool>& used, vector<string>& user_id, vector<string>& banned_id, vector<string>& result, set<set<string>>& answers)
 {
-    if(ans) return;
-    if(res.size() == candidate.size())
+    // 불량 사용자 개수와 같은 개수를 뽑습니다.
+    if(result.size() == banned_id.size())
     {
-        if(check(res, banned_id)) ans = true;
+        if(IsPossible(result, banned_id))
+        {
+            // 정해진 순열 중 중복을 방지하기 위해 set을 통해 저장합니다.
+            set<string> r(result.begin(), result.end());
+            answers.insert(r);
+        }
         return;
     }
     
-    for(int i=0; i<candidate.size(); i++)
+    for(int i=0; i<user_id.size(); i++)
     {
         if(used[i]) continue;
+        result.push_back(user_id[i]);
         used[i] = true;
-        res.push_back(candidate[i]);
-        dfs2(used, candidate, banned_id, res, ans);
-        res.pop_back();
+        DFS(used, user_id, banned_id, result, answers);
         used[i] = false;
-    }
-}
-
-void dfs(int idx, vector<string>& user_id, vector<string>& banned_id, vector<string>& candidate_id, int& ans)
-{
-    if(candidate_id.size() == banned_id.size())
-    {
-        vector<bool> used(candidate_id.size(), false);
-        vector<string> v;
-        bool result = false;
-        dfs2(used, candidate_id, banned_id, v, result);
-        if(result) ans++;
-        return;
-    }
-    
-    for(int i=idx; i<user_id.size(); i++)
-    {
-        candidate_id.push_back(user_id[i]);
-        dfs(i + 1, user_id, banned_id, candidate_id, ans);
-        candidate_id.pop_back();
+        result.pop_back();
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
     
-
-    for(auto& c: banned_id)
-        cout << c << " ";
-    cout << "----------------------------\n\n";
-    
-    vector<string> candidate_id;
-    dfs(0, user_id, banned_id, candidate_id, answer);
-    return answer;
+    vector<bool> used(user_id.size(), false);
+    vector<string> result;
+    set<set<string>> answers;
+    DFS(used, user_id, banned_id, result, answers);
+    return answers.size();
 }
