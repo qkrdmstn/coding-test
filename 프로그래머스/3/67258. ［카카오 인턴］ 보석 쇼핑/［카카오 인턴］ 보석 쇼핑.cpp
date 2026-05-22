03b038158933a4c47114b1e8539c41e2b6dae55f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-22 15:28:03 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 10.02 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -2,32 +2,43 @@
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    vector<int> answer;
-    
+    vector<int> answer(2);
     int n = gems.size();
-    int totalKind = unordered_set<string>(gems.begin(), gems.end()).size();
-    unordered_map<string, int> count;
     
-    int minLen = n + 1;
-    int l = 0;
-    for(int r=0; r<n; r++)
-    {
-        count[gems[r]]++;
-        while(count.size() == totalKind)
-        {
+    // 전체 보석의 종류를 set을 활용해 계산합니다.
+    int gemsKind = unordered_set<string>(gems.begin(), gems.end()).size();
 
-            int len = r-l+1;
+    unordered_map<string, int> gemsCount;
+    int st = 0;
+    int minLen = n+1;
+    gemsCount[gems[0]] = 1;
+    
+    if(gemsCount.size() == gemsKind)
+        return {1,1};
+    // 슬라이딩 윈도우
+    for(int ed=1; ed<n; ed++)
+    {
+        gemsCount[gems[ed]]++;
+        // 각 종류의 보석이 1개만 남도록 윈도우의 st 지점을 수정합니다.
+        while(gemsCount[gems[st]] > 1)
+        {
+            gemsCount[gems[st]]--;
+            st++;
+        }
+        
+        // map에 기록된 보석의 종류가 전체 종류와 동일하면 조건 만족
+        if(gemsCount.size() == gemsKind)
+        {
+            int len = ed- st;
             if(len < minLen)
             {
                 minLen = len;
-                answer = {l+1, r+1};
+                answer = {st+1, ed+1};
             }
-            count[gems[l]]--;
-            if(count[gems[l]] == 0) count.erase(gems[l]);
-            l++;
         }
     }
     return answer;
