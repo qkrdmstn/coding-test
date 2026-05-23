118f0a2a32e07fbda1ea94bf1b12e45d9454b705
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-23 17:00:59 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.32 ms, Memory: 4.25 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -8,9 +8,13 @@ const int INF = 0x3f3f3f3f;
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
 int solution(vector<vector<int>> board) {
+    int answer = INF;
     int n = board.size();
     vector<vector<vector<int>>> cost(n, vector<vector<int>>(n, vector<int>(4, INF)));
-    priority_queue<tuple<int, int, int, int>, vector<tuple<int, int, int, int>>, greater<tuple<int, int, int, int>>> pq;
+    priority_queue
+        <tuple<int, int, int, int>,
+        vector<tuple<int, int, int, int>>, 
+        greater<tuple<int, int, int, int>>> pq;
     
     pq.push({0,0,0,0});
     pq.push({0,0,0,1});
@@ -19,28 +23,25 @@ int solution(vector<vector<int>> board) {
     {
         int curCost, cx, cy, cDir;
         tie(curCost, cx, cy, cDir) = pq.top(); pq.pop();
-        if(curCost > cost[cx][cy][cDir]) continue;
         
+        if(cost[cx][cy][cDir] != curCost) continue;
         for(int dir=0; dir<4; dir++)
         {
-            int nx = cx + dx[dir];
-            int ny = cy + dy[dir];
-            if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if(board[nx][ny] == 1) continue;     
+            int nx = cx + dx[dir], ny = cy + dy[dir];
+            if(nx < 0 || nx >= n || ny < 0 || ny >=  n) continue;
+            if(board[nx][ny] == 1) continue;
             
-            int addCost = (dir == cDir) ? 100:600;
+            int addCost = (dir == cDir) ? 100 : 600;
             int newCost = curCost + addCost;
             if(newCost < cost[nx][ny][dir])
             {
-                cost[nx][ny][dir] = newCost;
                 pq.push({newCost, nx, ny, dir});
+                cost[nx][ny][dir] = newCost;
             }
-            
         }
     }
     
-    int answer = INF;
-    for(int dir=0; dir<4;dir++)
-        answer = min(answer, cost[n-1][n-1][dir]);
+    for(int dir=0; dir<4; dir++)
+        answer = min(cost[n-1][n-1][dir], answer);
     return answer;
 }
