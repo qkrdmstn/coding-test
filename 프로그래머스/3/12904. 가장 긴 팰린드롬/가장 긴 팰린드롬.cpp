a7d0d67c5237606ad8c0067fcdcf737d0c325e5e
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-23 17:30:12 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 12.50 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
@@ -4,31 +4,32 @@
 using namespace std;
 int solution(string s)
 {
-    int n = s.length();
-    vector<vector<bool>> dp(n, vector<bool>(n, false));
-    
     int answer = 1;
-    for(int i=0; i<n; i++)
-        dp[i][i] = true;
+    int len = s.length();
     
-    for(int i=0; i<n-1; i++)
+    // dp[st][ed]: st부터 ed까지의 문자열이 팰린드롬인지 나타낸다.
+    vector<vector<bool>> dp(len, vector<bool>(len, false));
+    
+    for(int i=0; i<len; i++)
     {
-        if(s[i] == s[i+1])
+        dp[i][i] = true;
+        if(i+1 < len && s[i] == s[i+1])
         {
             dp[i][i+1] = true;
-            answer = max(answer, 2);  
+            answer = 2;
         }
     }
     
-    for(int diff=2; diff<n; diff++)
+    for(int diff=2; diff<len; diff++)
     {
-        for(int st=0; st<n-diff; st++)
+        for(int st=0; st<len-diff; st++)
         {
             int ed = st + diff;
+            // [st, ed]는 [st+1, ed-1]가 팰린드롬이고, st와 ed번째 문자가 같으면 팰린드롬이다.
             if(s[st] == s[ed] && dp[st+1][ed-1])
             {
-                answer = max(diff+1, answer);
                 dp[st][ed] = true;
+                answer = max(diff+1, answer);
             }
         }
     }
