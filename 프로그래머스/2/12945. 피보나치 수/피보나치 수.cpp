e04dd657ff8b6e0e06043a34339761b46916877d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-05-26 16:52:16 +0900

    [level 2] Title: 피보나치 수, Time: 0.48 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int PRIME = 1234567;
+int solution(int n) {
+    
+    vector<int> dp(n+1, 0);
+    
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i=2; i<=n; i++)
+        dp[i] = (dp[i-1] + dp[i-2]) % PRIME;
+    return dp[n];
+}
