12854581f45a155ec9934119e6271f0c8e7f353e
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 14:36:23 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 0.50 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.cpp
@@ -2,19 +2,20 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 int dx[4] = {0,1,0,-1};
 int dy[4] = {1,0,-1,0};
+
 vector<pair<int, int>> Normalize(vector<pair<int, int>>& block)
 {
     int minX = block[0].first, minY = block[0].second;
-    for(auto& pos: block)
+    for(const auto& pos: block)
     {
         minX = min(pos.first, minX);
         minY = min(pos.second, minY);
     }
+    
     for(auto& pos: block)
     {
         pos.first -= minX;
@@ -32,15 +33,14 @@ vector<pair<int, int>> Rotate(vector<pair<int, int>>& block)
         pos.first = pos.second;
         pos.second = -tmp;
     }
-    Normalize(block);
-    return block;
+    return Normalize(block);
 }
 
 vector<vector<pair<int, int>>> GetBlocks(vector<vector<int>>& board, int target)
 {
-    vector<vector<pair<int, int>>> blocks;
-    
     int n = board.size();
+    vector<vector<pair<int, int>>> blocks;
+    queue<pair<int, int>> q;
     vector<vector<bool>> vis(n, vector<bool>(n, false));
     
     for(int i=0; i<n; i++)
@@ -48,11 +48,9 @@ vector<vector<pair<int, int>>> GetBlocks(vector<vector<int>>& board, int target)
         for(int j=0; j<n; j++)
         {
             if(vis[i][j] || board[i][j] != target) continue;
-            queue<pair<int, int>> q;
-            vector<pair<int, int>> block;
-            
             q.push({i, j});
             vis[i][j] = true;
+            vector<pair<int, int>> block;
             while(!q.empty())
             {
                 auto cur = q.front(); q.pop();
@@ -70,40 +68,37 @@ vector<vector<pair<int, int>>> GetBlocks(vector<vector<int>>& board, int target)
             blocks.push_back(Normalize(block));
         }
     }
-    
-
     return blocks;
 }
 
 int solution(vector<vector<int>> game_board, vector<vector<int>> table) {
     int answer = 0;
-    vector<vector<pair<int, int>>> blanks = GetBlocks(game_board, 0);
+    vector<vector<pair<int, int>>> cells = GetBlocks(game_board, 0);
     vector<vector<pair<int, int>>> blocks = GetBlocks(table, 1);
-    
     vector<bool> usedBlock(blocks.size(), false);
     
-    for(auto& blank: blanks)
+    for(const auto& hole: cells)
     {
-        bool isFound = false;
+        bool isFind = false;
         for(int i=0; i<blocks.size(); i++)
         {
-            if(usedBlock[i]) continue;
-            if(blank.size() != blocks[i].size()) continue;
+            if(usedBlock[i] || hole.size() != blocks[i].size()) continue;
+            vector<pair<int, int>> curBlock = blocks[i];
+            
             for(int rot=0; rot<4; rot++)
             {
-                if(blank == blocks[i])
+                if(hole == curBlock)
                 {
-                    answer += blocks[i].size();
                     usedBlock[i] = true;
-                    isFound = true;
+                    isFind = true;
+                    answer += blocks[i].size();
                     break;
                 }
-                Rotate(blocks[i]);
+                
+                curBlock = Rotate(curBlock);
             }
-            if(isFound) break;
+            if(isFind) break;
         }
     }
-    
-
     return answer;
 }
