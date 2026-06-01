a6a64c6f2ca027865050aacd16812acaacc9185f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-02 00:08:43 +0900

    [level 3] Title: 풍선 터트리기, Time: 10.23 ms, Memory: 46.2 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -2,25 +2,30 @@
 #include <vector>
 
 using namespace std;
-
-const int INF = 0x3f3f3f3f;
+const int INF = 1'000'000'001;
 int solution(vector<int> a) {
     int answer = 0;
     
+    // 각각 좌/우로부터 시작해 i번째까지 가장 작은 수를 기록합니다.
     int n = a.size();
-    vector<int> rmin(n, INF);
-    vector<int> lmin(n, INF);
+    vector<int> minLeft(n, INF);
+    vector<int> minRight(n, INF);
     
-    lmin[0] = a[0];
-    rmin[n-1] = a[n-1];
+    minLeft[0] = a[0];
+    minRight[n-1] = a[n-1];
     for(int i=1; i<n; i++)
     {
-        lmin[i] = min(a[i], lmin[i-1]);
-        rmin[n-1-i] = min(a[n-1-i], rmin[n-i]);
+        minLeft[i] = min(minLeft[i-1], a[i]);
+        minRight[n-1-i] = min(minRight[n-i], a[n-1-i]);
     }
+    
+    // 현재 풍선은 양쪽의 가장 최소값을 가진 풍선보다 하나라도 작으면 최후까지 남을 수 있습니다.
     for(int i=1; i<n-1; i++)
     {
-        if(lmin[i-1] > a[i] || rmin[i+1] > a[i]) answer++;
+        if(a[i] < minLeft[i-1] || a[i] < minRight[i+1])
+            answer++;
     }
-    return answer+2;
+
+    // 양쪽 끝 풍선은 반드시 최후까지 남을 수 있습니다.
+    return answer + 2;
 }
