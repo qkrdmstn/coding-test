0dd53df15d367031e87739fe9134e11ac5a648f2
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 15:12:44 +0900

    [level 3] Title: 외벽 점검, Time: 15.09 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -1,37 +1,39 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-const int INF = 0x3f3f3f3f;
-int foo(vector<int>& dist, vector<int>& weak)
+const int INF = 17;
+int func(vector<int>& dist, vector<int>& weak)
 {
     int ans = INF;
-    for(int s=0; s<weak.size()/2; s++)
+    for(int st=0; st<weak.size()/2; st++)
     {
         int friendIdx = 0;
-        int finishLine = weak[s] + dist[friendIdx];
-        for(int i=s; i<s+weak.size()/2; i++)
+        int cur = weak[st] + dist[friendIdx];
+        for(int i=st; i<st + weak.size()/2; i++)
         {
-            if(finishLine < weak[i])
+            if(cur < weak[i])
             {
                 friendIdx++;
                 if(friendIdx >= dist.size()) break;
-                finishLine = weak[i] + dist[friendIdx];
+                cur = weak[i] + dist[friendIdx];
             }
         }
         if(friendIdx != dist.size())
-            ans = min(ans, friendIdx + 1);
+            ans = min(friendIdx+1, ans);
     }
     return ans;
 }
+
 void DFS(vector<bool>& used, vector<int>& dist, vector<int>& res, vector<int>& weak, int& ans)
 {
     if(res.size() == dist.size())
     {
-        ans = min(ans, foo(res, weak));
+        ans = min(func(res, weak), ans);
         return;
     }
+    
     for(int i=0; i<dist.size(); i++)
     {
         if(used[i]) continue;
@@ -45,9 +47,9 @@ void DFS(vector<bool>& used, vector<int>& dist, vector<int>& res, vector<int>& w
 int solution(int n, vector<int> weak, vector<int> dist) {
     int answer = INF;
     
-    int sz = weak.size();
-    for(int i=0; i<sz; i++)
-        weak.push_back(weak[i] + n);
+    int weakNum = weak.size();
+    for(int i=0; i<weakNum; i++)
+        weak.push_back(weak[i]+n);
     
     vector<int> res;
     vector<bool> used(dist.size(), false);
