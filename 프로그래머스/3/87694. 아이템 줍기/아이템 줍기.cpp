87f614ac0bfce239d7219507085e87e9bf4040da
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 18:09:23 +0900

    [level 3] Title: 아이템 줍기, Time: 0.08 ms, Memory: 3.93 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,32 +1,34 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
+#include <queue>
+
 using namespace std;
+int dx[4] = {0,1,0,-1};
+int dy[4] = {1,0,-1,0};
 
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {1, 0, -1, 0};
-
+// 사각형들의 좌표를 바탕으로 맵에 다각형의 테두리를 구분합니다.
 vector<vector<int>> BuildBoard(vector<vector<int>>& rectangle)
 {
-    vector<vector<int>> board(110, vector<int>(110, 0));
-    for(auto& r: rectangle)
+    // 좌표 확장을 고려해 150x150 크기로 맵을 설정합니다.
+    vector<vector<int>> board(150, vector<int>(150, 0));
+            
+    for(auto& rect: rectangle)
     {
-        int minX = r[0], minY = r[1];
-        int maxX = r[2], maxY = r[3];
-        for(int x = minX; x<=maxX; x++)
+        int minX = rect[0], minY = rect[1];
+        int maxX = rect[2], maxY = rect[3];
+        for(int x=minX; x<=maxX; x++)
         {
             for(int y=minY; y<=maxY; y++)
             {
-                // 사각형 내부
-                if(x > minX && x < maxX && y > minY && y < maxY)
-                    board[x][y] = 2;
-                // 사각형 가장자리
+                // 다른 사각형의 내부로 판정된 영역은 다각형의 테두리가 될 수 없습니다.
+                if(board[x][y] == 2) continue;
+                
+                // 현재 사각형의 가장자리는 테두리 후보(1), 내부(2)로 마킹합니다.
+                if(x == minX || x == maxX || y == minY || y == maxY)
+                    board[x][y] = 1;
                 else
-                {
-                    // 다른 사각형의 내부가 아니면 가장자리로 처리 (1)
-                    if(board[x][y] != 2) board[x][y] = 1;
-                }
+                    board[x][y] = 2;
             }
         }
     }
@@ -34,50 +36,44 @@ vector<vector<int>> BuildBoard(vector<vector<int>>& rectangle)
 }
 
 int solution(vector<vector<int>> rectangle, int characterX, int characterY, int itemX, int itemY) {
-    int answer = 0;
-    
-    // 점과 선을 구분하기 위해 모든 좌표계를 2배 늘립니다.
-    characterX *= 2, characterY *= 2;
-    itemX *= 2, itemY *= 2;
+    // 점과 선을 구분하기 위해 좌표계를 2배 늘립니다.
+    characterX *= 2;
+    characterY *= 2;
+    itemX *= 2;
+    itemY *= 2;
     for(auto& rect: rectangle)
     {
-        for(auto& r: rect)
-            r *= 2;
+        for(int i=0; i<4; i++)
+            rect[i] *= 2;
     }
-    // 주어진 사각형의 내부와 외부를 구분하여 플레이어가 이동한 길을 표시합니다.
+
+    // 맵을 구축하고, BFS를 통해 테두리(1)만 타고 이동하는 item에 도달하는 최단 거리를 구합니다.
     vector<vector<int>> board = BuildBoard(rectangle);
-    // for(int i=0; i<30; i++)
-    // {
-    //     for(int j=0; j<30; j++)
-    //         cout << board[i][j] << " ";
-    //     cout << "\n";
-    // }
-    
-    // BFS 알고리즘을 이용해 플레이어의 이동을 시뮬레이션합니다.
+    vector<vector<int>> dist(150, vector<int>(150, -1));
     queue<pair<int, int>> q;
-    vector<vector<int>> dist(110, vector<int>(110, -1));
     q.push({characterX, characterY});
     dist[characterX][characterY] = 0;
     
     while(!q.empty())
     {
         auto cur = q.front(); q.pop();
-        // 목표 지점에 도달한 경우
+        // 아이템 이치에 도달하면 탐색을 종료합니다.
         if(cur.first == itemX && cur.second == itemY)
         {
-            answer = dist[itemX][itemY]/2;
-            break;
+            // 2배 확장된 좌표계에서 원본 스케일로 복구합니다.
+            return dist[cur.first][cur.second] / 2;
         }
-        for(int dir=0; dir<4; dir++)
+        
+        for(int dir=0;dir<4; dir++)
         {
             int nx = cur.first + dx[dir];
             int ny = cur.second + dy[dir];
-            if(nx < 0 || nx >= 101 || ny < 0 || ny >= 101) continue;
-            if(board[nx][ny] != 1 || dist[nx][ny] >= 0) continue;
+            // 2배 확장된 사각형들의 최대 좌표 범위를 고려하여 경계를 확인합니다.
+            if(nx < 0 || nx > 101 || ny < 0 || ny > 101) continue;
+            // 미방문 지역이면서 테두리인 경로만 방문 가능합니다.
+            if(dist[nx][ny] >= 0 || board[nx][ny] != 1) continue;
             q.push({nx, ny});
             dist[nx][ny] = dist[cur.first][cur.second] + 1;
         }
     }
-     
-    return answer;
 }
