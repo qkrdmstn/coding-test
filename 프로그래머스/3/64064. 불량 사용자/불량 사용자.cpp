cf71d587a142bf33c02c09342df73950f587c1dc
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 15:47:35 +0900

    [level 3] Title: 불량 사용자, Time: 10.36 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,58 +1,50 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <iostream>
 using namespace std;
 
-// result가 제재 아이디가 될 수 있는지 확인합니다.
-bool IsPossible(vector<string>& result, vector<string>& banned_id)
+bool IsMatch(string& user, string banned)
 {
-    int n = result.size();
-    for(int i=0; i<n; i++)
+    if(user.length() != banned.length()) return false;
+    
+    int len = banned.length();
+    for(int i=0; i<len; i++)
     {
-        if(result[i].length() != banned_id[i].length()) return false;
-        int len = result[i].length();
-        for(int j=0; j<len; j++)
-        {
-            if(banned_id[i][j] == '*') continue;
-            if(result[i][j] != banned_id[i][j]) return false;
-        }
+        if(banned[i] == '*') continue;
+        if(banned[i] != user[i]) return false;
     }
     return true;
 }
 
-// DFS를 활용해 user_id 중 banned_id와 같은 개수를 뽑아 나열하는 경우를 확인합니다.
-void DFS(vector<bool>& used, vector<string>& user_id, vector<string>& banned_id, vector<string>& result, set<set<string>>& answers)
+// 각 depth당 banned_id에 매치되는 user_Id를 하나씩 선택해 나아간다.
+void DFS(int depth, vector<bool>& used, vector<string>& user_id, vector<string>& banned_id, set<int>& res, set<set<int>>& ans)
 {
-    // 불량 사용자 개수와 같은 개수를 뽑습니다.
-    if(result.size() == banned_id.size())
+    // 중복 제거를 위해 set 자료구조에 삽입
+    if(depth == banned_id.size())
     {
-        if(IsPossible(result, banned_id))
-        {
-            // 정해진 순열 중 중복을 방지하기 위해 set을 통해 저장합니다.
-            set<string> r(result.begin(), result.end());
-            answers.insert(r);
-        }
+        ans.insert(res);
         return;
     }
     
     for(int i=0; i<user_id.size(); i++)
     {
+        // 이전 depth 에서 이미 사용된 user_id라면 건너뛴다,
         if(used[i]) continue;
-        result.push_back(user_id[i]);
+        if(!IsMatch(user_id[i], banned_id[depth])) continue;
         used[i] = true;
-        DFS(used, user_id, banned_id, result, answers);
+        res.insert(i);
+        DFS(depth + 1, used, user_id, banned_id, res, ans);
+        res.erase(i);
         used[i] = false;
-        result.pop_back();
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
     int answer = 0;
     
+    set<set<int>> ans;
     vector<bool> used(user_id.size(), false);
-    vector<string> result;
-    set<set<string>> answers;
-    DFS(used, user_id, banned_id, result, answers);
-    return answers.size();
+    set<int> res;
+    DFS(0, used, user_id, banned_id, res, ans);
+    return ans.size();
 }
