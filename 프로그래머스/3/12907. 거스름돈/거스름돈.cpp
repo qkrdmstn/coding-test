d16cd4045159782013d49c5f2e542e6cd9b5aaec
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 13:56:56 +0900

    [level 3] Title: 거스름돈, Time: 1.83 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -2,15 +2,18 @@
 #include <vector>
 
 using namespace std;
-const int PRIME = 1'000'000'007;
 
 int solution(int n, vector<int> money) {
+    
     vector<int> dp(n+1, 0);
     dp[0] = 1;
-    for(int& coin: money)
+    
+    // 같은 조합을 중복으로 세지 않도록 동전을 기준으로 먼저 순회합니다.
+    for(const auto& coin: money)
     {
+        // i원은 (i-coin)원을 만드는 방법에 현재 동전을 더하면 만들 수 있습니다.
         for(int i=coin; i<=n; i++)
-            dp[i] = (dp[i] + dp[i-coin]) % PRIME;
+            dp[i] += dp[i-coin];
     }
     return dp[n];
 }
