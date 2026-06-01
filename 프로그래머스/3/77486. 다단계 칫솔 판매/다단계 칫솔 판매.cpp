d246feb21a44b812b901522bb20bf7e474a86ccd
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-01 16:35:22 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.19 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -1,31 +1,34 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
+#include <iostream>
 using namespace std;
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
     vector<int> answer;
+    
+    // 이익금을 분배할 트리를 구성합니다.
     unordered_map<string, string> parent;
     for(int i=0; i<enroll.size(); i++)
         parent[enroll[i]] = referral[i];
     
+    // 각 판매자로부터, 부모를 따라 올라가 이익금을 분배합니다.
     unordered_map<string, int> money;
     for(int i=0; i<seller.size(); i++)
     {
-        string cur = seller[i];
-        int totalMoney = amount[i] * 100;
-        while(cur != "-" && totalMoney >= 1)
+        int curMoney = amount[i] * 100;
+        string curName = seller[i];
+        while(curMoney >= 1)
         {
-            int fee = totalMoney / 10;;
-            money[cur] += (totalMoney - fee);
-            
-            cur = parent[cur];
-            totalMoney = fee; 
+            int fee = curMoney/10;
+            money[curName] += curMoney - fee;
+            curName = parent[curName];
+            curMoney = fee;
         }
-        money[cur] += totalMoney;
     }
-    
-    for(auto& e: enroll)
-        answer.push_back(money[e]);
+   
+    // enroll에 적힌 순서대로 answer에 수익 저장
+    for(int i=0; i<enroll.size(); i++)
+        answer.push_back(money[enroll[i]]);
     return answer;
 }
