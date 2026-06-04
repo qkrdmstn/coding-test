a816f41a2dd46bb1d641d68a2d7cffbe2e03281c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-04 17:14:26 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.10 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
     
+    // 최소 힙을 통해 정해진 우선순위에 따라 작업을 선택할 수 있도록 합니다.
     priority_queue<tuple<int, int, int>,
                     vector<tuple<int, int, int>>,
                     greater<tuple<int, int, int>>> reqPQ; // {요청 시각, 소요 시간, 작업 번호}
@@ -15,6 +16,7 @@ int solution(vector<vector<int>> jobs) {
                     vector<tuple<int, int, int>>,
                     greater<tuple<int, int, int>>> waitPQ; // {소요 시간, 요청 시각, 작업 번호}
     
+    // 요청 힙에 작업들을 모두 넣어 요청시각 우선으로 오름차순 정렬합니다.
     int n = jobs.size();
     for(int i=0; i<n; i++)
     {
@@ -23,9 +25,10 @@ int solution(vector<vector<int>> jobs) {
     }
     
     int curTime = 0;
+    // 모든 요청이 완료될 때까지 작업을 수행합니다.
     while(!reqPQ.empty() || !waitPQ.empty())
     {
-        cout << curTime << " \n";
+        // 요청 시각이 현재 시간보다 이전이라면 대기 큐에 작업을 삽입합니다.
         while(!reqPQ.empty())
         {
             int s, l, i;
@@ -34,6 +37,8 @@ int solution(vector<vector<int>> jobs) {
             reqPQ.pop();
             waitPQ.push({l, s, i});
         }
+        
+        // 현재 시간보다 이전에 온 요청이 없으면 가장 빠른 다음 요청으로 시간을 건너뜁니다.
         if(waitPQ.empty() && !reqPQ.empty())
         {
             int s, l, i;
@@ -42,12 +47,15 @@ int solution(vector<vector<int>> jobs) {
             continue;
         }
         
+        // 대기 큐에 있는 우선순위가 가장 높은 작업을 수행합니다.
         int l, s, i;
         tie(l, s, i) = waitPQ.top(); waitPQ.pop();
         
+        // 작업 소요 시간만큼 현재 시간을 이동시키고, 반환 시간을 계산합니다.
         curTime += l;
         answer += (curTime - s);
-        cout << i << " " << s << ' ' << curTime << "\n";
     }
+    
+    // 평균을 계산합니다.
     return answer/n;
 }
