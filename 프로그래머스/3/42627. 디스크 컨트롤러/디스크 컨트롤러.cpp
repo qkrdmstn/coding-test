bc51bf794203d529a1170b8efeb4f6dd5082382f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-04 16:10:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.18 ms, Memory: 3.75 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -1,38 +1,53 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <tuple>
+#include <iostream>
 using namespace std;
 
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-    int curTime = 0;
-    priority_queue
-        <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>
-        jobQueue; //{요청 시각, 소요 시간}
-    priority_queue
-        <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>
-        waitQueue; //{소요 시간, 요청 시각}
-
-    for(auto& job: jobs)
-        jobQueue.push({job[0], job[1]});
-
-    while(!jobQueue.empty() || !waitQueue.empty())
+    
+    priority_queue<tuple<int, int, int>,
+                    vector<tuple<int, int, int>>,
+                    greater<tuple<int, int, int>>> reqPQ; // {요청 시각, 소요 시간, 작업 번호}
+    priority_queue<tuple<int, int, int>,
+                    vector<tuple<int, int, int>>,
+                    greater<tuple<int, int, int>>> waitPQ; // {소요 시간, 요청 시각, 작업 번호}
+    
+    int n = jobs.size();
+    for(int i=0; i<n; i++)
     {
-        while(!jobQueue.empty() && jobQueue.top().first <= curTime)
-        {
-            waitQueue.push({jobQueue.top().second, jobQueue.top().first});
-            jobQueue.pop();
-        }
-        //현재 시각에 대기 중인 작업이 없는 경우, 점프
-        if(!jobQueue.empty() && waitQueue.empty())
-        {
-            curTime = jobQueue.top().first;
-            continue;            
-        }
-        curTime += waitQueue.top().first;
-        //반환 시간 = 작업 종료 시각 - 요청 시각
-        answer += curTime - waitQueue.top().second;
-        waitQueue.pop();
+        int s = jobs[i][0], l = jobs[i][1];
+        reqPQ.push({s, l, i});
     }
-    return answer/jobs.size();
+    
+    int curTime = 0;
+    while(!reqPQ.empty() || !waitPQ.empty())
+    {
+        cout << curTime << " \n";
+        while(!reqPQ.empty())
+        {
+            int s, l, i;
+            tie(s, l, i) = reqPQ.top();
+            if(curTime < s) break;
+            reqPQ.pop();
+            waitPQ.push({l, s, i});
+        }
+        if(waitPQ.empty() && !reqPQ.empty())
+        {
+            int s, l, i;
+            tie(s, l, i) = reqPQ.top();
+            curTime = s;
+            continue;
+        }
+        
+        int l, s, i;
+        tie(l, s, i) = waitPQ.top(); waitPQ.pop();
+        
+        curTime += l;
+        answer += (curTime - s);
+        cout << i << " " << s << ' ' << curTime << "\n";
+    }
+    return answer/n;
 }
