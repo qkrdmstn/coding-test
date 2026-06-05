7728a4bc50863629bf7e68a556905125131df650
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-05 18:33:42 +0900

    [level 3] Title: 합승 택시 요금, Time: 6.29 ms, Memory: 7.61 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -8,32 +8,40 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     int answer = 0;
     
     vector<vector<int>> dist(n+1, vector<int>(n+1, INF));
-    for(auto fare: fares)
-    {
-        dist[fare[0]][fare[1]] = fare[2];
-        dist[fare[1]][fare[0]] = fare[2];
-    }
-    
     for(int i=1; i<=n; i++)
         dist[i][i] = 0;
-    
-    for(int k=1; k<=n; k++)
+    for(const auto& f: fares)
     {
-        for(int i=1; i<=n; i++)
+        if(f[2] < dist[f[0]][f[1]])
         {
-            for(int j=1; j<=n; j++)
+            dist[f[0]][f[1]] = f[2];
+            dist[f[1]][f[0]] = f[2];
+        }
+    }
+    
+    // 플로이드 워셜 알고리즘을 통해 모든 경로의 최단 거리를 탐색합니다.
+    for(int mid=1; mid<=n; mid++)
+    {
+        for(int st=1; st<=n; st++)
+        {
+            for(int ed=1; ed<=n; ed++)
             {
-                if(dist[i][j] > dist[i][k] + dist[k][j])
-                    dist[i][j] = dist[i][k] + dist[k][j];
+                if(dist[st][mid] + dist[mid][ed] < dist[st][ed])
+                    dist[st][ed] = dist[st][mid] + dist[mid][ed];
             }
         }
     }
     
+    // 합승을 하지 않는 방법을 초기값으로 설정합니다.
     answer = dist[s][a] + dist[s][b];
-    for(int k=1; k<=n; k++)
+
+    // mid 지점까지 합승하는 방법을 탐색합니다.
+    for(int mid=1; mid<=n; mid++)
     {
-        if(dist[s][k] == INF || dist[k][a] == INF || dist[k][b] == INF) continue;
-        answer = min(answer, dist[s][k] + dist[k][a] + dist[k][b]);
+        // mid 지점을 거쳐가는 경로가 없는 경우 skip 합니다.
+        if(dist[s][mid] == INF || dist[mid][a] == INF || dist[mid][b] == INF) continue;
+        int d = dist[s][mid] + dist[mid][a] + dist[mid][b];
+        answer = min(d, answer);
     }
     return answer;
 }
