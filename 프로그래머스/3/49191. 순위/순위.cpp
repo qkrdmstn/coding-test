3c252c7c311532421652b3b778db736b716856de
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-05 16:52:53 +0900

    [level 3] Title: 순위, Time: 1.04 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -1,42 +1,40 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 int solution(int n, vector<vector<int>> results) {
     int answer = 0;
-    vector<vector<bool>> adj(n+1, vector<bool>(n+1, false));
-    for(auto& res: results)
-        adj[res[0]][res[1]] = true;
     
+    vector<vector<bool>> vis(n+1, vector<bool>(n+1, false));
+    for(auto const& res: results)
+        vis[res[0]][res[1]] = true;
+
+    // 플로이드 워셜 알고리즘을 이용해 모든 승패 관계를 계산합니다.
     for(int k=1; k<=n; k++)
     {
-        for(int i=1; i<=n; i++)
+        for(int st=1; st<=n; st++)
         {
-            for(int j=1; j<=n; j++)
+            for(int ed=1 ; ed<=n; ed++)
             {
-                if(adj[i][k] && adj[k][j]) adj[i][j] = true;
+                if(vis[st][k] && vis[k][ed])
+                    vis[st][ed] = true;
             }
         }
     }
     
-    vector<int> cnt(n+1, 0);
+    // 자신을 제외한 나머지 모든 사람과 승패가 결정된 사람은
+    // 순위를 계산할 수 있습니다.
     for(int i=1; i<=n; i++)
     {
+        int cnt = 0;
         for(int j=1; j<=n; j++)
         {
             if(i==j) continue;
-            if(adj[i][j])
-            {
-                cnt[i]++;
-                cnt[j]++;
-            }
+            if(vis[i][j] || vis[j][i]) cnt++;
         }
-    }
-    
-    for(int i=1; i<=n; i++)
-    {
-        if(cnt[i] == n-1) answer++;
+        if(cnt == n-1)
+            answer++;
     }
     return answer;
 }
