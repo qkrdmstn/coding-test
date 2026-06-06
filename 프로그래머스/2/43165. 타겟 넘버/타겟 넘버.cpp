b7c36e99d389c386a532a6158719af707f37b0c7
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 16:54:03 +0900

    [level 2] Title: 타겟 넘버, Time: 2.52 ms, Memory: 3.91 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,21 +3,24 @@
 
 using namespace std;
 
-void DFS(int curSum, int target, int curLen, int maxLen, vector<int>& numbers, int& answer)
+void DFS(int depth, int sum, vector<int>& numbers, int target, int& ans)
 {
-    if(curLen == maxLen)
+    // 더한 수의 개수가 주어진 배열과 같으면 종료합니다.
+    if(depth == numbers.size())
     {
-        if(curSum == target)
-            answer++;
+        if(sum == target) ans++;
         return;
     }
-    DFS(curSum + numbers[curLen], target, curLen + 1, maxLen, numbers, answer);
-    DFS(curSum - numbers[curLen], target, curLen + 1, maxLen, numbers, answer);
+    
+    // 각 재귀마다 현재 수의 부호를 양수/음수로 설정하여 더합니다.
+    int num = numbers[depth];
+    DFS(depth + 1, sum + num, numbers, target, ans);
+    DFS(depth + 1, sum - num, numbers, target, ans);
 }
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     
-    DFS(0, target, 0, numbers.size(), numbers, answer);
+    DFS(0, 0, numbers, target, answer);
     return answer;
 }
