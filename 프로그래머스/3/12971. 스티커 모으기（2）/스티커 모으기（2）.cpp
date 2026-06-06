03b850a115f0b49968eee53ba8da940eff8c9fe8
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 21:49:02 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.69 ms, Memory: 7.04 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -8,21 +8,18 @@ int solution(vector<int> sticker)
     int n = sticker.size();
     if(n == 1) return sticker[0];
     
-    // dp[i] = 스티커가 sitcker[i] 까지 있을 때의 최대 합
-    // 원형이기 때문에 첫 번째 스티거를 때는 경우/아닌 경우로 나눠 dp를 구성합니다.
-    
-    // sticker[0] 를 반드시 뜯어내는 경우 최대 합
     vector<int> dp1(n, 0);
+    vector<int> dp2(n, 0);
+    
     dp1[0] = dp1[1] = sticker[0];
-    for(int i=2; i<n; i++)
+    for(int i=2; i<n-1; i++)
         dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i]);
     
-    // sticker[1] 를 반드시 뜯어내는 경우의 최대 합
-    vector<int> dp2(n, 0);
     dp2[0] = 0;
     dp2[1] = sticker[1];
     for(int i=2; i<n; i++)
         dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i]);
+    
     answer = max(dp1[n-2], dp2[n-1]);
     return answer;
 }
