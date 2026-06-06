bc6515aae147ebcb126090e71a3627b0c0a19628
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 15:57:45 +0900

    [level 3] Title: 양과 늑대, Time: 0.44 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,41 +1,41 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
-// DFS로 현재 상태로부터 다음 방문 가능한 노드들을 업데이트하며 그곳을 순회합니다.
-void DFS(int sheep, int wolf, vector<int>& info, vector<vector<int>>& child, vector<int>& candidate, int& ans)
+void DFS(int curSheep, int curWolf, vector<int>& info, vector<vector<int>>& child, vector<int>& candidates, int& ans)
 {
-    ans = max(ans, sheep);
-    
-    // 현재 방문 가능한 후보들 순회
-    for(auto& cand: candidate)
+    ans = max(curSheep, ans);
+    for(auto& nxt: candidates)
     {
-        // 다음 노드 방문 시, 양과 늑대 세기
-        int newSheep = (info[cand] == 0) ? sheep + 1 : sheep;
-        int newWolf = (info[cand] == 1) ? wolf + 1 : wolf;
-        if(newWolf >= newSheep) continue; // 늑다 >= 양이면 다음 노드 방문 못함
+        // nxt 노드를 방문했을 때, 양과 늑대를 계산합니다.
+        int newSheep = (info[nxt] == 0) ? curSheep + 1 : curSheep;
+        int newWolf = (info[nxt] == 0) ? curWolf : curWolf + 1;
         
-        // 다음 후보지들 정리
-        vector<int> nxtCand;
-        for(auto& curCand: candidate)
+        // 늑대의 수가 양의 수보다 같거나 클 경우 방문이 불가능합니다.
+        if(newSheep <= newWolf) continue;
+        
+        // 다음 방문 가능한 후보지들을 갱신합니다. 이때 현재 방문지를 제외합니다.
+        vector<int> nxtCandidates;
+        for(int i=0; i<candidates.size(); i++)
         {
-            // 지금 정해진 곳 제외한 나머지 후보지들 그대로 넘기기
-            if(curCand != cand) nxtCand.push_back(curCand);
+            if(candidates[i] == nxt) continue;
+            nxtCandidates.push_back(candidates[i]);
         }
-        // 현재 노드의 자식 노드들도 후보지에 넣기
-        for(auto& ch: child[cand])
-            nxtCand.push_back(ch);
-        DFS(newSheep, newWolf, info, child, nxtCand, ans);
+        
+        // 현재 방문지와 연결된 노드들도 포함합니다.
+        for(const auto& c: child[nxt])
+            nxtCandidates.push_back(c);
+        DFS(newSheep, newWolf, info, child, nxtCandidates, ans);
     }
 }
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
-    vector<vector<int>> child(info.size());
-    for(auto& e: edges)
+    
+    int n = info.size();
+    vector<vector<int>> child(n);
+    for(const auto& e: edges)
         child[e[0]].push_back(e[1]);
-    
+
     DFS(1, 0, info, child, child[0], answer);
-    
     return answer;
 }
