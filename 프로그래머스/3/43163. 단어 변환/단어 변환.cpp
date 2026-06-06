210319f605f30a2b672d47fe7e5278595687710d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 17:35:22 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 3.98 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,66 +1,50 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <iostream>
+
 using namespace std;
 
-bool CheckChangePossible(string a, string b)
+const int INF = 55;
+
+// 주어진 규칙에 맞게 변환이 가능한지 확인하는 함수입니다.
+bool IsConvertable(string& cur, string& target)
 {
-    int cnt = 0;
-    for(int i=0; i<a.length(); i++)
+    int len = cur.length();
+    int diffCnt = 0;
+    for(int i=0; i<len; i++)
     {
-        if(a[i] != b[i]) cnt++;
+        if(cur[i] != target[i]) diffCnt++;
     }
-    if(cnt == 1) return true;
-    else return false;
+    return diffCnt == 1;
 }
 
-void BuildEdge(string begin, string target, int& beginIdx, int& targetIdx, vector<vector<int>>& edge, vector<string>& words)
+// DFS를 활용해 begin -> target 으로 변환하는 경로를 찾습니다.
+void FindPath(int depth, string cur, vector<bool>& used, vector<string>& words, string target, int& answer)
 {
+    // 변환 성공 시, 몇 번 변환했는지 기록하고 현재 경로 탐색을 종료합니다.
+    if(cur == target)
+    {
+        answer = min(depth, answer);
+        return;
+    }
+    
     for(int i=0; i<words.size(); i++)
     {
-        if(words[i] == begin) beginIdx = i;
-        if(words[i] == target) targetIdx = i;
-        for(int j=i; j<words.size(); j++)
-        {
-            if(i==j) continue;
-            if(CheckChangePossible(words[i], words[j]))
-            {
-                edge[i].push_back(j);
-                edge[j].push_back(i);
-            }
-        }
+        // 현재 문자열에서 다음 문자열로 규칙에 의해 변환이 불가능하면 건너뜁니다.
+        if(used[i] || !IsConvertable(cur, words[i])) continue;
+        used[i] = true;
+        FindPath(depth + 1, words[i], used, words, target, answer);
+        used[i] = false;
     }
 }
 
 int solution(string begin, string target, vector<string> words) {
-    int answer = 0;
-    int beginIdx = -1;
-    int targetIdx = -1;
-    words.push_back(begin);
-    vector<vector<int>> edge(words.size(), vector<int>());
-    vector<int> dist(words.size(), -1);
+    int answer = INF;
     
-    BuildEdge(begin, target, beginIdx, targetIdx, edge, words);
-    if(targetIdx == -1) return 0;
+    int n = words.size();
+    vector<bool> used(n, false);
+    FindPath(0, begin, used, words, target, answer);
     
-    queue<int> q;
-    q.push(beginIdx);
-    dist[beginIdx] = 0;
-    while(!q.empty())
-    {
-        int cur = q.front();
-        q.pop();
-        if(cur == targetIdx) break;
-        
-        for(auto nxt: edge[cur])
-        {
-            if(dist[nxt] >= 0) continue;
-            dist[nxt] = dist[cur] + 1;
-            q.push(nxt);
-        }
-    }
-    
-    if(dist[targetIdx] < 0) return 0;
-    return dist[targetIdx];
+    if(answer == INF) return 0;
+    return answer;
 }
