e4f7aa4cceec880831e03994f0fbf577c8f76d5f
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 22:13:03 +0900

    [level 4] Title: 사칙연산, Time: 1.44 ms, Memory: 3.92 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -1,49 +1,46 @@
 #include <vector>
 #include <string>
-#include <iostream>
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
 int solution(vector<string> arr)
 {
-    // 식에서 숫자를 추출한다.
-    vector<int> num;
-    for(auto& a: arr)
+    int answer = -1;
+    
+    vector<int> number;
+    for(const auto& a: arr)
     {
-        if(a != "+" && a != "-") num.push_back(stoi(a));
+        if(a != "+" && a != "-")
+            number.push_back(stoi(a));
     }
     
-    // DP[i][j]: i~j 번째 숫자 구간에서 서로 다른 연산순서의 최댓값 최대/최소 결과값
-    int n = num.size();
+    int n = number.size();
     vector<vector<int>> maxDP(n, vector<int>(n, -INF));
     vector<vector<int>> minDP(n, vector<int>(n, INF));
     for(int i=0; i<n; i++)
-        maxDP[i][i] = minDP[i][i] = num[i]; // diff == 0인 식은 자기 자신이 최대/최소 값이 된다.
+        maxDP[i][i] = minDP[i][i] = number[i];
     
-    for(int diff = 1; diff < n; diff++)
+    for(int len=2; len<=n; len++)
     {
-        for(int st = 0; st < n-diff; st++)
+        for(int st=0; st<=n-len; st++)
         {
-            int ed = st + diff;
-            // [st,ed] 구간에서 중간 지점 k를 택해 계산을 수행하여 최대/최소값을 택한다.
-            for(int k=st; k<ed; k++)
+            int ed = st + len - 1;
+            for(int m=st; m<ed; m++)
             {
-                int opIdx = 2*k + 1;
-                // 중간 연산자가 +인 경우, max[st][ed] = max[st][k] + max[k+1][ed] / min[st][ed] = min[st][k] + min[k+1][ed] 으로 성립
-                if(arr[opIdx] == "+")
+                string op = arr[2*m+1];
+                if(op == "+")
                 {
-                    maxDP[st][ed] = max(maxDP[st][k] + maxDP[k+1][ed], maxDP[st][ed]);
-                    minDP[st][ed] = min(minDP[st][k] + minDP[k+1][ed], minDP[st][ed]);
+                    maxDP[st][ed] = max(maxDP[st][m] + maxDP[m+1][ed], maxDP[st][ed]);
+                    minDP[st][ed] = min(minDP[st][m] + minDP[m+1][ed], minDP[st][ed]);
                 }
-                // -인 경우, max[st][ed] = max[st][k] - min[k+1][ed] / min[st][ed] = min[st][k] - max[k+1][ed] 이 된다.
                 else
                 {
-                    maxDP[st][ed] = max(maxDP[st][k] - minDP[k+1][ed], maxDP[st][ed]);
-                    minDP[st][ed] = min(minDP[st][k] - maxDP[k+1][ed], minDP[st][ed]);
+                    maxDP[st][ed] = max(maxDP[st][m] - minDP[m+1][ed], maxDP[st][ed]);
+                    minDP[st][ed] = min(minDP[st][m] - maxDP[m+1][ed], minDP[st][ed]);
                 }
             }
         }
     }
-
+    
     return maxDP[0][n-1];
 }
