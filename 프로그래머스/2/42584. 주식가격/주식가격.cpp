d8a9daa7508710e6cf0bab5b97f813843a8d4c9a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 17:59:21 +0900

    [level 2] Title: 주식가격, Time: 20.36 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -4,23 +4,31 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer(prices.size(), 0);
+    int n = prices.size();
+
+    vector<int> answer(n);
+    stack<pair<int, int>> st; // {가격, 시간}
     
-    stack<pair<int, int>> s;
+    // 이전 가격이 현재 가격보다 높을 경우 가격이 떨어진 것이므로 스택에서 제거합니다.
     for(int i=0; i<prices.size(); i++)
     {
-        while(!s.empty() && s.top().first > prices[i])
+        int price = prices[i];
+        
+        // 스택의 top에 있는 가격보다 현재 가격이 낮다면 최초로 가격이 떨어진 시점입니다.
+        while(!st.empty() && st.top().first > price)
         {
-            answer[s.top().second] = i - s.top().second;
-            s.pop();
+            // 가격이 떨어지지 않은 시간을 기록합니다.
+            answer[st.top().second] = i - st.top().second;
+            st.pop();
         }
-        s.push({prices[i], i});
+        st.push({price, i});
     }
     
-    while(!s.empty())
+    // 끝까지 가격이 떨어지지 않은 가격들을 제거합니다.
+    while(!st.empty())
     {
-        answer[s.top().second] = prices.size() - s.top().second - 1;
-        s.pop();
+        answer[st.top().second] = (n-1) - st.top().second;
+        st.pop();
     }
     return answer;
 }
