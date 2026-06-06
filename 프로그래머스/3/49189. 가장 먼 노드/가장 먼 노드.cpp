2da6bf9e8301151c22ff8aa42d3dd5158495a29c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-06-06 21:42:25 +0900

    [level 3] Title: 가장 먼 노드, Time: 5.65 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.cpp
@@ -1,14 +1,13 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 using namespace std;
 
 int solution(int n, vector<vector<int>> edge) {
     int answer = 0;
     
     vector<vector<int>> adj(n+1);
-    for(auto &e: edge)
+    for(const auto& e: edge)
     {
         adj[e[0]].push_back(e[1]);
         adj[e[1]].push_back(e[0]);
@@ -23,18 +22,18 @@ int solution(int n, vector<vector<int>> edge) {
     while(!q.empty())
     {
         int cur = q.front(); q.pop();
-        maxDist = max(maxDist, dist[cur]);
-        for(auto &nxt: adj[cur])
+        maxDist = max(dist[cur], maxDist);
+        for(const auto& nxt: adj[cur])
         {
-            if(dist[nxt] >= 0) continue;
+            if(dist[nxt]>=0) continue;
             q.push(nxt);
             dist[nxt] = dist[cur] + 1;
         }
     }
     
-    for(int i=1; i<=n; i++)
+    for(const int& d: dist)
     {
-        if(maxDist == dist[i]) answer++;
+        if(d == maxDist) answer++;
     }
     return answer;
 }
