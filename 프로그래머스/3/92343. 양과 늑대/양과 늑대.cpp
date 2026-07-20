2695955b841f4dd3c6d4dbaaff7187181c5cc453
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-20 15:24:22 +0900

    [level 3] Title: 양과 늑대, Time: 0.49 ms, Memory: 4.58 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -1,41 +1,40 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
-void DFS(int curSheep, int curWolf, vector<int>& info, vector<vector<int>>& child, vector<int>& candidates, int& ans)
+void DFS(int sheep, int wolf, vector<int> candidate, const vector<int>& info, const vector<vector<int>>& edge, int& answer)
 {
-    ans = max(curSheep, ans);
-    for(auto& nxt: candidates)
+    answer = max(answer, sheep);
+    
+    for(const auto& c1: candidate)
     {
-        // nxt 노드를 방문했을 때, 양과 늑대를 계산합니다.
-        int newSheep = (info[nxt] == 0) ? curSheep + 1 : curSheep;
-        int newWolf = (info[nxt] == 0) ? curWolf : curWolf + 1;
-        
-        // 늑대의 수가 양의 수보다 같거나 클 경우 방문이 불가능합니다.
+        int newSheep = sheep;
+        int newWolf = wolf;
+        if(info[c1] == 0) newSheep++;
+        else newWolf++;
         if(newSheep <= newWolf) continue;
         
-        // 다음 방문 가능한 후보지들을 갱신합니다. 이때 현재 방문지를 제외합니다.
-        vector<int> nxtCandidates;
-        for(int i=0; i<candidates.size(); i++)
+        vector<int> newCandidate;
+        for(const auto& c2: candidate)
         {
-            if(candidates[i] == nxt) continue;
-            nxtCandidates.push_back(candidates[i]);
+            if(c1 == c2) continue;
+            newCandidate.push_back(c2);
         }
+        for(const auto& child: edge[c1])
+            newCandidate.push_back(child);
         
-        // 현재 방문지와 연결된 노드들도 포함합니다.
-        for(const auto& c: child[nxt])
-            nxtCandidates.push_back(c);
-        DFS(newSheep, newWolf, info, child, nxtCandidates, ans);
+        DFS(newSheep, newWolf, newCandidate, info, edge, answer);
     }
 }
+
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
     
-    int n = info.size();
-    vector<vector<int>> child(n);
-    for(const auto& e: edges)
-        child[e[0]].push_back(e[1]);
-
-    DFS(1, 0, info, child, child[0], answer);
+    vector<vector<int>> edge(info.size(), vector<int>());
+    for(const auto &e: edges)
+        edge[e[0]].push_back(e[1]);
+    
+    DFS(0, 0, {0}, info, edge, answer);
     return answer;
 }
