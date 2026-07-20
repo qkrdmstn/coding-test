207c6bf54d912ef0f550f9bb2c2c320eca6f616d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-20 14:58:53 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 12.49 ms, Memory: 4.54 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
@@ -5,33 +5,36 @@ using namespace std;
 int solution(string s)
 {
     int answer = 1;
-    int len = s.length();
     
-    // dp[st][ed]: st부터 ed까지의 문자열이 팰린드롬인지 나타낸다.
+    int len = s.length();
+    // dp[st][ed]: st~ed까지의 부분문자열이 팰린드롬인지 확인
     vector<vector<bool>> dp(len, vector<bool>(len, false));
     
-    for(int i=0; i<len; i++)
+    // 길이가 1인 문자열은 모두 팰린드롬 문자열입니다.
+    dp[0][0] = true;
+    for(int i=1; i<len; i++)
     {
         dp[i][i] = true;
-        if(i+1 < len && s[i] == s[i+1])
-        {
-            dp[i][i+1] = true;
+        // 앞뒤가 같으면 팰린드롬 문자열입니다.
+        if(s[i-1] == s[i]) {
+            dp[i-1][i] = true;
             answer = 2;
         }
     }
     
-    for(int diff=2; diff<len; diff++)
+    for(int diff = 2; diff<len; diff++)
     {
-        for(int st=0; st<len-diff; st++)
+        for(int st = 0; st<len-diff; st++)
         {
             int ed = st + diff;
-            // [st, ed]는 [st+1, ed-1]가 팰린드롬이고, st와 ed번째 문자가 같으면 팰린드롬이다.
+            // 시작과 끝 문자가 같고, 사이의 부분문자열이 팰린드롬이면 st~ed는 팰린드롬입니다.
             if(s[st] == s[ed] && dp[st+1][ed-1])
             {
                 dp[st][ed] = true;
-                answer = max(diff+1, answer);
+                answer = max(answer, ed-st+1);                
             }
         }
     }
+    
     return answer;
 }
