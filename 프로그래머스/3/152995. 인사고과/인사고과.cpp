9651798b05bdd549df57343a908f8f2dd5f8c75c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-21 13:23:20 +0900

    [level 3] Title: 인사고과, Time: 20.26 ms, Memory: 21.8 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -1,11 +1,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
-
 using namespace std;
 
-bool cmp(vector<int>& a, vector<int>& b)
+bool cmp(const vector<int>& a, const vector<int>& b)
 {
     if(a[0] == b[0])
         return a[1] < b[1];
@@ -13,38 +11,36 @@ bool cmp(vector<int>& a, vector<int>& b)
 }
 
 int solution(vector<vector<int>> scores) {
+    int answer = 0;
+    
     // A: 근무 태도 점수, B: 동료 평가 점수
     int wanhoA = scores[0][0];
     int wanhoB = scores[0][1];
     
-    // A 내림차순, B 오름차순으로 정렬합니다.
+    // A를 내림차순, B를 오름차순 정렬
     sort(scores.begin(), scores.end(), cmp);
     
-    // 정렬에 의해 자신보다 앞에 있는 점수는 A가 무조건 높습니다.
-    // 따라서, 앞에 있는 점수들 중 B의 값이 나보다 높다면 둘 다 낮은 것이 됩니다.
-    // 앞에서부터 B의 최대값을 추적하여, 다른 사원보다 두 점수가 모두 낮은지 판단합니다.
+    // 인센티브를 받을 수 있는 인원을 필터링합니다.
     vector<int> sums;
     int maxB = scores[0][1];
-    for(const auto& score : scores)
+    for(const auto& score: scores)
     {
-        // 현재 사원의 점수는 다른 사원보다 A, B 모두 낮음
+        // A는 이미 내림차순이므로, B가 앞의 최대보다 작으면 인센티브를 받지 못합니다.
         if(score[1] < maxB)
         {
-            // 완호의 점수와 같다면 -1 반환
+            // 완호가 받지 못하면 즉시 종료합니다.
             if(score[0] == wanhoA && score[1] == wanhoB)
                 return -1;
             continue;
         }
-        // 통과한 점수들은 합을 계산하여 저장합니다.
         sums.push_back(score[0] + score[1]);
         maxB = max(score[1], maxB);
     }
     
+    // 점수의 합을 내림차순으로 정렬합니다.
     sort(sums.begin(), sums.end(), greater<int>());
-    for(int i=0; i<sums.size(); i++)
+    for(int i=0; i<(int)sums.size(); i++)
     {
-        if(wanhoA + wanhoB == sums[i]) return i + 1;
+        if(wanhoA + wanhoB == sums[i]) return i+1;
     }
-    
-    return 0;
 }
