ac48447dcd5ffb18d90a5fa05cfe795be1af2cce
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-21 12:13:32 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 7.86 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -1,39 +1,37 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
 long long solution(vector<int> sequence) {
     long long answer = 0;
-    int n = sequence.size();
     
-    // 주어진 수열에 각각의 펄스 수열을 곱합니다.
-    vector<int> arr1(n, 0);
-    vector<int> arr2(n, 0);
-    for(int i=0; i<n; i++)
+    vector<int> seq1;
+    vector<int> seq2;
+    int len = sequence.size();
+    
+    // 펄스 수열의 두 가지 경우를 곱한 수열을 만들어냅니다.
+    int c = 1;
+    for(const auto& s: sequence)
     {
-        if(i%2 == 0)
-        {
-            arr1[i] = sequence[i];
-            arr2[i] = -sequence[i];
-        }
-        else
-        {
-            arr1[i] = -sequence[i];
-            arr2[i] = sequence[i];
-        }
+        seq1.push_back(s * c);
+        seq2.push_back(s * -c);
+        c *= -1;
     }
     
-    vector<long long> dp1(n, 0);
-    vector<long long> dp2(n, 0);
-    dp1[0] = arr1[0];
-    dp2[0] = arr2[0];
-    for(int i=1; i<n; i++)
+    vector<long long> dp1(len, 0);
+    vector<long long> dp2(len, 0);
+    dp1[0] = max(seq1[0], 0);
+    dp2[0] = max(seq2[0], 0);
+    
+    // 누적합을 포함할지, 현재 원소부터 다시 합을 시작할지 결정하는 DP를 활용합니다.
+    for(int i=1; i<len; i++)
     {
-        dp1[i] = max(dp1[i-1] + arr1[i], (long long)arr1[i]);
-        dp2[i] = max(dp2[i-1] + arr2[i], (long long)arr2[i]);
+        dp1[i] = max(dp1[i-1] + seq1[i], (long long)seq1[i]);
+        dp2[i] = max(dp2[i-1] + seq2[i], (long long)seq2[i]);
     }
-    for(int i=0; i<n; i++)
+    
+    for(int i=0; i<len; i++)
         answer = max(max(dp1[i], dp2[i]), answer);
     return answer;
 }
