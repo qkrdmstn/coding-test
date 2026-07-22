2b7b2fccc2578bd5b517b64b3e108332838a455c
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-22 14:42:22 +0900

    [level 3] Title: 스티커 모으기(2), Time: 0.70 ms, Memory: 7.37 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.cpp
@@ -4,22 +4,21 @@ using namespace std;
 
 int solution(vector<int> sticker)
 {
-    int answer =0;
-    int n = sticker.size();
-    if(n == 1) return sticker[0];
+    int answer = 0;
     
-    vector<int> dp1(n, 0);
-    vector<int> dp2(n, 0);
+    int sz = sticker.size();
+    if(sz == 1) return sticker[0];
     
+    
+    vector<int> dp1(sz, 0);
     dp1[0] = dp1[1] = sticker[0];
-    for(int i=2; i<n-1; i++)
-        dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i]);
+    for(int i=2; i<sz-1; i++)
+        dp1[i] = max(dp1[i-2] + sticker[i], dp1[i-1]);
     
+    vector<int> dp2(sz, 0);
     dp2[0] = 0;
     dp2[1] = sticker[1];
-    for(int i=2; i<n; i++)
-        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i]);
-    
-    answer = max(dp1[n-2], dp2[n-1]);
-    return answer;
+    for(int i=2; i<sz; i++)
+        dp2[i] = max(dp2[i-2] + sticker[i], dp2[i-1]);
+    return max(dp1[sz-2], dp2[sz-1]);
 }
