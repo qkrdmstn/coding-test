700a87d5b285aaa994811bcbab69169df9f383d6
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-22 14:28:53 +0900

    [level 4] Title: 도둑질, Time: 6.65 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -6,20 +6,21 @@ using namespace std;
 int solution(vector<int> money) {
     int answer = 0;
     
-    vector<int> dp1(money.size(), 0);
-    vector<int> dp2(money.size(), 0);
+    int len = money.size();
     
-    // 1. 첫 번째 집을 터는 경우
+    // 원형 배치를 일렬로 나열한 뒤, 첫 번째 집을 터는 경우와 두 번째 집을 터는 경우를 나누어 계산합니다.
+    // 각 경우 dp를 활용해 현재 집을 털지, 털지 않을지를 계산합니다.
+    vector<int> dp1(len, 0);
     dp1[0] = dp1[1] = money[0];
-    for(int i=2; i<money.size() - 1; i++)
-        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1]);
-
-    // 2. 두 번째 집을 터는 경우
+    // 첫 번쨰 집을 터는 경우, 마지막 집을 털지 못하기 때문에 len-2번째 집까지 순회합니다.
+    for(int i=2; i<len-1; i++)
+        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i]);
+    
+    vector<int> dp2(len, 0);
     dp2[0] = 0;
     dp2[1] = money[1];
-    for(int i=2; i<money.size(); i++)
-        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1]);
-
-    answer = max(dp1[money.size()-2], dp2[money.size()-1]);
-    return answer;
+    for(int i=2; i<len; i++)
+        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i]);
+        
+    return max(dp1[len-2], dp2[len-1]);
 }
