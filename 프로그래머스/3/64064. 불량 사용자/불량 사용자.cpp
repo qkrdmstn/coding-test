79536af5091353857f2c151acedc8e0776bd5586
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-24 14:45:43 +0900

    [level 3] Title: 불량 사용자, Time: 9.15 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -1,52 +1,46 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <iostream>
 using namespace std;
 
-bool matched(string user, string banned)
+bool Matched(string& user, string& banned)
 {
     if(user.length() != banned.length()) return false;
-    
     for(int i=0; i<user.length(); i++)
     {
         if(banned[i] == '*') continue;
         if(user[i] != banned[i]) return false;
     }
     return true;
-} 
+}
 
-void DFS(vector<string>& user_id, vector<string>& banned_id, vector<bool>& used, set<int>& candidate, set<set<int>>& result)
+void DFS(int depth, vector<bool>& used, vector<string>& user_id, vector<string>& banned_id, set<int>& candidate, set<set<int>>& result)
 {
-    if(candidate.size() == banned_id.size())
+    if(depth == banned_id.size())
     {
-
         result.insert(candidate);
-
         return;
     }
-    
+
     for(int i=0; i<user_id.size(); i++)
     {
         if(used[i]) continue;
-        if(!matched(user_id[i], banned_id[candidate.size()])) continue;
-        //cout << user_id[i] << " " << banned_id[candidate.size()] <<"\n";
-
+        if(!Matched(user_id[i], banned_id[depth])) continue;
+        
         used[i] = true;
         candidate.insert(i);
-        DFS(user_id, banned_id, used, candidate, result);
+        DFS(depth + 1, used, user_id, banned_id, candidate, result);
         used[i] = false;
         candidate.erase(i);
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-    int answer = 0;
-    
+
     vector<bool> used(user_id.size(), false);
     set<int> candidate;
     set<set<int>> result;
     
-    DFS(user_id, banned_id, used, candidate, result);
+    DFS(0, used, user_id, banned_id, candidate, result);
     return result.size();
 }
