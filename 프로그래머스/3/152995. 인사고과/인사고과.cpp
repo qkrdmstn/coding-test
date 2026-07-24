2d614db8122b711ae72bcc543c4f2c39d006bd36
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-07-24 13:53:59 +0900

    [level 3] Title: 인사고과, Time: 17.12 ms, Memory: 22.5 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/3/152995. 인사고과/인사고과.cpp
@@ -16,31 +16,27 @@ int solution(vector<vector<int>> scores) {
     // A: 근무 태도 점수, B: 동료 평가 점수
     int wanhoA = scores[0][0];
     int wanhoB = scores[0][1];
-    
-    // A를 내림차순, B를 오름차순 정렬
     sort(scores.begin(), scores.end(), cmp);
     
-    // 인센티브를 받을 수 있는 인원을 필터링합니다.
-    vector<int> sums;
+    vector<int> sum;
     int maxB = scores[0][1];
     for(const auto& score: scores)
     {
-        // A는 이미 내림차순이므로, B가 앞의 최대보다 작으면 인센티브를 받지 못합니다.
         if(score[1] < maxB)
         {
-            // 완호가 받지 못하면 즉시 종료합니다.
             if(score[0] == wanhoA && score[1] == wanhoB)
                 return -1;
             continue;
         }
-        sums.push_back(score[0] + score[1]);
-        maxB = max(score[1], maxB);
+        sum.push_back(score[0] + score[1]);
+        maxB = score[1];
     }
+    sort(sum.begin(), sum.end(), greater<int>());
     
-    // 점수의 합을 내림차순으로 정렬합니다.
-    sort(sums.begin(), sums.end(), greater<int>());
-    for(int i=0; i<(int)sums.size(); i++)
+    for(const int& s: sum)
     {
-        if(wanhoA + wanhoB == sums[i]) return i+1;
+        if(s == wanhoA + wanhoB) break;
+        answer++;
     }
+    return answer+1;
 }
