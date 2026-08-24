4a11acab667e23ae86cd7e2a81449854513db2ca
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-24 13:27:18 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.18 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -7,28 +7,34 @@ using namespace std;
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
     vector<int> answer;
     
-    // 이익금을 분배할 트리를 구성합니다.
-    unordered_map<string, string> parent;
-    for(int i=0; i<enroll.size(); i++)
-        parent[enroll[i]] = referral[i];
-    
-    // 각 판매자로부터, 부모를 따라 올라가 이익금을 분배합니다.
+    // 조직 구성원의 이익금과 참여시킨 사람의 정보를 각각 map으로 구성합니다.
+    unordered_map<string, string> nxt;
     unordered_map<string, int> money;
-    for(int i=0; i<seller.size(); i++)
+    for(int i=0; i<(int)enroll.size(); i++)
+    {
+        nxt[enroll[i]] = referral[i];
+        money[enroll[i]] = 0;
+    }
+    
+    // 이익금의 분배를 시뮬레이션합니다.
+    for(int i=0; i<(int)seller.size(); i++)
     {
         int curMoney = amount[i] * 100;
-        string curName = seller[i];
-        while(curMoney >= 1)
+        string cur = seller[i];
+        while(true)
         {
-            int fee = curMoney/10;
-            money[curName] += curMoney - fee;
-            curName = parent[curName];
-            curMoney = fee;
+            // 자신을 참여시킨 사람에게 이익금의 10%를 나눕니다
+            int nxtMoney = curMoney * 0.1;
+            money[cur] += curMoney - nxtMoney;
+            curMoney = nxtMoney;
+            cur = nxt[cur];
+            if(curMoney < 1 || cur == "-") break;
         }
     }
-   
-    // enroll에 적힌 순서대로 answer에 수익 저장
-    for(int i=0; i<enroll.size(); i++)
-        answer.push_back(money[enroll[i]]);
+    
+    // enroll 배열의 순서에 맞게 answer를 구성합니다.
+    for(const auto& e: enroll)
+        answer.push_back(money[e]);
+    
     return answer;
 }
