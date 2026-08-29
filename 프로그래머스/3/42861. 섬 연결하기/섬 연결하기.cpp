e4c357cf1b2db70581de679f3639690395a063f7
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-29 21:07:13 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -3,47 +3,47 @@
 #include <algorithm>
 using namespace std;
 
-// 비용 오름차순으로 costs를 정렬
-bool cmp(vector<int>& a, vector<int>& b)
+bool cmp(const vector<int>& a, const vector<int>& b)
 {
     return a[2] < b[2];
 }
 
-int find(int u, vector<int>& p)
+int find(int x, vector<int>& p)
 {
-    if(p[u] < 0) return u;
-    return p[u] = find(p[u], p);
+    // 탐색하면서 거친 노트를 모두 해당 그룹의 루트 노드에 연결합니다.
+    if(p[x] < 0) return x;
+    return p[x] = find(p[x], p);
 }
 
-bool uni(int u, int v, vector<int>& p)
+bool uni(int a, int b, vector<int>& p)
 {
-    u = find(u, p);
-    v = find(v, p);
-    if(u == v) return false;
-    p[u] = v;
-    return true;
+    a = find(a, p);
+    b = find(b, p);
+    
+    if(a == b) return false;
+    // 트리의 높이를 최대한 낮게 유지하기 위해 높이가 높은 것에 낮은 것을 연결합니다.
+    if(p[a] < p[b])
+        p[b] = a;
+    else
+        p[a] = b;
+    return true;    
 }
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
+    
+    // 각 그룹의 루트를 나타내는 배열입니다.
     vector<int> p(n, -1);
     
-    // costs를 비용 오름차순으로 정렬합니다.
+    // 간선을 비용 오름차순으로 정렬합니다.
     sort(costs.begin(), costs.end(), cmp);
     
-    // 크루스칼 알고리즘을 활용해 최소신장트리를 구성합니다.
     int cnt = 0;
-    for(const auto& cost: costs)
+    for(const auto& c: costs)
     {
-        int u = cost[0], v = cost[1];
-        int c = cost[2];
-        if(uni(u, v, p))
-        {
-            cnt++;
-            answer += c;
-        }
-        
-        // 간선이 n-1개가 지정되면 크루스칼 알고리즘을 종료합니다.
+        if(!uni(c[0], c[1], p)) continue;
+        answer += c[2];
+        cnt++;
         if(cnt == n-1) break;
     }
     return answer;
