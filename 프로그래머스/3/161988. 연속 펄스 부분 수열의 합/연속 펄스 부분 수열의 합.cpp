99b5d25350b334b1e54e816201d6a0f382b0dc40
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-29 22:46:47 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 9.00 ms, Memory: 32.8 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -6,37 +6,30 @@ using namespace std;
 long long solution(vector<int> sequence) {
     long long answer = 0;
     
-    int len = sequence.size();
+    int n = sequence.size();
     vector<int> seq1;
     vector<int> seq2;
-    for(int i=0; i<len; i++)
+    
+    int c = 1;
+    for(const int& seq: sequence)
     {
-        int s = sequence[i];
-        if(i%2==0)
-        {
-            seq1.push_back(s);
-            seq2.push_back(-s);
-        }
-        else
-        {
-            seq1.push_back(-s);
-            seq2.push_back(s);
-        }
+        seq1.push_back(seq * c);
+        seq2.push_back(seq * -c);
+        c *= -1;
     }
     
-    vector<long long> dp1(len, 0);
-    vector<long long> dp2(len, 0);
-
+    vector<long long> dp1(n, 0);
+    vector<long long> dp2(n, 0);
+    
     dp1[0] = max(0, seq1[0]);
     dp2[0] = max(0, seq2[0]);
-    
-    for(int i=1; i<len; i++)
+    for(int i=1; i<n; i++)
     {
-        dp1[i] = max(dp1[i-1] + (long long)seq1[i], (long long)seq1[i]);
-        dp2[i] = max(dp2[i-1] + (long long)seq2[i], (long long)seq2[i]);
+        dp1[i] = max(max(dp1[i-1] + seq1[i], (long long)seq1[i]), (long long)0);
+        dp2[i] = max(max(dp2[i-1] + seq2[i], (long long)seq2[i]), (long long)0);
     }
     
-    for(int i=0; i<len; i++)
+    for(int i=0; i<n; i++)
         answer = max(answer, max(dp1[i], dp2[i]));
     return answer;
 }
