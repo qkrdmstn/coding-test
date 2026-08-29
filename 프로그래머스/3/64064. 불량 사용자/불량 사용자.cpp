38636553eb211dcb0f427821e5d8e3577e587c29
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-29 23:09:41 +0900

    [level 3] Title: 불량 사용자, Time: 8.47 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.cpp
@@ -3,44 +3,48 @@
 #include <set>
 using namespace std;
 
-bool Matched(string& user, string& banned)
+bool IsMatched(const string& user, const string& banned)
 {
     if(user.length() != banned.length()) return false;
-    for(int i=0; i<user.length(); i++)
+    for(int i=0; i<(int)user.length(); i++)
     {
         if(banned[i] == '*') continue;
         if(user[i] != banned[i]) return false;
     }
-    return true;
+     return true;
 }
 
-void DFS(int depth, vector<bool>& used, vector<string>& user_id, vector<string>& banned_id, set<int>& candidate, set<set<int>>& result)
+// DFS 알고리즘을 활용해 현재 banned_id와 매치되는 user_id를 탐색합니다.
+void FindCandidate(int depth, vector<bool>& used, set<int>& candidate, set<set<int>>& results, 
+                   const vector<string>& user_id, const vector<string>& banned_id)
 {
+    // 모든 banned_id와 매치되는 user_id를 찾은 경우 결과로 저장합니다.
     if(depth == banned_id.size())
     {
-        result.insert(candidate);
+        results.insert(candidate);
         return;
     }
-
-    for(int i=0; i<user_id.size(); i++)
+    
+    for(int i=0; i<(int)user_id.size(); i++)
     {
-        if(used[i]) continue;
-        if(!Matched(user_id[i], banned_id[depth])) continue;
+        // 이미 매칭됐거나, 매치되지 않는 user_id는 건너뜁니다.
+        if(used[i] || !IsMatched(user_id[i], banned_id[depth])) continue;
         
         used[i] = true;
         candidate.insert(i);
-        DFS(depth + 1, used, user_id, banned_id, candidate, result);
-        used[i] = false;
+        FindCandidate(depth + 1, used, candidate, results, user_id, banned_id);
         candidate.erase(i);
+        used[i] = false;
     }
 }
 
 int solution(vector<string> user_id, vector<string> banned_id) {
-
-    vector<bool> used(user_id.size(), false);
-    set<int> candidate;
-    set<set<int>> result;
     
-    DFS(0, used, user_id, banned_id, candidate, result);
-    return result.size();
+    // 제재 아이디 후보와 결과를 담을 자료구조를 선언합니다. 이때, 아이디의 순서만 다른 중복을 방지하기 위해 각각 set에 저장합니다.    
+    set<int> candidate;
+    set<set<int>> results;
+    vector<bool> used(user_id.size(), false);
+    
+    FindCandidate(0, used, candidate, results, user_id, banned_id);
+    return results.size();
 }
