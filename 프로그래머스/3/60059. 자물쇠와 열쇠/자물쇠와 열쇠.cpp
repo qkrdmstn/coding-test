b1816696c46e9ad1ceb767f71ba0d9773464e7d1
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-30 17:26:56 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 2.04 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -1,60 +1,60 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
-bool check(vector<vector<int>>& key, vector<vector<int>>& lock, int x, int y)
+// 열쇠를 시계방향으로 회전합니다.
+vector<vector<int>> Rotate(vector<vector<int>>& key, int m)
 {
-    int m = key.size();
-    int n = lock.size();
-    
+    vector<vector<int>> newKey(m, vector<int>(m));
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<m; j++)
+            newKey[j][m-i-1] = key[i][j];
+    }
+    return newKey;
+}
+
+bool IsMatched(int sx, int sy, const vector<vector<int>>& key, const vector<vector<int>>& lock, int n , int m)
+{
     for(int i=0; i<n; i++)
     {
         for(int j=0; j<n; j++)
         {
-            if(i >= x && i < x + m && j >= y && j < y + m)
+            // 열쇠 기준 좌표로 변환합니다.
+            int x = i-sx;
+            int y = j-sy;
+            
+            // 열쇠와 겹친 부분
+            if(x >= 0 && x < m && y >= 0 && y < m)
             {
-                if(lock[i][j] == key[i-x][j-y] ) 
-                    return false;
+                // 홈이 채워지지 않거나 돌기가 만난 경우 매칭X
+                if(key[x][y] == lock[i][j]) return false;
             }
-            else
-            {
-                if(lock[i][j] == 0)
-                    return false;
-            }
+            // 열쇠와 겹치지 않은 부분에 홈이 있다면 매칭 X
+            else if(lock[i][j] == 0) return false;
         }
     }
     return true;
 }
 
-vector<vector<int>> Rotate(vector<vector<int>>& key)
-{
-    int m = key.size();
-    vector<vector<int>> newKey(m, vector<int>(m));
-    for(int i=0; i<key.size(); i++)
-    {
-        for(int j=0; j<key.size(); j++)
-            newKey[m-1-j][i] = key[i][j];
-    }
-    return newKey;
-}
-
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
-    bool answer = true;
-    
-    int n = lock.size();
     int m = key.size();
-    for(int rot=0; rot<4; rot++)
+    int n = lock.size();
+    
+    // 열쇠를 이동 및 회전하면서 자물쇠와의 매칭을 확인합니다.
+    // 열쇠와 자물쇠는 한 칸만 겹쳐도 되기 때문에 열쇠는 -m+1 ~ n-1까지 이동할 수 있습니다.
+    for(int i=-m+1; i<n; i++)
     {
-        for(int i=-m+1; i<n; i++)
+        for(int j= -m+1; j<n; j++)
         {
-            for(int j=-m+1; j<n; j++)
+            for(int rot=0; rot<4; rot++)
             {
-                if(check(key, lock, i, j)) return true;
+                if(IsMatched(i, j, key, lock, n, m)) 
+                    return true;
+                key = Rotate(key, m);
             }
         }
-        key = Rotate(key);
     }
     return false;
 }
