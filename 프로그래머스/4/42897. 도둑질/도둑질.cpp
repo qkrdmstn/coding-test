206e42f02f0e8385b08b50cc136693d88736231d
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-30 15:58:48 +0900

    [level 4] Title: 도둑질, Time: 6.48 ms, Memory: 40.3 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/4/42897. 도둑질/도둑질.cpp
@@ -4,23 +4,28 @@
 using namespace std;
 
 int solution(vector<int> money) {
-    int answer = 0;
     
-    int len = money.size();
+    // 스티커의 개수가 1개인 경우 해당 스티커의 값이 최대입니다. 
+    int n = money.size();
     
-    // 원형 배치를 일렬로 나열한 뒤, 첫 번째 집을 터는 경우와 두 번째 집을 터는 경우를 나누어 계산합니다.
-    // 각 경우 dp를 활용해 현재 집을 털지, 털지 않을지를 계산합니다.
-    vector<int> dp1(len, 0);
+    // 집이 원형으로 연결돼있고, 인접한 집을 털 수 없기 때문에 두 경우로 나눠서 계산합니다.
+    // 각각의 경우 (i-2번째까지의 합 + 현재 집의 돈)과 (i-1번째까지의 합) 중 더 큰 값을 기록합니다.
+    
+    // 첫 번째 집을 훔치는 경우, 마지막 집과 두 번째 집은 털지 못합니다.
+    // 따라서, dp[1] = money[0]가 되고, n-2 번째까지만 계산합니다.
+    vector<int> dp1(n, 0);
     dp1[0] = dp1[1] = money[0];
-    // 첫 번쨰 집을 터는 경우, 마지막 집을 털지 못하기 때문에 len-2번째 집까지 순회합니다.
-    for(int i=2; i<len-1; i++)
-        dp1[i] = max(dp1[i-1], dp1[i-2] + money[i]);
-    
-    vector<int> dp2(len, 0);
+    for(int i=2; i<n-1; i++)
+        dp1[i] = max(dp1[i-2] + money[i], dp1[i-1]);
+
+    // 두 번째 집을 터는 경우, 첫 번째 집은 털지 못합니다.
+    // 따라서, dp[0] = 0이 됩니다.
+    vector<int> dp2(n, 0);
     dp2[0] = 0;
     dp2[1] = money[1];
-    for(int i=2; i<len; i++)
-        dp2[i] = max(dp2[i-1], dp2[i-2] + money[i]);
-        
-    return max(dp1[len-2], dp2[len-1]);
+    for(int i=2; i<n; i++)
+        dp2[i] = max(dp2[i-2] + money[i], dp2[i-1]);
+    
+    // 두 경우에 대한 누적합 중 더 큰 값을 반환합니다.
+    return max(dp1[n-2], dp2[n-1]);
 }
