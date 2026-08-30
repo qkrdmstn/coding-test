28fe9b9615938ced19f2d97256714069b7ebdf09
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-30 16:52:54 +0900

    [level 4] Title: 사칙연산, Time: 0.77 ms, Memory: 4.57 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.cpp
@@ -8,48 +8,47 @@ int solution(vector<string> arr)
 {
     int answer = -1;
     
-    //숫자 추출
-    vector<int> number;
-    for(int i=0; i<arr.size(); i++)
+    // 숫자를 추출합니다.
+    vector<int> numbers;
+    for(int i=0; i<(int)arr.size(); i++)
     {
-        if(i%2==0)
-            number.push_back(stoi(arr[i]));
+        if(i%2==0) 
+            numbers.push_back(stoi(arr[i]));
     }
     
-    int num = number.size();
-    vector<vector<int>> maxDP(num, vector<int>(num, -INF));
-    vector<vector<int>> minDP(num, vector<int>(num, INF));
+    // 연산자에 따라 최대값 연산 방법이 다르기 때문에, min과 max인 경우 모두 저장합니다.
+    int n = numbers.size();
+    vector<vector<int>> minDP(n, vector<int>(n, INF));
+    vector<vector<int>> maxDP(n, vector<int>(n, -INF));
     
-    //길이가 0인 연산
-    for(int i=0; i<num; i++)
-    {
-        maxDP[i][i] = number[i];
-        minDP[i][i] = number[i];
-    }
+    // 길이가 0인 연산을 수행합니다.
+    for(int i=0; i<n; i++)
+        minDP[i][i] = maxDP[i][i] = numbers[i];
     
-    for(int diff=1; diff<num; diff++)
+    for(int diff=0; diff<n; diff++)
     {
-        for(int st=0; st<num-diff; st++)
+        for(int st=0; st<n-diff; st++)
         {
-            int ed=st+diff;
+            int ed = st+diff;
             
-            // st~ed 사이의 수식 중 중간점 k를 기준으로 좌우로 나눠 최대/최소값을 계산합니다.
+            // st~ed-1 사이에 k번째 수를 지정해 k를 기준으로 앞뒤의 식을 계산해 최대/최소값을 갱신합니다.
             for(int k=st; k<ed; k++)
             {
-                int operatorIdx = 2*k+1;
-                // 각 연산자에 맞는 최대/최소 값 계산식을 적용합니다.
-                if(arr[operatorIdx] == "+")
+                // k가 나눈 식의 중간 연산자가 +인 경우와 -인 경우 각각 알맞은 식을 사용해 계산합니다.
+                string op = arr[2*k+1];
+                if(op == "+")
                 {
-                    maxDP[st][ed] = max(maxDP[st][k] + maxDP[k+1][ed], maxDP[st][ed]);
                     minDP[st][ed] = min(minDP[st][k] + minDP[k+1][ed], minDP[st][ed]);
+                    maxDP[st][ed] = max(maxDP[st][k] + maxDP[k+1][ed], maxDP[st][ed]);
                 }
                 else
                 {
-                    maxDP[st][ed] = max(maxDP[st][k] - minDP[k+1][ed], maxDP[st][ed]);
                     minDP[st][ed] = min(minDP[st][k] - maxDP[k+1][ed], minDP[st][ed]);
+                    maxDP[st][ed] = max(maxDP[st][k] - minDP[k+1][ed], maxDP[st][ed]);
                 }
             }
+            
         }
     }
-    return maxDP[0][num-1];
+    return maxDP[0][n-1];
 }
