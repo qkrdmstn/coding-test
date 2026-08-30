261cdfef42d14537136bbcbf8c331c9d4cfcb739
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-30 16:29:11 +0900

    [level 3] Title: 양과 늑대, Time: 0.38 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -3,38 +3,49 @@
 
 using namespace std;
 
-void DFS(int sheep, int wolf, vector<int> candidate, const vector<int>& info, const vector<vector<int>>& edge, int& answer)
+void DFS(int curSheep, int curWolf, vector<int>& candidate, int& maxSheep,
+         const vector<int>& info, const vector<vector<int>>& adj)
 {
-    answer = max(answer, sheep);
-    
-    for(const auto& c1: candidate)
+    if(curSheep > maxSheep)
+        maxSheep = curSheep;
+
+    // 지금까지 방문했던 노드와 인접한 노드를 candidate로 저장하고, 방문 가능해질 때 방문합니다.
+    for(const int& nxt: candidate)
     {
-        int newSheep = sheep;
-        int newWolf = wolf;
-        if(info[c1] == 0) newSheep++;
-        else newWolf++;
-        if(newSheep <= newWolf) continue;
+        // 후보 노드에 방문한 경우 늑대가 같거나 많아지지 않는지 확인합니다.
+        int nxtSheep = curSheep;
+        int nxtWolf = curWolf;
+        if(info[nxt] == 0) nxtSheep++;
+        else nxtWolf++;
+
+        // 늑대가 양과 같거나 많아지면 방문하지 못합니다.
+        if(nxtWolf >= nxtSheep) continue;
         
+        // 방문 가능한 경우
         vector<int> newCandidate;
-        for(const auto& c2: candidate)
+        for(const int& c: candidate)
         {
-            if(c1 == c2) continue;
-            newCandidate.push_back(c2);
+            // 방금 방문한 노드를 제외합니다.
+            if(c == nxt) continue;
+            newCandidate.push_back(c);
         }
-        for(const auto& child: edge[c1])
-            newCandidate.push_back(child);
+        // 현재 방문한 노드와 인접한 노드를 후보지에 추가합니다.
+        for(const int& e: adj[nxt])
+            newCandidate.push_back(e);
         
-        DFS(newSheep, newWolf, newCandidate, info, edge, answer);
+        DFS(nxtSheep, nxtWolf, newCandidate, maxSheep, info, adj);
     }
 }
 
 int solution(vector<int> info, vector<vector<int>> edges) {
     int answer = 0;
     
-    vector<vector<int>> edge(info.size(), vector<int>());
-    for(const auto &e: edges)
-        edge[e[0]].push_back(e[1]);
+    int n = info.size();
+    vector<vector<int>> adj(n);
+    for(const auto& e: edges)
+        adj[e[0]].push_back(e[1]);
     
-    DFS(0, 0, {0}, info, edge, answer);
+    vector<int> candidate = {0};
+    DFS(0, 0, candidate, answer, info, adj);
     return answer;
 }
