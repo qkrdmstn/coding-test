f36c6b7f84801454adc55624b196d1946c9ddf7a
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-31 23:23:53 +0900

    [level 2] Title: 완전범죄, Time: 0.02 ms, Memory: 4.89 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.cpp
@@ -6,24 +6,25 @@ using namespace std;
 int solution(vector<vector<int>> info, int n, int m) {
     int answer = 0;
     
-    int itemCnt = info.size();
-    vector<vector<int>> dp(itemCnt + 1, vector<int>(m, 0));
-    
-    for(int i=1; i<=itemCnt; i++)
+    int num = info.size();
+    vector<vector<int>> dp(num+1, vector<int>(m, 0));
+
+    // dp[i][m]: 물건이 i개, B가 남길 수 있는 흔적이 m개일 때 A도둑이 남긴 흔적의 최솟값
+    for(int i=1; i<=num; i++)
     {
         for(int j=0; j<m; j++)
         {
-            int a = info[i-1][0];
-            int b = info[i-1][1];
+            int itemIdx = i-1;
             
-            // b의 흔적을 더 남길 수 없다면, a가 훔칩니다.
-            if(j < b)
-                dp[i][j] = dp[i-1][j] + a;
-            // b가 훔치는 것과, a가 훔치는 것 중 a의 남는 흔적이 최소인 것을 선택합니다.
+            // B의 흔적이 m 이상이 되어 훔치지 못하는 경우 A의 흔적을 남깁니다.
+            if(j < info[itemIdx][1])
+                dp[i][j] = dp[i-1][j] + info[itemIdx][0];
+            // 훔칠 수 있는 경우, B가 훔치는 경우와 A가 훔치는 경우 중 A의 흔적이 적게 남는 것을 택합니다.
             else
-                dp[i][j] = min(dp[i-1][j-b], dp[i-1][j] + a);
+                dp[i][j] = min(dp[i-1][j-info[itemIdx][1]], dp[i-1][j] + info[itemIdx][0]);
         }
     }
-    if(dp[itemCnt][m-1] >= n) return -1;
-    return dp[itemCnt][m-1];
+    
+    if(dp[num][m-1] >= n) return -1;
+    return dp[num][m-1];
 }
