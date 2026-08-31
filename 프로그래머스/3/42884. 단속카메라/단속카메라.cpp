3519626acccf076c6d8a8b0d80199c3dd176d5eb
qkrdmstn <82009755+qkrdmstn@users.noreply.github.com>
2026-08-31 22:54:46 +0900

    [level 3] Title: 단속카메라, Time: 0.41 ms, Memory: 4.78 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -1,35 +1,9 @@
 #include <string>
 #include <vector>
-#include <algorithm>
-using namespace std;
 
-bool cmp(const vector<int>& a, const vector<int>& b)
-{
-    return a[1] < b[1];    
-}
+using namespace std;
 
 int solution(vector<vector<int>> routes) {
     int answer = 0;
-
-    // 진출 지점 오름차순으로 정렬
-    sort(routes.begin(), routes.end(), cmp);
-    
-
-    // 첫 번째 차가 나가는 지점에 단속카메라를 하나 설치합니다.
-    answer++;
-    
-    int n = routes.size();
-    int lastCameraPos = routes[0][1];
-    for(int i=1; i<n; i++)
-    {
-        // 진출지점 오름차순이기 때문에,
-        // 마지막으로 카메라가 설치된 위치보다 앞에서 들어왔으면 해당 카메라와 만납니다.
-        if(routes[i][0] <= lastCameraPos) continue;
-        
-        // 마지막 카메라보다 뒤에 진입한 경우, 해당 차의 진출지점에 카메라를 설치합니다.
-        answer++;
-        lastCameraPos = routes[i][1];
-    }
-    
     return answer;
 }
